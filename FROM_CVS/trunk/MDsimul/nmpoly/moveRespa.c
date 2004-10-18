6808bf4cfb702b418a976b367257e2adf55623bf
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2004-10-18 23:04:45 +0000

    * forse così è la soluz. ottimale...
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5564 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- FROM_CVS/trunk/MDsimul/nmpoly/moveRespa.c
+++ FROM_CVS/trunk/MDsimul/nmpoly/moveRespa.c
@@ -770,8 +770,8 @@ void shakePosRespa(COORD_TYPE dt, COORD_TYPE tol, int maxIt, int NB, COORD_TYPE 
 #ifdef MD_RAPACONSTR
       if (!doshake[i])
 	continue;
-      else 
-	doshake[i]=0;
+      //else 
+	//doshake[i]=0;
 #endif
       /* ====== >>>> VELOCITY VERLET ALGORITHM PART A <<< ======= */
       for(a=0; a < NA; a++)
@@ -2581,7 +2581,7 @@ void movebRespa(COORD_TYPE dt, COORD_TYPE tol, int maxIt, int NB,
 #endif
 #endif
     }
-#if 0
+#if 1
 #if defined(MD_RAPACONSTR)  
   shakeVelRespaNPT(Oparams.parnum, Oparams.steplength, Oparams.m, 150, NA-1, Oparams.d, 
   		      1E-7, px, py, pz);
