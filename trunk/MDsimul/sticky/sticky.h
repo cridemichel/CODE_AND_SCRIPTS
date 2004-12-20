4171e4341e83749002ce6f1e283026c73ead54ac
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2004-12-20 22:51:50 +0000

    * implementato il calcolo della distanza ma forse andrebbe ottimizzato.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@11125 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- trunk/MDsimul/sticky/sticky.h
+++ trunk/MDsimul/sticky/sticky.h
@@ -65,7 +65,7 @@ enum {MD_CORE_BARRIER=0,MD_INOUT_BARRIER,MD_OUTIN_BARRIER,MD_EVENT_NONE};
 
 #define C_T COORD_TYPE
 #define NK 10000
-#define NA 1 /* number of atoms for each molecule (particle) */
+#define NA 5 /* number of atoms for each molecule (particle) */
 
 #define MAXPAR 3000      /* maximum number of simulated particles */
 
