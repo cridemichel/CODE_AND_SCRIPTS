a07dd91fe33dee2fc8692eb48a1c0228d43ed877
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-04-01 09:40:29 +0000

    * qualche sui parametri per la Silica.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5955 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- FROM_CVS/trunk/MDsimul/sticky/sticky.h
+++ FROM_CVS/trunk/MDsimul/sticky/sticky.h
@@ -675,11 +675,12 @@ struct singlePar OsinglePar[] = {
   {"P",          &Oparams.P,                  CT},
   {"L",          &L,                          CT},
 #ifdef MD_SILICA
-  {"sigmaSi", &Oparams.sigma[0][0], CT},
-  {"sigmaO",  &Oparams.sigma[1][0], CT},
+  {"sigmaSiSi", &Oparams.sigma[0][0], CT},
+  {"sigmaOO",  &Oparams.sigma[1][0], CT},
   {"sigmaSiO", &Oparams.sigma[0][1], CT},
   {"massSi",   &Oparams.m[0], CT},
   {"massO",    &Oparams.m[1], CT},
+  {"bheight",  &Oparams.bheight,            CT},
 #ifndef MD_ASYM_ITENS
   {"ISi",      &Oparams.I[0],      CT},
   {"IO",       &Oparams.I[1],      CT},
