bd91861da4f74c8a760541edb6ca46507a643e12
scala <scala@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-06-22 17:26:56 +0000

    analogous of g(r) for cos^2(theta) and for Y20 Y20
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@22 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- ELAB-DATA-ELLIPSOID/calc_LEN_cristiano.c
+++ ELAB-DATA-ELLIPSOID/calc_LEN_cristiano.c
@@ -17,11 +17,10 @@ main(){
   double hist[Nbins]; // histogram
   double norm[Nbins]; // bin normalization
   double rx,ry,rz,r,dr;
-
-  double histY22[Nbins]; // histogram
-  double Y2i,Y2j,Y22;
   
   double box, unit_length, cosTheta;
+  
+  double corr_len;
   
 #ifndef Npart
 #define Npart 256
@@ -39,9 +38,6 @@ main(){
   scanf("%d",&TotalConfs); // number of configurations
   scanf("%s",nomedir); // directory where configurations are         
   allocate = MY_TRUE;  // first conf must be allocated  
-  
-  /* initialize histograms */
-  for(ibin=0;ibin<Nbins;ibin++) hist[ibin]=norm[ibin]=0.0;
 	      
   for(iConf=0; iConf<(TotalConfs); iConf++){
   	
@@ -51,13 +47,17 @@ main(){
     read_cristiano_ellipses(&conf,allocate);
     if(conf.parnum!=Npart){fprintf(stderr,"cazzo!");exit(1);}
 
-	box = conf.box; dr = (box/2.)/(double)Nbins;
+	box = conf.box; dr = box/(double)Nbins;
 #ifdef SCHILLING 
       unit_length = 2.0*conf.aA;
 #else
       unit_length = 2.0*pow(conf.aA*conf.bA*conf.cA,1./3.);
 #endif
-	
+
+
+  	/* initialize histograms */
+  	for(ibin=0;ibin<Nbins;ibin++) hist[ibin]=norm[ibin]=0.0;
+
 
     for(i=1;i<Npart;i++) for(j=0;j<i;j++){
 
@@ -66,53 +66,36 @@ main(){
 	  	conf.R[i][1] * conf.R[j][1] +
 	  	conf.R[i][2] * conf.R[j][2];
 	  	
-	  	rx = conf.rx[i] - conf.rx[j];
-	  	ry = conf.ry[i] - conf.ry[j];
-	  	rz = conf.rz[i] - conf.rz[j];
+	  	rx = conf.R[i][0] - conf.R[j][0];
+	  	ry = conf.R[i][1] - conf.R[j][1];
+	  	rz = conf.R[i][2] - conf.R[j][2];
 
 		rx=remainder(rx,box);	
 		ry=remainder(ry,box);
 		rz=remainder(rz,box);
-
-		Y22 = 0;
-		for (k = 0; k < 3; ++k) {
-			Y2i=conf.R[i][k];
-			Y2j=conf.R[j][k];
-			Y2i=(3.*Y2i*Y2i-1.)*0.5;
-			Y2j=(3.*Y2j*Y2j-1.)*0.5;
-			Y22 += Y2i*Y2j;
-		}
-		Y22 /=3.;
 	  
 		r=sqrt(rx*rx+ry*ry+rz*rz); ibin=r/dr;
 		if((ibin>0)&&(ibin<Nbins)) {
-			hist[ibin] += cosTheta*cosTheta;
-			histY22[ibin] += Y22; 
+			hist[ibin] += cosTheta*cosTheta; 
 			norm[ibin]++ ;
 		}
 		
 		
       }/* Npart x Npart */
 
+  	for(ibin=0;ibin<Nbins;ibin++) if(norm[ibin]>0.) 
+  		hist[ibin]=(3*hist[ibin]/norm[ibin]-1.)/2.;
+  	hist[0] = 1.0;
+
+	corr_len = 0.;
+	for(ibin=0;ibin<Nbins;ibin++) corr_len += hist[ibin];
+	corr_len *= dr;
+	printf("%lf %lf %s\n",corr_len,unit_length,conf.nomefile);
 	
     allocate = MY_FALSE; 
 
   }/* iConf */
     
-  for(ibin=0;ibin<Nbins;ibin++) 
-  	if(norm[ibin]>0.){
-  		hist[ibin]=(3.*hist[ibin]/norm[ibin]-1.)/2.;
-  		histY22[ibin]/=norm[ibin];
-  	}
-  	else
-  		histY22[ibin]=hist[ibin]=0.; 	
   
-  
-  for(ibin=0;ibin<Nbins;ibin++) { 
-  	r=(ibin+0.5)*dr; 
-    printf("%lf %lf %lf %lf\n",r/unit_length,
-    	hist[ibin],histY22[ibin],norm[ibin]);
-  }
-	
 }
 
