4b20dfac1db0a1793bde1ec27b844882304f184a
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-07-29 22:59:19 +0000

    * comincia a funzionare il codice per il calcolo delle Slml'm'(q,t)
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@100 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- FQT/calcfqt.c
+++ FQT/calcfqt.c
@@ -21,28 +21,29 @@
 #include <string.h>
 
 #define MAXPTS 1000
-#define MAXQ 100
+#define MAXQ 50
 #define NUMQ 100
 char fname[1024]; 
-double time, Cav, rhoR0[MAXQ], rhoI0[MAXQ], cc[3][MAXPTS],C[3][MAXPTS], 
-       rhoR1[MAXQ], rhoI1[MAXQ], ti[MAXPTS], *rhoRt[MAXQ], *rhoIt[MAXQ];
+double time, Cav, rhoR0[MAXQ], rhoI0[MAXQ], cc[MAXQ][MAXPTS],C[MAXQ][MAXPTS], 
+       rhoR1[MAXQ], rhoI1[MAXQ], ti[MAXPTS], *rhoRt[MAXQ], *rhoIt[MAXQ],
+       *rhoRtp[MAXQ], *rhoItp[MAXQ];
 int NQarr[NUMQ];
 int points;
 int main(int argc, char **argv)
 {
   FILE *f, *f2, *f3;
   double A1, A2, A3 ;
-  int NQ, nq, c1, c2, c3, i, ii, nlines, nr1, nr2, ll, mm;
+  int first=1, firstp=1, NQ, nq, c1, c2, c3, i, ii, nlines, nr1, nr2, ll, mm, llp, mmp;
   if (argc <= 1)
     {
-      printf("you must supply a filename!\n");
+      printf("Usage: calcfqt <l> <m> <l'> <m'> [points] \n");
       exit(-1);
     }
-  if (argc == 4)
-    points = atoi(argv[2]);
+  if (argc == 6)
+    points = atoi(argv[5]);
   else
     points = MAXPTS;
-  for (i=0; i < 3; i++)
+  for (i=0; i < MAXQ; i++)
     for (ii=0; ii < MAXPTS; ii++)
       {
 	C[i][ii] = 0.0;
@@ -50,30 +51,63 @@ int main(int argc, char **argv)
       }
   ll = atoi(argv[1]);
   mm = atoi(argv[2]);
+  llp = atoi(argv[3]);
+  mmp = atoi(argv[4]);
   for (ii=0; ii < MAXPTS; ii++)
     ti[ii] = -1.0;
+  c2 = 0;
+  sprintf(fname, "RHOTMP/ro.%02d.k=%03d", 0, 2);
+  f2 = fopen(fname, "r");
+  while (!feof(f2))
+    {
+      fscanf(f2, "%lf %d ", &time, &NQ);
+      printf("time=%f nq=%d\n", time, NQ);
+      for (i = 0; i < NQ; i++)
+	fscanf(f2, "(%lf,%lf) ", &A1, &A2);
+      c2++;
+    }	
+  fprintf(stderr, "allocating %d items\n", c2);
+  for (i=0; i < MAXQ; i++)
+    {
+      rhoRt[i] = malloc(sizeof(double)*c2);
+      rhoIt[i] = malloc(sizeof(double)*c2);
+      rhoRtp[i] = malloc(sizeof(double)*c2);
+      rhoItp[i] = malloc(sizeof(double)*c2);
+    }
+  first = 0;
+  nlines = c2;
+  fclose(f2);
+
   for (nq = 2; nq < NUMQ; nq++)
     {
       fprintf(stderr, "nf=%d\n", nq);
+      sprintf(fname, "RHOTMP/ro.%02d.k=%03d", ll*10+mm,nq);
+      printf("Opening fname=%s\n", fname);
+    
       c2 = 0;
-      sprintf(fname, "RHOTMP/ro.%2d.k=%3d", ll*10+mm,nq);
       f2 = fopen(fname, "r");
       while (!feof(f2))
 	{
+	  //printf("reading c2=%d\n", c2);
 	  fscanf(f2, "%lf %d ", &time, &NQ);
-	  for (i = 0; i < NQ; i++)
-	    fscanf(f2, "(%lf,%lf) ", &A1, &A2);
+	  if (c2==0)
+	    NQarr[nq] = NQ;
+  	  for (i = 0; i < NQ; i++)
+	    {
+	      fscanf(f2, "(%lf,%lf) ", &(rhoRt[i][c2]), &(rhoIt[i][c2]));
+	    }
+	  //printf("dopo\n");
+	  if (c2 < MAXPTS && ti[c2] == -1.0)
+	    {
+	      ti[c2] = time;
+	      //printf("c2=%d time=%.15G\n", c2, ti[c2]);
+	    }
+	  //printf("%d fname: %s %.15G %.15G %.15G\n", c2, fname, P0[0], P0[1], P0[2]);
 	  c2++;
-	}	 
-      fprintf(stderr, "allocating %d items\n", c2);
-      NQarr[nq] = NQ;
-      for (i=0; i < NQ; i++)
-	{
-	  rhoRt[i] = malloc(sizeof(double)*c2);
-	  rhoIt[i] = malloc(sizeof(double)*c2);
 	}
-      nlines = c2;
       fclose(f2);
+      //c2 = 0;
+      sprintf(fname, "RHOTMP/ro.%02d.k=%03d", llp*10+mmp,nq);
     
       c2 = 0;
       f2 = fopen(fname, "r");
@@ -82,13 +116,16 @@ int main(int argc, char **argv)
 	  //printf("reading c2=%d\n", c2);
 	  fscanf(f2, "%lf %d ", &time, &NQ);
   	  for (i = 0; i < NQ; i++)
-	    fscanf(f2, "(%lf,%lf) ", &rhoRt[c2][i], &rhoIt[c2][i]);
-	  //printf("dopo\n");
-	  if (c2 < MAXPTS && ti[c2] == -1.0)
 	    {
-	      ti[c2] = time;
-	      //printf("c2=%d time=%.15G\n", c2, ti[c2]);
+	      fscanf(f2, "(%lf,%lf) ", &(rhoRtp[i][c2]), &(rhoItp[i][c2]));
+	      //printf("NQ=%d nq=%d QUI i=%d c2=%d\n",NQ, nq, i, c2);
 	    }
+	  //printf("dopo\n");
+	  //if (c2 < MAXPTS && ti[c2] == -1.0)
+	   // {
+	    //  ti[c2] = time;
+	      //printf("c2=%d time=%.15G\n", c2, ti[c2]);
+	   // }
 	  //printf("%d fname: %s %.15G %.15G %.15G\n", c2, fname, P0[0], P0[1], P0[2]);
 	  c2++;
 	}
@@ -104,33 +141,39 @@ int main(int argc, char **argv)
 	    {
 	      for (i=0; i < NQ; i++) 
 		{
-		  rhoR1[i] = rhoRt[i][nr2];
-		  rhoI1[i] = rhoIt[i][nr2];
+		  //printf("i=%d NQ=%d rhoRtp=%f rhoItp=%f\n", i, NQ, rhoRtp[i][nr2], rhoItp[i][nr2]);
+		  rhoR1[i] = rhoRtp[i][nr2];
+		  rhoI1[i] = rhoItp[i][nr2];
 		  C[i][nr2-nr1] += rhoR1[i]*rhoR0[i]+rhoI1[i]*rhoI0[i];
 		  cc[i][nr2-nr1] += 1.0;
 		  //printf("qui c3-c2=%d\n", c3-c2);
 		}
 	    }
 	}
-      for (i=0; i < NQ; i++)
-	{
-	  free(rhoIt[i]);
-	  free(rhoRt[i]);
-	}
-	  
     }
+  for (i=0; i < MAXQ; i++)
+    {
+      free(rhoIt[i]);
+      free(rhoRt[i]);
+      free(rhoItp[i]);
+      free(rhoRtp[i]);
+    }
+	
   for (nq = 0; nq < NUMQ; nq++)
     {
-      sprintf(fname, "sqt.%2d.k=%3d",ll*10+mm,nq);
+      sprintf(fname, "sqt.%02d.k=%03d",ll*10+mm,nq);
       f = fopen(fname, "w+");
       for (ii=1; ii < points; ii++)
 	{
 	  Cav = 0.0;
 	  for (i = 0; i < NQarr[nq]; i++)
-	    if (cc[i][ii] > 0)
-	  Cav += C[i][ii]/cc[i][ii];
+	    {
+	      if (cc[i][ii] > 0)
+		Cav += C[i][ii]/cc[i][ii];
+	    }
+	  Cav /= ((double)NQarr[nq]);
 	  if (ti[ii] > -1.0)
-	    printf("%.15G %.15G %f\n", ti[ii]-ti[0], Cav, cc[0][ii]);
+	    fprintf(f, "%.15G %.15G %f\n", ti[ii]-ti[0], Cav, cc[0][ii]);
 	}
       fclose(f);
     }
