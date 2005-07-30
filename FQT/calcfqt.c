274601b95c887288fb51eeb1bd1a70b4c6ba4322
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-07-30 02:32:58 +0000

    * ora calcfqt tiene conto del salvataggio logaritmico.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@103 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- FQT/calcfqt.c
+++ FQT/calcfqt.c
@@ -34,21 +34,18 @@ int main(int argc, char **argv)
   FILE *f, *f2, *f3;
   double A1, A2, A3 ;
   int first=1, firstp=1, NQ, nq, c1, c2, c3, i, ii, nlines, nr1, nr2, ll, mm, llp, mmp;
+  int NN, fine, JJ;
   if (argc <= 1)
     {
-      printf("Usage: calcfqt <l> <m> <l'> <m'> [points] \n");
+      printf("Usage: calcfqt <l> <m> <l'> <m'> <NN> [points] \n");
+      printf("where NN is the number of configurations in a logarithmic block\n");
       exit(-1);
     }
-  if (argc == 6)
-    points = atoi(argv[5]);
+  NN =  atoi(argv[5]);
+  if (argc == 7)
+    points = atoi(argv[6]);
   else
-    points = MAXPTS;
-  for (i=0; i < MAXQ; i++)
-    for (ii=0; ii < MAXPTS; ii++)
-      {
-	C[i][ii] = 0.0;
-	cc[i][ii] = 0;
-      }
+    points = NN;
   ll = atoi(argv[1]);
   mm = atoi(argv[2]);
   llp = atoi(argv[3]);
@@ -80,10 +77,15 @@ int main(int argc, char **argv)
 
   for (nq = 2; nq < NUMQ; nq++)
     {
+      for (i=0; i < MAXQ; i++)
+	for (ii=0; ii < MAXPTS; ii++)
+	  {
+	    C[i][ii] = 0.0;
+	    cc[i][ii] = 0;
+	  }
       fprintf(stderr, "nf=%d\n", nq);
       sprintf(fname, "RHOTMP/ro.%02d.k=%03d", ll*10+mm,nq);
       printf("Opening fname=%s\n", fname);
-    
       c2 = 0;
       f2 = fopen(fname, "r");
       while (!feof(f2))
@@ -96,7 +98,6 @@ int main(int argc, char **argv)
 	    {
 	      fscanf(f2, "(%lf,%lf) ", &(rhoRt[i][c2]), &(rhoIt[i][c2]));
 	    }
-	  //printf("dopo\n");
 	  if (c2 < MAXPTS && ti[c2] == -1.0)
 	    {
 	      ti[c2] = time;
@@ -118,49 +119,42 @@ int main(int argc, char **argv)
   	  for (i = 0; i < NQ; i++)
 	    {
 	      fscanf(f2, "(%lf,%lf) ", &(rhoRtp[i][c2]), &(rhoItp[i][c2]));
-	      //printf("NQ=%d nq=%d QUI i=%d c2=%d\n",NQ, nq, i, c2);
+	     //printf("NQ=%d nq=%d QUI i=%d c2=%d rhoRtp: %.15G  rhoItp: %.15G \n",NQ, nq, i, c2,
+	     //	     rhoRtp[i][c2], rhoItp[i][c2]);
 	    }
-	  //printf("dopo\n");
-	  //if (c2 < MAXPTS && ti[c2] == -1.0)
-	   // {
-	    //  ti[c2] = time;
-	      //printf("c2=%d time=%.15G\n", c2, ti[c2]);
-	   // }
-	  //printf("%d fname: %s %.15G %.15G %.15G\n", c2, fname, P0[0], P0[1], P0[2]);
-	  c2++;
+  	  c2++;
 	}
       fclose(f2);
-      for (nr1 = 0; nr1 < nlines; nr1++)
+      for (nr1 = 0; nr1 < nlines; nr1=nr1+NN)
 	{	
 	  for (i=0; i < NQ; i++)
 	    {
 	      rhoR0[i] = rhoRt[i][nr1];
 	      rhoI0[i] = rhoIt[i][nr1]; 
 	    }
-	  for (nr2 = nr1; nr2-nr1 < points && nr2 < nlines; nr2++)
+	  fine = 0;
+	  for (JJ = 0; fine == 0; JJ++)
 	    {
-	      for (i=0; i < NQ; i++) 
+	      for (nr2 = nr1 + JJ*NN; nr2-nr1-JJ*NN < NN; nr2++)
 		{
-		  //printf("i=%d NQ=%d rhoRtp=%f rhoItp=%f\n", i, NQ, rhoRtp[i][nr2], rhoItp[i][nr2]);
-		  rhoR1[i] = rhoRtp[i][nr2];
-		  rhoI1[i] = rhoItp[i][nr2];
-		  C[i][nr2-nr1] += rhoR1[i]*rhoR0[i]+rhoI1[i]*rhoI0[i];
-		  cc[i][nr2-nr1] += 1.0;
-		  //printf("qui c3-c2=%d\n", c3-c2);
+		  if (nr2 == nlines || nr2 - nr1 >= points)
+		    {
+		      fine = 1;
+		      break;
+		    }
+		  for (i=0; i < NQ; i++) 
+		    {
+		      //printf("i=%d NQ=%d rhoRtp=%f rhoItp=%f\n", i, NQ, rhoRtp[i][nr2], rhoItp[i][nr2]);
+		      rhoR1[i] = rhoRtp[i][nr2];
+		      rhoI1[i] = rhoItp[i][nr2];
+		      C[i][nr2-nr1] += rhoR1[i]*rhoR0[i]+rhoI1[i]*rhoI0[i];
+		      //printf("C[%d][%d]: %.15G cc:%f\n", i, nr2-nr1, C[i][nr2-nr1], cc[i][nr2-nr1]);
+		      cc[i][nr2-nr1] += 1.0;
+		      //printf("qui c3-c2=%d\n", c3-c2);
+		    }
 		}
 	    }
 	}
-    }
-  for (i=0; i < MAXQ; i++)
-    {
-      free(rhoIt[i]);
-      free(rhoRt[i]);
-      free(rhoItp[i]);
-      free(rhoRtp[i]);
-    }
-	
-  for (nq = 2; nq < NUMQ; nq++)
-    {
       sprintf(fname, "sqt.%02d.k=%03d",ll*10+mm,nq);
       f = fopen(fname, "w+");
       for (ii=1; ii < points; ii++)
@@ -177,5 +171,13 @@ int main(int argc, char **argv)
 	}
       fclose(f);
     }
+  for (i=0; i < MAXQ; i++)
+    {
+      free(rhoIt[i]);
+      free(rhoRt[i]);
+      free(rhoItp[i]);
+      free(rhoRtp[i]);
+    }
+	
   return 0;
 }
--- FQT/converti.sh
+++ FQT/converti.sh
@@ -1,7 +1,8 @@
+gunzip Store*gz
 ls Store* > junk
 cat junk | while read nomefile
 do
 echo  $nomefile
 awk -F":" 'BEGIN {cc=0} {if (cc>0) print $0; if ($1=="@@@") cc=cc+1; }' $nomefile > Cnf$nomefile
 done
-
+gzip Store*
