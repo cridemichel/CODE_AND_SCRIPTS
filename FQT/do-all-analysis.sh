cbe27ddc8feeaf915e376e90a29a9a7dbdaef9be
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-07-31 00:28:00 +0000

    * script che fa tutte le misure.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@120 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ FQT/do-all-analysis.sh
@@ -0,0 +1,62 @@
+for f in Phi*
+do
+cd $f
+PR=$HOME/ELLIPSOIDS/MSD/calcmsd
+if [ "$1" == "" ]
+then
+NN=`cat Store-0-0.gz | gunzip -c | awk -F : '{if ($1=="NN") print $2}'` 
+else
+NN=$1
+fi
+NN=`echo "$NN*5"| bc`
+gunzip Store*gz
+ls Store* | sort -t - -k 2 -k 3 -n > listamsd
+$PR listamsd $NN 
+#gzip Store*
+#gunzip Store*gz
+ls Store* > junk
+cat junk | while read nomefile
+do
+echo  $nomefile
+awk -F":" 'BEGIN {cc=0} {if (cc>0) print $0; if ($1=="@@@") cc=cc+1; }' $nomefile > Cnf$nomefile
+done
+gzip Store*
+PERC=$HOME/ELLIPSOIDS/FQT/
+ls Cnf* | sort -t - -k 2 -k 3 -n > listaconf
+q=2
+while [ $q -lt 100 ]
+do
+$PERC/rho-dip << !
+$q
+!
+q=$[$q+1]
+done
+rm -f CnfStore* 
+$PERC/sq-statico-1k << !
+2
+99
+!
+PR=$HOME/ELLIPSOIDS/FQT/calcfqt
+if [ "$1" == "" ]
+then
+NN=`cat Store-0-0.gz | gunzip -c | awk -F : '{if ($1=="NN") print $2}'` 
+else
+NN=$1
+fi
+echo "NN"=$NN
+$PR 0  0  0  0 $NN 
+$PR 2  0  2  0 $NN
+$PR 0  0  1  0 $NN
+$PR 0  0  2  0 $NN 
+$PR 1 -1  1 -1 $NN 
+$PR 1 -1  2 -1 $NN
+$PR 1  0  1  0 $NN
+$PR 1  0  2  0 $NN
+$PR 1  1  1  1 $NN 
+$PR 2  1  1  1 $NN
+$PR 2 -2  2 -2 $NN
+$PR 2  1  2  1 $NN
+$PR 2  2  2  2 $NN
+rm -f RHOTMP/ro.*
+cd ..
+done
