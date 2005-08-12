a17b796187cd7562f2ca0ec58d308eaf3d35f441
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-08-12 13:18:09 +0000

    * script che crea un link simbolico allo script per l'analisi e ridireziona stdout e stderr su di un file.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@135 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ FQT/do-analysis-by-link.sh
@@ -0,0 +1,12 @@
+SCR=../FQT/do-all-analysis.sh
+if [ "$1" == "" ]
+then
+CDIR=`pwd`
+DIR=`basename $CDIR`
+EL=`echo $DIR | awk -F _ '{print $2}'`
+else
+EL=$1
+fi
+SCRLNK=do-analysis-X0_${EL}.sh
+ln -sf $SCR $SCRLNK
+$SCRLNK > screen_$SCRLNK 2>&1
