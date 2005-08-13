7eb1cd04ccf0cc9d4ec53b39c2456fe1a03abacd
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-08-13 14:55:29 +0000

    * piccolo bug fissato
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@138 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MSD/calcmsd.sh
+++ MSD/calcmsd.sh
@@ -1,12 +1,12 @@
 PR=$HOME/ELLIPSOIDS/MSD/calcmsd
-gunzip Store*gz
 if [ "$1" == "" ]
 then
 NN=`cat Store-0-0.gz | gunzip -c | awk -F : '{if ($1=="NN") print $2}'` 
 else
 NN=$1
 fi
-NN=`echo "$NN*5"| bc`
+gunzip Store*gz
+NN=`echo "$NN*30"| bc`
 ls Store* | sort -t - -k 2 -k 3 -n > listamsd
 $PR listamsd $NN 
 gzip Store*
