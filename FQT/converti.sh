b84ffef09a733a823badfef15a28ad31b03cfb5a
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-11-01 14:03:21 +0000

    * ora tutti gli script supportano correttamente il big dt.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@319 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- FQT/converti.sh
+++ FQT/converti.sh
@@ -4,7 +4,6 @@ cat junk | while read nomefile
 do
 echo  $nomefile
 REFTIME=`awk -F":" 'BEGIN {found=0} {if ($1=="refTime") { print $2; found=1}} END {if (!found) printf("0.0");}' $nomefile`
-echo "reftime:" $REFTIME
 awk -v reft="$REFTIME" -F":" 'BEGIN {cc=0} {if (cc>0) {if ($1=="time") printf("time: %.15G\n", reft+$2); else print $0}; if ($1=="@@@") cc=cc+1; }' $nomefile > Cnf$nomefile
 #awk -F":" 'BEGIN {cc=0} {if (cc>0) print $0; if ($1=="@@@") cc=cc+1; }' $nomefile > Cnf$nomefile
 done
--- FQT/do-one-fqt.sh
+++ FQT/do-one-fqt.sh
@@ -17,7 +17,9 @@ ls Store* > junk
 cat junk | while read nomefile
 do
 echo  $nomefile
-awk -F":" 'BEGIN {cc=0} {if (cc>0) print $0; if ($1=="@@@") cc=cc+1; }' $nomefile > Cnf$nomefile
+REFTIME=`awk -F":" 'BEGIN {found=0} {if ($1=="refTime") { print $2; found=1}} END {if (!found) printf("0.0");}' $nomefile`
+awk -v reft="$REFTIME" -F":" 'BEGIN {cc=0} {if (cc>0) {if ($1=="time") printf("time: %.15G\n", reft+$2); else print $0}; if ($1=="@@@") cc=cc+1; }' $nomefile > Cnf$nomefile
+#awk -F":" 'BEGIN {cc=0} {if (cc>0) print $0; if ($1=="@@@") cc=cc+1; }' $nomefile > Cnf$nomefile
 done
 gzip Store*
 PERC=$HOME/ELLIPSOIDS/FQT/
