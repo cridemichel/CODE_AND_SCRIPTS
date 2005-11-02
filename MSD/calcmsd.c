cb9ed8e51f15963600654baa6176f85082a16f58
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2005-11-02 14:30:19 +0000

    * ora l'allocazione è dinamica.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@326 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MSD/calcmsd.c
+++ MSD/calcmsd.c
@@ -4,15 +4,15 @@
 #include <math.h>
 #define MAXPTS 10000
 #define MAXFILES 5000
-char fname[MAXFILES][256]; 
-double L, time, ti[MAXPTS], rotMSD[MAXPTS], MSD[MAXPTS], rotMSDA[MAXPTS], MSDA[MAXPTS], rotMSDB[MAXPTS], MSDB[MAXPTS], cc[MAXPTS];
-double DR[MAXPTS][3], DR0[MAXPTS][3];
+char **fname; 
+double L, time, *ti, *rotMSD, *MSD, *rotMSDA, *MSDA, *rotMSDB, *MSDB, *cc;
+double **DR, **DR0;
 double *r0[3], *w0[3], *rt[3], *wt[3], *rtold[3];
 char parname[128], parval[256000], line[256000];
-char dummy[1024];
-int points, foundDRs=0;
+char dummy[2048];
+int points, foundDRs=0, foundrot=0;
 
-void readconf(char *fname, double *ti, double *refTime, int NP, double *r[3], double *w[3], double DR[MAXPTS][3])
+void readconf(char *fname, double *ti, double *refTime, int NP, double *r[3], double *w[3], double **DR)
 {
   FILE *f;
   int nat=0, i, cpos;
@@ -45,6 +45,7 @@ void readconf(char *fname, double *ti, double *refTime, int NP, double *r[3], do
 		{
 		  fscanf(f, " %lf ", &w[0][i]); 
 		}
+	      foundrot = 1;
 	    }
 	  else if (!strcmp(parname,"sumoy"))
 	    {
@@ -92,7 +93,7 @@ int main(int argc, char **argv)
   FILE *f, *f2, *f3, *fA, *fB, *f2A, *f2B;
   double *adjDr[3], Dr, Dw, A1, A2, A3, dr;
   int c1, c2, c3, i, nfiles, nf, ii, nlines, nr1, nr2, a;
-  int NP, NPA=-1, NN, fine, JJ, nat;
+  int NP, NPA=-1, NN, fine, JJ, nat, maxl;
   double refTime;
   if (argc <= 1)
     {
@@ -100,27 +101,25 @@ int main(int argc, char **argv)
       //printf("where NN il the lenght of the logarithmic block\n");
       exit(-1);
     }
-  for (ii=0; ii < MAXPTS; ii++)
-    {
-      ti[ii] = -1.0;
-      rotMSD[ii] = 0.0;
-      MSD[ii] = 0.0;
-      rotMSDA[ii] = 0.0;
-      MSDA[ii] = 0.0;
-      rotMSDB[ii] = 0.0;
-      MSDB[ii] = 0.0;
-      cc[ii]=0.0;
-    }
   f2 = fopen(argv[1], "r");
   c2 = 0;
+  maxl = 0;
   while (!feof(f2))
     {
-      fscanf(f2, "%[^\n]\n", fname[c2]);
+      fscanf(f2, "%[^\n]\n", dummy); 
+      if (strlen(dummy) > maxl)
+	maxl = strlen(dummy)+1;
       c2++;
     }	
   nfiles = c2;
+  rewind(f2);
+  fname = malloc(sizeof(char*)*nfiles);
+  for (ii=0; ii < nfiles; ii++)
+    {
+      fname[ii] = malloc(sizeof(char)*maxl);
+      fscanf(f2, "%[^\n]\n", fname[ii]); 
+    }
   fclose(f2);
-
   f = fopen(fname[0], "r");
   nat = 0;
   while (!feof(f) && nat < 2) 
@@ -153,8 +152,39 @@ int main(int argc, char **argv)
     points=atoi(argv[2]);
   else
     points=NN;
- 
-  printf("points=%d files=%d NP = %d L=%.15G NN=%d\n", points, nfiles, NP, L, NN);
+  if (points > nfiles)
+    points = nfiles;
+  ti = malloc(sizeof(double)*points);
+  rotMSD = malloc(sizeof(double)*points);
+  MSD = malloc(sizeof(double)*points);
+  MSDA = malloc(sizeof(double)*points);
+  MSDB = malloc(sizeof(double)*points);
+  rotMSDA = malloc(sizeof(double)*points);
+  rotMSDB = malloc(sizeof(double)*points);
+  cc = malloc(sizeof(double)*points);
+  DR = malloc(sizeof(double*)*NP);
+  DR0= malloc(sizeof(double*)*NP);
+  for (ii = 0; ii < NP; ii++)
+    {
+      DR[ii]  = malloc(sizeof(double)*3);
+      DR0[ii] = malloc(sizeof(double)*3);
+    }
+  for (ii=0; ii < points; ii++)
+    {
+      ti[ii] = -1.0;
+      rotMSD[ii] = 0.0;
+      MSD[ii] = 0.0;
+      rotMSDA[ii] = 0.0;
+      MSDA[ii] = 0.0;
+      rotMSDB[ii] = 0.0;
+      MSDB[ii] = 0.0;
+      cc[ii]=0.0;
+    }
+    
+  if (NPA != NP)
+    printf("[MIXTURE] points=%d files=%d NP = %d NPA=%d L=%.15G NN=%d maxl=%d\n", points, nfiles, NP, NPA, L, NN, maxl);
+  else
+    printf("[MONODISPERE] points=%d files=%d NP = %d L=%.15G NN=%d maxl=%d\n", points, nfiles, NP, L, NN, maxl);
   for (a=0; a < 3; a++)
     {
       r0[a] = malloc(sizeof(double)*NP);
@@ -195,7 +225,7 @@ int main(int argc, char **argv)
 		      rtold[a][i] = rt[a][i];
 		}
 	      readconf(fname[nr2], &time, &refTime, NP, rt, wt, DR);
-	      if (nr2 < MAXPTS && ti[nr2] == -1.0)
+	      if (nr2 < points && ti[nr2] == -1.0)
 		{
 		  ti[nr2] = time + refTime;
 		  //printf("nr1=%d time=%.15G\n", nr2, ti[nr2]);
@@ -225,18 +255,21 @@ int main(int argc, char **argv)
 		    //printf("adjDr[%d][%d]:%f\n", a, i, adjDr[a][i]);
 		    
 		    MSD[nr2-nr1] += (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
-		    rotMSD[nr2-nr1] += Dw*Dw;
+		    if (foundrot)
+		      rotMSD[nr2-nr1] += Dw*Dw;
 		    if (NP != NPA)
 		      {
 			if (i < NPA)
 			  {
 			    MSDA[nr2-nr1] += (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
-			    rotMSDA[nr2-nr1] += Dw*Dw;
+			    if (foundrot)
+			      rotMSDA[nr2-nr1] += Dw*Dw;
 			  }
 			else
 			  {
 		    	    MSDB[nr2-nr1] += (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
-	    		    rotMSDB[nr2-nr1] += Dw*Dw;
+	    		    if (foundrot)
+			      rotMSDB[nr2-nr1] += Dw*Dw;
 			  }
 		      }
 		  }
@@ -246,13 +279,16 @@ int main(int argc, char **argv)
 	}
     }
   f = fopen("MSDcnf.dat", "w+");
-  f2 = fopen("rotMSDcnf.dat", "w+");
+  if (foundrot)
+    f2 = fopen("rotMSDcnf.dat", "w+");
   if (NP != NPA)
     {
       fA = fopen("MSDAcnf.dat", "w+");
-      f2A = fopen("rotMSDAcnf.dat", "w+");
+      if (foundrot)
+	f2A = fopen("rotMSDAcnf.dat", "w+");
       fB = fopen("MSDBcnf.dat", "w+");
-      f2B = fopen("rotMSDBcnf.dat", "w+");
+      if (foundrot)
+	f2B = fopen("rotMSDBcnf.dat", "w+");
     }
   for (ii=1; ii < points; ii++)
     {
@@ -260,24 +296,30 @@ int main(int argc, char **argv)
       if (cc[ii] > 0 && ti[ii] > -1.0)
 	{
 	  fprintf(f, "%.15G %.15G %f\n", ti[ii]-ti[0], MSD[ii]/cc[ii]/((double)NP), cc[ii]);
-	  fprintf(f2, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSD[ii]/cc[ii]/((double)NP), cc[ii]);
+	  if (foundrot)
+	    fprintf(f2, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSD[ii]/cc[ii]/((double)NP), cc[ii]);
 	}
       if (NP != NPA)
 	{
 	  fprintf(fA, "%.15G %.15G %f\n", ti[ii]-ti[0], MSDA[ii]/cc[ii]/((double)NPA), cc[ii]);
-	  fprintf(f2A, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSD[ii]/cc[ii]/((double)NPA), cc[ii]);
+	  if (foundrot)
+	    fprintf(f2A, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSD[ii]/cc[ii]/((double)NPA), cc[ii]);
 	  fprintf(fB, "%.15G %.15G %f\n", ti[ii]-ti[0], MSDB[ii]/cc[ii]/((double)NP-NPA), cc[ii]);
-	  fprintf(f2B, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSDB[ii]/cc[ii]/((double)NP-NPA), cc[ii]);
+	  if (foundrot)
+	    fprintf(f2B, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSDB[ii]/cc[ii]/((double)NP-NPA), cc[ii]);
 	}
     }
   fclose(f);
-  fclose(f2);
+  if (foundrot)
+    fclose(f2);
   if (NP != NPA)
     {
       fclose(fA);
-      fclose(f2A);
+      if (foundrot)
+	fclose(f2A);
       fclose(fB);
-      fclose(f2B);
+      if (foundrot)
+	fclose(f2B);
     }
   return 0;
 }
