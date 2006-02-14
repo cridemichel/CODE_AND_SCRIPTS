8419b5d7ed8ec8689bab427ec92670cc062324d8
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2006-02-14 13:00:59 +0000

    * script per il calcolo del tau da funzioni di correlazioni con fit stretched.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@399 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ SCRIPTS/calcallTau.sh
@@ -0,0 +1,50 @@
+PERC=$HOME/hardellipsoid/hardellSVN/CODE/FQT/
+if [ "$1" == "" ]
+then
+CDIR=`pwd`
+DIR=`basename $CDIR`
+EL=`echo $DIR | awk -F _ '{print $2}'`
+else
+EL=$1
+fi
+FNT="allTAUtra-X0_${EL}.dat"
+FNR="allTAUrot-X0_${EL}.dat"
+echo -n "" > $FNT
+echo -n "" > $FNR
+for f in Phi* 
+do
+cd $f
+PHI=`echo $f | awk -F Phi '{print $2}'`
+if [ ! -e Fqs-10.dat ]
+then
+echo "Phi=" $PHI " The file Fqs-10 does not exist, skipping..."
+cd ..
+continue
+fi
+if [ ! -e Cn.dat ]
+then
+echo "Phi=" $PHI " The file Cn.dat does not exist, skipping..."
+cd ..
+continue
+fi
+echo "Processing Phi=" $PHI
+echo "Find Maximum of S(q)..."
+MAXQ=`$PERC/findmax Sq.dat 12`
+#STA=`tail -n 50 screen_ell${EL}EQ${PHI} | awk '{if ($1=="[MSDcheck]") print $5}'` 
+ST=0.1
+echo "a=1.0; b=0.1; c=1.0; fit [$ST:] a*exp(-((x/b)**c)) 'Fq-$MAXQ.dat' via a,b,c" > fit.tmp
+gnuplot fit.tmp > gpout.tmp 2>&1  
+B=`cat gpout.tmp | awk 'BEGIN {pr=0} {if (pr==1 && $1=="b") {print $3; pr++;}; if ($1=="Final") pr=1;}'`
+C=`cat gpout.tmp | awk 'BEGIN {pr=0} {if (pr==1 && $1=="c") {print $3; pr++;}; if ($1=="Final") pr=1;}'`
+TAUM=`echo "gamma($C)*$B/$C" | octave | awk '{if ($1=="ans") print $3}'`
+echo $EL $PHI $TAUM >> ../$FNT
+echo "a=1.0; b=0.1; c=1.0; fit [$ST:] a*exp(-((x/b)**c)) 'Cn.dat' using 1:2 via a,b,c" > fit.tmp
+gnuplot fit.tmp > gpout.tmp 2>&1
+B=`cat gpout.tmp | awk 'BEGIN {pr=0} {if (pr==1 && $1=="b") {print $3; pr++;}; if ($1=="Final") pr=1;}'`
+C=`cat gpout.tmp | awk 'BEGIN {pr=0} {if (pr==1 && $1=="c") {print $3; pr++;}; if ($1=="Final") pr=1;}'`
+TAUM=`echo "gamma($C)*$B/$C" | octave | awk '{if ($1=="ans") print $3}'`
+rm gpout.tmp
+echo $EL $PHI $TAUM >> ../$FNR
+rm fit.tmp
+cd ..
+done
