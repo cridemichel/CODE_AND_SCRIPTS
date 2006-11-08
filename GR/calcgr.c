6a662f3d00870dea472f9c282a7b6d495ec70576
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2006-11-08 13:58:01 +0000

    * implementate le linked lists in calcgr.c
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@583 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- GR/calcgr.c
+++ GR/calcgr.c
@@ -5,10 +5,12 @@
 #define Sqr(x) ((x)*(x))
 char line[100000], parname[124], parval[256000];
 char dummy[2048];
-int N;
-double *x[3], L, ti, *w[3];
-double **DR;
+int N, particles_type=1;
+double *x[3], L, ti, *w[3], storerate;
+double **DR, deltaAA=-1.0, deltaBB=-1.0, deltaAB=-1.0, sigmaAA=-1.0, sigmaAB=-1.0, sigmaBB=-1.0, 
+       Dr, theta, sigmaSticky=-1.0, sa[2], sb[2], sc[2], maxax0, maxax1, maxax, maxsax, maxsaxAA, maxsaxAB, maxsaxBB;
 char fname[1024], inputfile[1024];
+int eventDriven=0;
 int points;
 int foundDRs=0, foundrot=0;
 void readconf(char *fname, double *ti, double *refTime, int NP, double *r[3], double *w[3], double **DR)
@@ -134,12 +136,40 @@ void parse_param(int argc, char** argv)
     points=100;
 }
 double pi;
+int *inCell[3]={NULL,NULL,NULL}, *cellList=NULL, cellsx, cellsy, cellsz;
+int NP, NPA;
+void build_linked_list(void)
+{
+  double L2;
+  int j, n;
+  L2 = 0.5 * L;
+
+  for (j = 0; j < cellsx*cellsy*cellsz + NP; j++)
+    cellList[j] = -1;
+
+  for (n = 0; n < NP; n++)
+    {
+      inCell[0][n] =  (x[0][n] + L2) * cellsx / L;
+      inCell[1][n] =  (x[1][n] + L2) * cellsy / L;
+      inCell[2][n] =  (x[2][n] + L2) * cellsz / L;
+      j = (inCell[2][n]*cellsy + inCell[1][n])*cellsx + 
+	inCell[0][n] + NP;
+      cellList[n] = cellList[j];
+      cellList[j] = n;
+    }
+}
+
+
 int main(int argc, char** argv)
 {
   FILE *f, *f2;
-  int nf, i, a, b, nat, NPA, NP, NN, j, ii, *g0, bin;
+  int nf, i, a, b, nat, NN, j, ii, *g0, bin;
   double r, delr, tref=0.0, Dx[3], g0m, distSq, rlower, rupper, cost, nIdeal;
-  double time, refTime;
+  double time, refTime, RCUT;
+  int iZ, jZ, iX, jX, iY, jY, NP1, NP2;
+  double shift[3];
+  double ene=0.0;
+
 #if 0
   double g2m, g4m, g6m;
   double *g2, *g4, *g6;
@@ -167,20 +197,134 @@ int main(int argc, char** argv)
 	}
       sscanf(line, "%[^:]:%[^\n]\n", parname, parval); 
       if (!strcmp(parname,"parnum"))
-	NP = atoi(parval);
-      if (!strcmp(parname,"parnumA"))
+	{
+	  if (sscanf(parval, "%d %d ", &NP1, &NP2) < 2)
+	    {
+	      NP = atoi(parval);
+	    }
+	  else
+	    {
+	      NP = NP1+NP2;
+	      NPA = NP1;
+	    }
+	}
+      else if (!strcmp(parname,"parnumA"))
 	NPA = atoi(parval);
-      if (!strcmp(parname,"NN"))
+      else if (nat == 0 && !strcmp(parname,"NN"))
 	NN = atoi(parval);
+      else if (!strcmp(parname,"storerate"))
+	{
+	  storerate = atof(parval);
+	  eventDriven = 1;
+	}
+      else if (nat==1 && !strcmp(parname,"a"))
+	sscanf(parval, "%lf %lf\n", &sa[0], &sa[1]);	
+      else if (nat==1 && !strcmp(parname,"b"))
+	sscanf(parval, "%lf %lf\n", &sb[0], &sb[1]);	
+      else if (nat==1 && !strcmp(parname,"c"))
+	sscanf(parval, "%lf %lf\n", &sc[0], &sc[1]);	
+      else if (nat==1 && !strcmp(parname,"sigma"))
+	sscanf(parval, "%lf %lf %lf %lf\n", &sigmaAA, &sigmaAB, &sigmaAB, &sigmaBB);	
+      else if (nat==1 && !strcmp(parname,"delta"))
+	sscanf(parval, "%lf %lf %lf %lf\n", &deltaAA, &deltaAB, &deltaAB, &deltaBB);	
+      else if (nat==1 && !strcmp(parname,"sigmaSticky"))
+	sigmaSticky = atof(parval);
+      else if (nat==1 && !strcmp(parname,"theta"))
+	theta = atof(parval);
+      else if (nat==1 && !strcmp(parname,"Dr"))
+	Dr = atof(parval);
     }
   fclose(f);
   if (NPA == -1)
     NPA = NP;
+  if (eventDriven)
+    printf("[ED] Event-Driven simulation\n");
+  else
+    printf("[MD] Time-Driven simulation\n");
+ 
+  if (NP==NPA)
+    {
+      printf("[MONODISPERSE] NP=%d\n", NP);
+    }
+  else
+    {
+      printf("[MIXTURE] NP=%d NPA=%d\n", NP, NPA);
+    }
+
   for (a = 0; a < 3; a++)
     {
       x[a] = malloc(sizeof(double)*NP);
       w[a] = malloc(sizeof(double)*NP);
     }
+
+  if (!eventDriven)
+    L = cbrt(L);
+  if (deltaAA!=-1)
+    particles_type = 2; /* 2 means square well system*/
+  else if (sigmaAA != -1.0)
+    particles_type = 0;
+  
+
+   if (particles_type == 1)
+    {
+      maxax0 = sa[0];
+      if (sb[0] > maxax0)
+	maxax0 = sb[0];
+      if (sc[0] > maxax0)
+	maxax0 = sc[0];
+      maxax1 = sa[1];
+      if (sb[1] > maxax1)
+	maxax1 = sb[1];
+      if (sc[0] > maxax1)
+	maxax1 = sc[1];
+      maxsax = fabs(maxax1)+fabs(maxax0)+2.0*sigmaSticky;
+      //printf("maxsax=%.15G\n", maxsax);
+    }
+  else if (particles_type == 0)
+    {
+      maxsaxAA = fabs(sigmaAA)+2.0*sigmaSticky;
+      maxsaxAB = fabs(sigmaAB)+2.0*sigmaSticky;
+      maxsaxBB = fabs(sigmaBB)+2.0*sigmaSticky;
+    }
+  else if (particles_type == 2)
+    {
+      maxsaxAA = sigmaAA + deltaAA;
+      maxsaxAB = sigmaAB + deltaAB;
+      maxsaxBB = sigmaBB + deltaBB;
+      maxsax = maxsaxAA;
+      if (maxsaxAB > maxsax)
+	maxsax = maxsaxAB;
+      if (maxsaxBB > maxsax)
+	maxsax = maxsaxBB;
+    }
+  /* WARNING: se i diametri sono diversi va cambiato qua!! */ 
+  if (particles_type == 1)
+    RCUT = maxsax;
+  else if (particles_type == 0)
+    RCUT = maxsaxAA*1.01;
+  else if (particles_type == 2)
+    RCUT = maxsax*1.01;
+  if (particles_type==1)
+    {
+      printf("SYSTEM: ELLISPOIDS - DGEBA\n");
+    }
+  else if (particles_type==0)
+    {
+      printf("SYSTEM: SPHERES 3-2\n");
+    }
+  else if (particles_type == 2)
+    {
+      printf("SYSTEM: SQUARE WELL\n");
+    }
+
+  cellsx = L / RCUT;
+  cellsy = L / RCUT;
+  cellsz = L / RCUT;
+  cellList = malloc(sizeof(int)*(cellsx*cellsy*cellsz+NP));
+  inCell[0] = malloc(sizeof(int)*NP);
+  inCell[1] = malloc(sizeof(int)*NP);
+  inCell[2] = malloc(sizeof(int)*NP);
+  printf("L=%.15G RCUT=%.15G \n", L, RCUT);
   g0 = malloc(sizeof(double)*points);
   DR = malloc(sizeof(double*)*NP);
   for (ii = 0; ii < NP; ii++)
@@ -201,30 +345,82 @@ int main(int argc, char** argv)
       //printf("fname=%s argv[2]=%s\n",fname, argv[2]);
       nf++;
       readconf(fname, &time, &refTime, NP, x, w, DR);
-
-      for (i = 0; i < NP-1; i++)
-	for (j = i+1; j < NP; j++)
-	  {
-	    for (a = 0; a < 3; a++)
-	      {
-		Dx[a] = x[a][i] - x[a][j];
-	      }
-	    for (a = 0; a < 3; a++)
-	      Dx[a] = Dx[a] - L * rint(Dx[a]/L);
-	   distSq = 0.0;
-	   for (a = 0; a < 3; a++)
-	     distSq += Sqr(Dx[a]);
-	   bin = ((int) (sqrt(distSq) / delr)); 
-	   if (bin < points || bin >= 0)
-	     {
-	       g0[bin] += 2.0;
-	     }
-	   else 
-	     {
-	       //printf("bin=%d\n", bin);
-	       //exit(1);
-	     }
-	  }
+      build_linked_list();
+      for (i = 0; i < NP; i++)
+	{
+	  for (iZ = -1; iZ <= +1; iZ++) 
+	    {
+	      jZ = inCell[2][i] + iZ;    
+	      shift[2] = 0.;
+	      /* apply periodico boundary condition along z if gravitational
+	       * fiels is not present */
+	      if (jZ == -1) 
+		{
+		  jZ = cellsz - 1;    
+		  shift[2] = - L;
+		} 
+	      else if (jZ == cellsz) 
+		{
+		  jZ = 0;    
+		  shift[2] = L;
+		}
+	      for (iY = -1; iY <= +1; iY ++) 
+		{
+		  jY = inCell[1][i] + iY;    
+		  shift[1] = 0.0;
+		  if (jY == -1) 
+		    {
+		      jY = cellsy - 1;    
+		      shift[1] = -L;
+		    } 
+		  else if (jY == cellsy) 
+		    {
+		      jY = 0;    
+		      shift[1] = L;
+		    }
+		  for (iX = -1; iX <= +1; iX ++) 
+		    {
+		      jX = inCell[0][i] + iX;    
+		      shift[0] = 0.0;
+		      if (jX == -1) 
+			{
+			  jX = cellsx - 1;    
+			  shift[0] = - L;
+			} 
+		      else if (jX == cellsx) 
+			{
+			  jX = 0;   
+			  shift[0] = L;
+			}
+		      j = (jZ *cellsy + jY) * cellsx + jX + NP;
+		      for (j = cellList[j]; j > -1; j = cellList[j]) 
+			{
+			  if (j <= i) 
+			    continue;
+			  for (a = 0; a < 3; a++)
+			    {
+			      Dx[a] = x[a][i] - x[a][j];
+			    }
+			  for (a = 0; a < 3; a++)
+			    Dx[a] = Dx[a] - L * rint(Dx[a]/L);
+			  distSq = 0.0;
+			  for (a = 0; a < 3; a++)
+			    distSq += Sqr(Dx[a]);
+			  bin = ((int) (sqrt(distSq) / delr)); 
+			  if (bin < points || bin >= 0)
+			    {
+			      g0[bin] += 2.0;
+			    }
+			  else 
+			    {
+			      //printf("bin=%d\n", bin);
+			      //exit(1);
+			    }
+			}
+		    }
+		}
+	    }
+	}
     }
   fclose(f2); 
   f = fopen("gr.dat", "w+");
@@ -247,3 +443,4 @@ int main(int argc, char** argv)
     }
   fclose(f);
 }
+
