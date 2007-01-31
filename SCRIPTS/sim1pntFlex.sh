9e7891e179d056286a989d78773eb8b1cc9823f2
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2007-01-31 18:25:46 +0000

    * ora funzionicchia
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@695 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/sim1pntFlex.sh
+++ SCRIPTS/sim1pntFlex.sh
@@ -5,51 +5,57 @@ exit 0
 fi
 if [ "$1" = "" ]
 then
-EQSTPS=2000
+EQSTPS=4000
 else
 EQSTPS=$1
 fi
 N2=`ls -1 CONF-1-*.dat|wc -l`
 NT=`ls -1 CONF-*-*.dat | wc -l`
-N1=`echo NT/N2 | bc`
-nr="0"
-#densità superificiale di antigeni
-SIG=`basename `pwd`| awk -F 'sigma' '{print $2}'`
+N1=`echo $NT/$N2 | bc`
+n1="0"
+n2="0"
+#densità superficiale di antigeni
+CDIR=`pwd`
+BN=`basename $CDIR`
+SIG=`echo $BN | awk -F 'sigma' '{print $2}'`
+echo "N1=" $N1 "N2=" $N2
 while [ $n1 -lt $N1 ]
 do
+n2="0"
 while [ $n2 -lt $N2 ]
 do
+#echo "n1=" $n1 "n2=$n2"
 if [ ! -d RUN-${n1}-${n2} ]
 then
 mkdir RUN-${n1}-${n2}
 fi
 cd RUN-${n1}-${n2}
-PARFILE="ellips_flex.par"
+PARFILE="ellipsoid_flex.par"
 INICONF="CONF-$n1-$n2.dat"
 rm -f COORD_TMP*
 rm -f Store-*
 ELLEXE="../../ellipsoid"
 SIMEQ="ellflexEQ-${SIG}-${n1}-${n2}"
 SIMPR="ellflexPR-${SIG}-${n1}-${n2}"
-STORERATE="0.01"
+STORERATE="1.0"
 USENNL=0
 PARNUM=2024
 DT="0.05"
 RCUT="-1"
 #N.B. it's supposed that we use NNL here!!
 cp ../$PARFILE .
-mv ../$INICONF .
+cp ../$INICONF .
 if [ $EQSTPS -eq 0 ]
 then
-STPS=2000
+STPS=4000
 else
 STPS=$EQSTPS
 fi
-../../set_params.sh $PARFILE stepnum $STPS storerate 0.0 scalevel 1 rescaleTime 5.0 intervalSum 10.0 inifile $INICONF endfile ${SIMEQ}.cor
+../set_params.sh $PARFILE stepnum $STPS storerate 0.0 scalevel 1 rescaleTime 5.0 intervalSum 10.0 inifile $INICONF endfile ${SIMEQ}.cor
 ln -sf $ELLEXE $SIMEQ 
 $SIMEQ -fa ./$PARFILE > screen_$SIMEQ 
 #PRODUZIONE
-../../set_params.sh $PARFILE stepnum $STPS storerate $STORERATE intervalSum 10.0 inifile ${SIMEQ}.cor endfile ${SIMPR}.cor
+../set_params.sh $PARFILE stepnum $STPS base 1 NN 1 storerate $STORERATE intervalSum 10.0 inifile ${SIMEQ}.cor endfile ${SIMPR}.cor
 ln -sf $ELLEXE $SIMPR
 $SIMPR -f ./$PARFILE > screen_$SIMPR 
 cd ..
