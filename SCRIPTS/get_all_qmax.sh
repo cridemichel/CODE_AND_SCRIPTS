d1e50746505ec2a43d73a5c9f1f9fb4b0df72324
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2007-02-08 15:09:22 +0000

    * ora funziona utilizzando gli Store files se ci sono...
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@703 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/get_all_qmax.sh
+++ SCRIPTS/get_all_qmax.sh
@@ -3,8 +3,9 @@
 #alha stimato dal caso X0=0.33333333 Phi=0.45
 #(ved. fig. tau_vs_q_X0_0.33333333_Phi0.45.agr)
 ALPHA='1.6' 
-EXE_PATH=$HOME/postdoc/hardellipsoid/hardellSVN/CODE/
+EXE_PATH=$HOME/ELLIPSOIDS
 FN="all_qmax.dat"
+FN2="all_qmaxsc.dat"
 echo -n "" > $FN
 for f in X0*
 do 
@@ -13,6 +14,7 @@ cd $f
 for g in Phi*
 do
 cd $g
+#echo $f "---" $g
 if [ ! -e Sq.dat ]
 then
 echo $f " " $g " Sq.dat does not exist!"
@@ -24,12 +26,15 @@ if [ \( -e "Store-0-0.gz" \) -o \( -e "Store-0-0" \) ]
 then
 if [ -e "Store-0-0.gz" ]
 then
-gunzip Store-0.0.gz
+gunzip Store-0-0.gz
 RIZIP=1
 else
 RIZIP=0
 fi
 L=`tail -1 Store-0-0` 
+A0=`cat Store-0-0 | awk -F ':' '{ if ($1=="a") print $2}' | awk '{print $1}'`
+B0=`cat Store-0-0 | awk -F ':' '{ if ($1=="b") print $2}' | awk '{print $1}'`
+C0=`cat Store-0-0 | awk -F ':' '{ if ($1=="c") print $2}' | awk '{print $1}'`
 if [ "$RIZIP" == "1" ]
 then
 gzip Store-0-0
@@ -50,12 +55,15 @@ fi
 echo "L= " $L " Doing " $g
 PR=$EXE_PATH/FQT/findmax
 SF=`echo "e((1.0/3.0)*l($A0*$B0*$C0))"|bc -l`
+#echo ">>>>> qui A0=" $A0
 SQMAX=`$PR Sq.dat 0`
 #PI=`echo "pi" |octave| LANG=C gawk -F '=' '{if ($1=="ans") print $2}'`
 SQMAXFQS=`echo "$SQMAX" | LANG=C gawk -v SF=$SF -v sqmax="$SQMAX" -v L="$L" -v alpha="$ALPHA" '{pi=atan2(1.0,1.0)*4.0; printf("%.15G",((SF*2.0*pi/L)*(0.5*sqmax+1.25))^alpha)}'`
+SQMAXSC=`echo "$SQMAX" | LANG=C gawk -v SF=$SF -v sqmax="$SQMAX" -v L="$L" -v alpha="$ALPHA" '{pi=atan2(1.0,1.0)*4.0; printf("%.15G",(SF*2.0*pi/L)*(0.5*sqmax+1.25))}'`
 X0=`echo $f | gawk -F '_' '{print $2}'`
 PHI=`echo $g | LANG=C gawk -F 'Phi' '{print $2}'`
 echo $X0 " " $PHI " " $SQMAXFQS >> ../../$FN
+echo $X0 " " $PHI " " $SQMAXSC >> ../../$FN2
 cd ..
 done
 cd ..
