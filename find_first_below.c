2445d4752d80f85d302c696874b8faef56e553a9
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2007-03-27 15:44:57 +0000

    * programmetto per trovare il primo punto sotto una certa soglia in un file di dati.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@764 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ find_first_below.c
@@ -0,0 +1,65 @@
+/*
+ * =====================================================================================
+ * 
+ *        Filename:  fitsq.c
+ * 
+ *     Description:  trova rozzamente il massimo con un fit quadratico. 
+ * 
+ *         Version:  1.0
+ *         Created:  14/08/2005 12:54:34 CEST
+ *        Revision:  none
+ *        Compiler:  gcc
+ * 
+ *          Author:   (), 
+ *         Company:  
+ * 
+ * =====================================================================================
+ */
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+char fname[1024];
+double *func[2];
+char line[4096], dummy[4096];
+int main(int argc, char **argv)
+{
+  FILE *f;
+  int maxxi, cc, pnts, ccini;
+  double first, soglia;
+  double maxy, maxx;
+  if (argc < 3)
+    {
+      printf("devi fornire il nome del file come argomento e la soglia\n");
+      exit(-1);
+    }
+  soglia = atof(argv[2]);
+  f = fopen(argv[1], "r");
+  cc=0;
+  while (!feof(f))
+    {
+      fscanf(f, "%[^\n]\n", line);
+      cc++;
+    }
+  pnts = cc;
+  func[0] = malloc(sizeof(double)*pnts);
+  func[1] = malloc(sizeof(double)*pnts);
+  rewind(f);
+  for (cc = 0; cc < pnts; cc++)
+    {
+      fscanf(f, "%lf %lf%[^\n]\n", &func[0][cc], &func[1][cc], dummy);
+      //printf("x: %.15G y: %.15G\n", func[0][cc], func[1][cc]);
+    }
+  fclose(f);
+  first = func[1][0];
+  for (cc = ccini; cc < pnts; cc++)
+    {
+      //printf("maxx: %.15G maxy: %.15G x: %.15G y: %.15G\n", maxx, maxy, func[0][cc], func[1][cc]);
+      if (func[1][cc] < soglia)
+	{
+	  printf("%.15G", func[0][cc]);
+	  exit(-1);
+	}
+    }
+  return 0;
+}
+
