b117bfe283f6345ea2ff8cfbc9ef62b841d63c04
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2007-05-18 14:23:01 +0000

    * aggiunto file per superellissoidi
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@11885 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ trunk/MDsimul/ellipsoid/superellipsoid.c
@@ -0,0 +1,67 @@
+#if defined(MD_SUPERELLIPSOID) 
+#include<mdsimul.h>
+#define SIMUL
+#define SignR(x,y) (((y) >= 0) ? (x) : (- (x)))
+#define MD_DEBUG10(x)  
+#define MD_DEBUG11(x) 
+#define MD_DEBUG15(x) 
+#define MD_DEBUG20(x) 
+#define MD_DEBUG29(x) 
+#define MD_DEBUG30(x)  //qui 
+#define MD_DEBUG31(x) //qui 
+#define MD_DEBUG32(x)    
+#define MD_DEBUG33(x) 
+#define MD_DEBUG34(x) 
+#define MD_DEBUG36(x) 
+#define MD_NEGPAIRS
+#define MD_NO_STRICT_CHECK
+#define MD_OPTDDIST
+#ifdef EDHE_FLEX
+extern void set_angmom_to_zero(int i);
+extern int *is_a_sphere_NNL;
+#endif
+#if defined(MPI)
+extern int my_rank;
+extern int numOfProcs; /* number of processeses in a communicator */
+extern int *equilibrated;
+#endif 
+extern double **Xa, **Xb, **RA, **RB, ***R, **Rt, **RtA, **RtB, **REtA, **REtB;
+extern double cosEulAng[2][3], sinEulAng[2][3];
+extern long long int itsFNL, timesFNL, timesSNL, itsSNL;
+extern int do_check_negpairs;
+
+#ifdef EDHE_FLEX
+extern int *mapbondsaFlex, *mapbondsbFlex, nbondsFlex;
+extern double *mapBheightFlex, *mapBhinFlex, *mapBhoutFlex, *mapSigmaFlex; 
+extern double *t2arr, *distsOld, *dists, *distsOld2, *maxddoti;
+extern int *crossed, *tocheck, *dorefine, *crossed, *negpairs;
+#endif
+#ifdef MD_ASYM_ITENS
+extern double **Ia, **Ib, **invIa, **invIb, **Iatmp, **Ibtmp, *angM;
+#else
+extern double Ia, Ib, invIa, invIb;
+#endif
+extern double gradplane[3];
+struct LastBumpS *lastbump;
+extern double *axa, *axb, *axc;
+extern int *scdone;
+extern double *maxax;
+/* Routines for LU decomposition from Numerical Recipe online */
+void ludcmpR(double **a, int* indx, double* d, int n);
+void lubksbR(double **a, int* indx, double *b, int n);
+extern void update_MSDrot(int i);
+extern void InvMatrix(double **a, double **b, int NB);
+extern void calc_angmom(int i, double **I);
+double min(double a, double b);
+extern void upd_refsysM(int i);
+extern double calc_maxddot(int i, int j);
+double zbrent(double (*func)(double), double x1, double x2, double tol);
+extern double invaSq[2], invbSq[2], invcSq[2];
+extern double rxC, ryC, rzC;
+extern int SolveLineq (double **a, double *x, int n); 
+extern double calc_norm(double *vec);
+int calcdist_retcheck;
+extern double rA[3], rB[3];
+extern double gradplane_all[6][3], rBall[6][3];
+extern int polinterr, polinterrRyck;
+#endif
