f506fcfbf477b067dcb116d6496b996f1756fbe2
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2008-02-19 14:14:20 +0000

    * small fix
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@881 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/get_cluster.sh
+++ SCRIPTS/get_cluster.sh
@@ -18,9 +18,11 @@ COL1=${colarr[$ai]}
 COL2=${colarr[$[$ai+1]]}
 fi 
 #echo " COL1=" $COL1 " COL2=" $COL2 " PRIMO=" $PRIMO " RESTO=" $RESTO
+#echo $RESTO
 for par in $RESTO
 do
-cat $1 | awk -v numpar=$par -v col1=$COL1 -v col2=$COL2 'BEGIN {fpA=0; fbB=0; np=0; cc=0}{if ($17=="C[red]") {np++; if (np==numpar) fpA=1}; if ($17=="C[green]") {np++; if (np==numpar) fpB=1} if (fpA==1) {if (cc==0) {for (i=1; i < 17; i++) printf("%s ",$i); printf("C[%s]\n", col1); } else if (cc < 6) print $0; cc++; if (cc==6) {fpA=0;cc=0;}}; if (fpB==1) {if (cc==0) {for (i=1; i < 17; i++) printf("%s ",$i); printf("C[%s]\n",col2);} else if (cc < 3) print $0; cc++; if (cc==3) {fpB=0;cc=0;} };}'
+#continue
+cat $1 | awk -v numpar=$par -v col1=$COL1 -v col2=$COL2 'BEGIN {fpA=0; fbB=0; np=0; cc=0}{if ($17=="C[red]") {if (np==numpar) fpA=1; np++}; if ($17=="C[green]") {if (np==numpar) fpB=1; np++} if (fpA==1) {if (cc==0) {for (i=1; i < 17; i++) printf("%s ",$i); printf("C[%s]\n", col1); } else if (cc < 6) print $0; cc++; if (cc==6) {fpA=0;cc=0;}}; if (fpB==1) {if (cc==0) {for (i=1; i < 17; i++) printf("%s ",$i); printf("C[%s]\n",col2);} else if (cc < 3) print $0; cc++; if (cc==3) {fpB=0;cc=0;} };}'
 done
 ai=$[$ai+2]
 done
