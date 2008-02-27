5ce22be1e2f4e26cfc28a45a7828302b976ba11a
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2008-02-27 17:34:34 +0000

    * script per bimixNM
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@888 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ SCRIPTS/bimixNM/sim1statepnt.sh
@@ -0,0 +1,51 @@
+PARFILE=BMsoft.par
+if [ "$1" == "" ]
+then
+echo "You must supply the temperature!"
+exit
+fi
+ln -sf $HOME/MDsimul/bin/bimix bimixNM 
+if [ ! -d eqT$1 ]
+then
+mkdir eqT$1
+fi
+cp $PARFILE eqT$1/
+cd eqT$1
+rm -f COORD_TMP*
+BMEXE="../bimixNM"
+SIMRA="bimixRA$1"
+SIMEQ="bimixEQ$1"
+SIMPR="bimixPR$1"
+STORERATE="0.01"
+SIG2EQ="2.0"
+USENNL=1
+PARNUM=1000
+DT="0.002"
+#RANDOMIZZAZIONE INIZIALE
+cp $PARFILE rand_$PARFILE
+#>>> SET TEMPERATURE TO 10.0
+../set_params.sh rand_$PARFILE steplength $DT stepnum 1000 chkeqstps 50 eqFact 5.0 endfile ${SIMRA}.cor parnum $PARNUM temperat 10.0 sResetSteps 100 CMreset 100 bakSaveMode 0
+ln -sf $BMEXE $SIMRA
+$SIMRA -f ./rand_${PARFILE} > screen_$SIMRA 
+cp rand_$PARFILE $PARFILE
+#EQUILIBRATURA
+../set_params.sh $PARFILE stepnum 1000000000 inifile ${SIMRA}.cor endfile ${SIMEQ}.cor sResetSteps 1000 CMreset 0 chkeqstps 50 eqFact $SIG2EQ temperat $1
+ln -sf $BMEXE $SIMEQ 
+$SIMEQ -f ./$PARFILE > screen_$SIMEQ 
+#PRODUZIONE
+if [ ! -d ../T$1 ]
+then
+mkdir ../T$1
+fi
+cp screen_$SIMEQ ../T$1
+cp $SIMEQ.cor ../T$1
+cp BMsoft.par ../T$1
+cd ../T$1
+rm -f COORD_TMP*
+STCI=`cat screen_$SIMEQ | awk '{if ($1=="[MSDcheck]") print $3}'`
+STPS=`echo "$STCI*$2"| bc -l`
+NN=`echo "l($STCI)/l(1.4)" | bc -l | awk '{printf("%d",$0)}'`
+../set_params.sh $PARFILE stepnum $STPS chkeqstps 0 NN $NN inifile ${SIMEQ}.cor endfile ${SIMPR}.cor sResetSteps 0 bakSaveMode 1
+ln -sf $BMEXE $SIMPR
+./$SIMPR -f ./$PARFILE > screen_$SIMPR  
+cd ..
