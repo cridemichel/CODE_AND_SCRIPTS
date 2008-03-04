19ced6e9791523c5996be9b515694b8149b6a616
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2008-03-04 17:17:13 +0000

    * added scripts to run NVE simulations from NVT ones. * some fixes in the other scripts for bimixNM.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@891 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ SCRIPTS/bimixNM/NVEfromNVT.sh
@@ -0,0 +1,42 @@
+PARFILE=BMsoft.par
+if [ "$1" == "" ]
+then
+echo "Syntax: NVEfromBVT.sh <temperature> [FACTOR] [NN] [custom string] " 
+exit
+fi
+if [ ! -e NVE-T$1 ]
+then
+mkdir NVE-T$1
+fi
+cp $PARFILE NVE-T$1/
+cd NVE-T$1
+rm -f COORD_TMP*
+rm -f Cnf*
+ELLEXE="../../bimix"
+SIMNVE="bimixNM-NVE-${4}T$1"
+STORERATE="0.01"
+PARNUM=1000
+DT="0.002"
+if [ "$2" == "" ]
+then
+FACT=1
+else
+FACT=$2
+fi
+LASTFILE=`ls ../T$1/Cnf*|sort -t - -k 3 -n | tail -1`
+TOTSTPS=`cat $LASTFILE | awk -v fact=$FACT -F : '{if ($1=="totStep") print $2*fact}'`
+echo "totSteps=>" $TOTSTPS
+cat $LASTFILE | awk -v tstps=$TOTSTPS -F : ' BEGIN {doout=0} {if (doout==1) print $0; if ($0=="@@@") doout=1;}' > restart.cor
+../set_params.sh $PARFILE bakSaveMode 1 stepnum $TOTSTPS inifile restart.cor endFormat 2 endfile ${SIMNVE}.cor Nose 0 nRun NVE
+if [ "$3" != "" ]
+then 
+#NOTA: se si specifica NN FACT si riferisce ai blocchi di NN configurazioni 
+#  invece se NN non viene specificato allora FACT è un fattore rispetto ai passi 
+#  che gia' si trovano in BMsoft.par
+TOTSTPS=`echo "(1.4^$3)*$FACT" | bc -l | awk '{printf("%d",$0)}'`
+../set_params.sh $PARFILE NN $3 stepnum $TOTSTPS
+echo "===> NN= " $3 " Total Steps=" $TOTSTPS
+fi
+ln -sf $ELLEXE $SIMNVE 
+$SIMNVE -fa $PARFILE > screen_$SIMNVE
+cd ..
