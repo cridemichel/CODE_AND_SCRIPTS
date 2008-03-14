be25de4a0cd6c5d71f16ac97ff2723d96982c10f
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2008-03-14 11:08:17 +0000

    * aggiunto il calcolo del parametro non-gaussiano con il flag -ng
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@896 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MSD/calcmsd.c
+++ MSD/calcmsd.c
@@ -5,12 +5,12 @@
 #define MAXPTS 10000
 #define MAXFILES 5000
 char **fname; 
-double L, time, *ti, *rotMSD, *MSD, *rotMSDA, *MSDA, *rotMSDB, *MSDB, *cc, *rotMSDcls[2], *MSDcls[2], *cc_cls[2];
+double L, time, *ti, *rotMSD, *alpha2, *alpha2A, *alpha2B, *MSD, *rotMSDA, *MSDA, *rotMSDB, *MSDB, *cc, *rotMSDcls[2], *MSDcls[2], *cc_cls[2];
 double **DR, **DR0;
 double *r0[3], *w0[3], *rt[3], *wt[3], *rtold[3];
 char parname[128], parval[256000], line[256000];
 char dummy[2048];
-int points=-1, foundDRs=0, foundrot=0, eventDriven=0, skip=1, clusters=0;
+int points=-1, foundDRs=0, foundrot=0, eventDriven=0, skip=1, clusters=0, nongauss=0;
 int *isPercPart;
 char *pnum;
 char inputfile[2048], cluststr[2048];
@@ -148,6 +148,10 @@ void parse_param(int argc, char** argv)
 	{
 	  clusters=1;
 	}
+      else if (!strcmp(argv[cc],"--nongauss") || !strcmp(argv[cc],"-ng"))
+	{
+	  nongauss=1;
+	}
       else if (cc == argc || extraparam == 2)
 	print_usage();
       else if (extraparam == 0)
@@ -169,7 +173,7 @@ void parse_param(int argc, char** argv)
 
 int main(int argc, char **argv)
 {
-  FILE *f, *f2, *f3, *fA, *fB, *f2A, *f2B;
+  FILE *fngA, *fngB, *fng, *f, *f2, *f3, *fA, *fB, *f2A, *f2B;
   double *adjDr[3], Dr, Dw, A1, A2, A3, dr;
   int c1, c2, c3, i, nfiles, nf, ii, nlines, nr1, nr2, a;
   int NP, NPA=-1, NN=-1, fine, JJ, nat, maxl, maxnp, np, NP1, NP2;
@@ -264,6 +268,12 @@ int main(int argc, char **argv)
   ti = malloc(sizeof(double)*points);
   rotMSD = malloc(sizeof(double)*points);
   MSD = malloc(sizeof(double)*points);
+  if (nongauss)
+    {
+      alpha2 = malloc(sizeof(double)*points);
+      alpha2A = malloc(sizeof(double)*points);
+      alpha2B = malloc(sizeof(double)*points);
+    }  
   MSDA = malloc(sizeof(double)*points);
   MSDB = malloc(sizeof(double)*points);
   rotMSDA = malloc(sizeof(double)*points);
@@ -289,6 +299,12 @@ int main(int argc, char **argv)
     {
       ti[ii] = -1.0;
       rotMSD[ii] = 0.0;
+      if (nongauss)
+	{
+	  alpha2[ii] = 0.0;
+	  alpha2A[ii] = 0.0;
+	  alpha2B[ii] = 0.0;
+	}
       MSD[ii] = 0.0;
       rotMSDA[ii] = 0.0;
       MSDA[ii] = 0.0;
@@ -416,6 +432,11 @@ int main(int argc, char **argv)
 
 		      tmpdbl = (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
 		      MSD[np] += tmpdbl;
+		      if (nongauss)
+			{
+			  alpha2[np] += tmpdbl*tmpdbl;
+
+			}
 		      if (clusters)
 			{
 			  if (isPercPart[i]==1)
@@ -439,13 +460,19 @@ int main(int argc, char **argv)
 			{
 			  if (i < NPA)
 			    {
-			      MSDA[np] += (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
+			      tmpdbl = (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
+			      MSDA[np] += tmpdbl;
+			      if (nongauss)
+				alpha2A[np] += tmpdbl*tmpdbl;
 			      if (foundrot)
 				rotMSDA[np] += Dw*Dw;
 			    }
 			  else
 			    {
-			      MSDB[np] += (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
+			      tmpdbl = (Dr+adjDr[a][i])*(Dr+adjDr[a][i]);
+			      MSDB[np] += tmpdbl;
+			      if (nongauss)
+				alpha2B[np] += tmpdbl*tmpdbl;
 			      if (foundrot)
 				rotMSDB[np] += Dw*Dw;
 			    }
@@ -465,6 +492,9 @@ int main(int argc, char **argv)
 	}
     }
   f = fopen("MSDcnf.dat", "w+");
+  if (nongauss)
+    fng = fopen("alpha2.dat", "w+");
+
   if (foundrot)
     f2 = fopen("rotMSDcnf.dat", "w+");
   if (NP != NPA)
@@ -472,6 +502,11 @@ int main(int argc, char **argv)
       fA = fopen("MSDAcnf.dat", "w+");
       if (foundrot)
 	f2A = fopen("rotMSDAcnf.dat", "w+");
+      if (nongauss)
+	{
+  	  fngA = fopen("alpha2A.dat", "w+");
+  	  fngB = fopen("alpha2B.dat", "w+");
+	}
       fB = fopen("MSDBcnf.dat", "w+");
       if (foundrot)
 	f2B = fopen("rotMSDBcnf.dat", "w+");
@@ -481,7 +516,11 @@ int main(int argc, char **argv)
       //printf("cc[%d]=%f ti=%f\n", ii, cc[ii], ti[ii]);
       if (cc[ii] > 0 && ti[ii] > -1.0)
 	{
+	  tmpdbl = MSD[ii]/cc[ii]/((double)NP);
+	  tmpdbl = tmpdbl*tmpdbl;
 	  fprintf(f, "%.15G %.15G %f\n", ti[ii]-ti[0], MSD[ii]/cc[ii]/((double)NP), cc[ii]);
+	  if (nongauss)
+	    fprintf(fng, "%.15G %.15G %f\n", ti[ii]-ti[0], (3.0/5.0)*alpha2[ii]/cc[ii]/((double)NP)/tmpdbl, cc[ii]);
 	  if (foundrot)
 	    fprintf(f2, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSD[ii]/cc[ii]/((double)NP), cc[ii]);
 	}
@@ -493,13 +532,29 @@ int main(int argc, char **argv)
 	  fprintf(fB, "%.15G %.15G %f\n", ti[ii]-ti[0], MSDB[ii]/cc[ii]/((double)NP-NPA), cc[ii]);
 	  if (foundrot)
 	    fprintf(f2B, "%.15G %.15G %f\n", ti[ii]-ti[0], rotMSDB[ii]/cc[ii]/((double)NP-NPA), cc[ii]);
+	  if (nongauss)
+	    {
+	      tmpdbl = MSDA[ii]/cc[ii]/((double)NPA);
+	      tmpdbl = tmpdbl*tmpdbl;
+	      fprintf(fngA, "%.15G %.15G %f\n", ti[ii]-ti[0], (3.0/5.0)*alpha2A[ii]/cc[ii]/((double)NPA)/tmpdbl, cc[ii]);
+	      tmpdbl = MSDB[ii]/cc[ii]/((double)NP-NPA);
+	      tmpdbl = tmpdbl*tmpdbl;
+	      fprintf(fngB, "%.15G %.15G %f\n", ti[ii]-ti[0], (3.0/5.0)*alpha2B[ii]/cc[ii]/((double)NP-NPA)/tmpdbl, cc[ii]);
+	    }
 	}
     }
   fclose(f);
+  if (nongauss)
+    fclose(fng);
   if (foundrot)
     fclose(f2);
   if (NP != NPA)
     {
+      if (nongauss)
+	{
+	  fclose(fngA);
+	  fclose(fngB);
+	}
       fclose(fA);
       if (foundrot)
 	fclose(f2A);
