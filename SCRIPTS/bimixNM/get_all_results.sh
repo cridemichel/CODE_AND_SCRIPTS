86e5a84b7d74552cec433bbf6d7528ced9a1b0fd
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2008-04-09 14:42:20 +0000

    * get_all_results.sh added
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@905 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ SCRIPTS/bimixNM/get_all_results.sh
@@ -0,0 +1,9 @@
+DATA=`date '+%d%m%y'`
+FL=""
+for f in NM*-*
+do
+FL="$FL $f/rho*/NVE-T*/Fqs* $f/rho*/NVE-T*/N-sqt* $f/rho*/NVE-T*/MSD* $f/rho*/NVE-T*/Sq* $f/rho*/NVE-T*/alpha*" 
+done
+echo "FILE_LIST=" $FL
+tar czf  BMNMres${DATA}.tgz $FL
+#tar czf BMNMres${DATA}-2.tgz NM11-6/rho*/NVE-T*/Fqs* NM11-6/rho*/NVE-T*/N-sqt* NM11-6/rho*/NVE-T*/MSD* NM11-6/rho*/NVE-T*/Sq* NM11-6/rho*/NVE-T*/alpha* NM12-6/rho*/NVE-T*/Fqs* NM12-6/rho*/NVE-T*/N-sqt* NM12-6/rho*/NVE-T*/MSD* NM12-6/rho*/NVE-T*/Sq* NM12-6/rho*/NVE-T*/alpha* NM9-6/rho*/NVE-T*/Fqs* NM9-6/rho*/NVE-T*/N-sqt* NM9-6/rho*/NVE-T*/MSD* NM9-6/rho*/NVE-T*/Sq* NM9-6/rho*/NVE-T*/alpha* NM12-11/rho*/NVE-T*/Fqs* NM12-11/rho*/NVE-T*/N-sqt* NM12-11/rho*/NVE-T*/MSD* NM12-11/rho*/NVE-T*/Sq* NM12-11/rho*/NVE-T*/alpha* NM8-5/rho*/NVE-T*/Fqs* NM8-5/rho*/NVE-T*/N-sqt* NM8-5/rho*/NVE-T*/MSD* NM8-5/rho*/NVE-T*/Sq* NM8-5/rho*/NVE-T*/alpha*
