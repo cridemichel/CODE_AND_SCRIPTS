8996ebc17655247ae5e1d663b9f2923e06de2043
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2010-02-18 06:56:37 +0000

    * ancora non compila la versione in C.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1170 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HE_MC/he_overlap.c
+++ HE_MC/he_overlap.c
@@ -1,16 +1,19 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<math.h>
+#define TINY 1E-20
+int brentTooManyIter;
 const tol=1E-15, LAM0=0.0, LAM1=0.5, LAM2=1.0;
 double saA[3], saB[3], RA[3][3], RB[3][3];
 double COMA[3], COMB[3];
-void ludcmp(double **a, int n,  int* indx, double* d, int *ok)
+void ludcmp(double a[3][3], int n,  int* indx, double* d, int *ok)
 {
   /* A[i][j] = Aij 
    * A x = b  
    * per semplicità nel seguito si assume che l'ordine della matrice è 3 */
   int i,imax=-1,j,k;
   double big,dum,sum,temp; 
-  double vv[MD_NBMAX]; /* vv stores the implicit scaling of each row.*/
+  double vv[3]; /* vv stores the implicit scaling of each row.*/
   /*vv = vector(1,n);*/
   *d=1.0; /* No row interchanges yet. */
   *ok = 0;
@@ -88,7 +91,7 @@ void ludcmp(double **a, int n,  int* indx, double* d, int *ok)
   /*free_vector(vv,1,n); */
 }
 
-void lubksb(double **a, int n, int* indx, double *b)
+void lubksb(double a[3][3], int n, int* indx, double *b)
 { 
   int i,ii=0,ip,j; 
   double sum; 
@@ -120,7 +123,7 @@ void lubksb(double **a, int n, int* indx, double *b)
 }
 
 #define MOV3(a,b,c,d,e,f) (a)=(d);(b)=(e);(c)=(f);
-double dbrent(double ax, double bx, double cx, double (*f)(double), double (*df)(double), double tol, double *xmin) 
+double dbrent(double ax, double bx, double cx, void (*f)(double,double*,double*), double tol, double *xmin) 
   /* Given a function f and its derivative function df, and given a bracketing triplet of abscissas ax, bx, cx
    * [such that bx is between ax and cx, and f(bx) is less than both f(ax) and f(cx)], this routine isolates the
    * minimum to a fractional precision of about tol using a modi cation of Brent s method that uses derivatives.
@@ -131,12 +134,16 @@ double dbrent(double ax, double bx, double cx, double (*f)(double), double (*df)
   const int ITMAXDBR=100; 
   int iter,ok1,ok2; /*Will be used as  ags for whether proposed steps are acceptable or not.*/
   double a,b,d=0.0,d1,d2,du,dv,dw,dx,e=0.0; double fu,fv,fw,fx,olde,tol1,tol2,u,u1,u2,v,w,x,xm; 
-  a=(ax < cx ? ax : cx); b=(ax > cx ? ax : cx); x=w=v=bx; fw=fv=fx=(*f)(x); dw=dv=dx=(*df)(x); 
+  a=(ax < cx ? ax : cx); b=(ax > cx ? ax : cx); x=w=v=bx; 
+  
+  (*f)(x,&fx,&dx);
+  fw=fv=fx; 
+  dw=dv=dx; 
   /*All our housekeeping chores are doubled by the necessity of moving derivative values around as well 
    * as function values. */
   for (iter=1;iter<=ITMAXDBR;iter++)
     { 
-      printf("x=%.15G iter=%d\n", x, iter );
+      //printf("x=%.15G iter=%d\n", x, iter );
       xm=0.5*(a+b); 
       tol1=tol*fabs(x)+ZEPSDBR; tol2=2.0*tol1; 
       if (fabs(x-xm) <= (tol2-0.5*(b-a))) 
@@ -188,19 +195,20 @@ double dbrent(double ax, double bx, double cx, double (*f)(double), double (*df)
 	} 
       if (fabs(d) >= tol1) 
 	{ 
-	  u=x+d; fu=(*f)(u);
+	  u=x+d; 
+	  (*f)(u,&fu,&du);
 	} 
       else
 	{ 
 	  u=x+SIGN(tol1,d); 
-	  fu=(*f)(u);
+	  (*f)(u, &fu, &du);
 	  if (fu > fx) 
 	    { /*If the minimum step in the downhill direction takes us uphill, then we are done.*/
 	      *xmin=x;
 	      return fx;
 	    }
 	}
-      du=(*df)(u); /*Now all the housekeeping, sigh.*/
+      //du=(*df)(u); /*Now all the housekeeping, sigh.*/
       if (fu <= fx) 
 	{ 
 	  if (u >= x) a=x;
@@ -329,233 +337,204 @@ double brent(double ax, double bx, double cx, double (*f)(double), double tol, d
   return fx;
 }
 const int USEDBRENT=1, READFROMFILE=1,USEDIRINV=1;
-void matinv(A,Ainv)
+void MATINV(double A[3][3],double Ainv[3][3])
 {
-       REAL*8 A(3,3),D,Ainv(3,3)
-       INTEGER i,j,INDX(3)
-       do i=1,3 
-       do j=1,3
-         Ainv(i,j)=0.0d0
-       end do 
-       Ainv(i,i)=1.0d0
-       end do 
-       CALL ludcmp(A,3,3,INDX,D) 
-       do j=1,3
-        CALL lubksb(A,3,3,INDX,Ainv(1,j))
-	  /*     Note that FORTRAN stores two-dimensional matrices by column, so y(1,j) is the
-       address of the jth column of y.
-       end do*/
+  double D, col[3];
+  int i,j,INDX[3], ok;
+  for (i=0; i < 3; i++)
+    {
+      for (j=0; j < 3; j++)
+	Ainv[i][j]=0.0;
+      Ainv[i][i]=1.0;
+    }
+  ludcmp(A,3,INDX,&D, &ok);
+  for (j=0; j < 3; j++)
+    {
+      for (i=0; i < 3; i++)
+	col[i]=0.0;
+      col[j]=1.0;
+      lubksb(A,3,INDX,col[j]);
+      for (i=0; i < 3; i++)
+	Ainv[i][j] = col[i];
+    }
 }
-       SUBROUTINE MATINV33(A,Ainv)
-       REAL*8 A(3,3),Ainv(3,3)
-       REAL*8 DET
-       DET = A(1,1)*A(2,2)*A(3,3)-A(1,1)*A(2,3)*A(3,2)-A(1,2)*A(2,1)
-     * *A(3,3)+A(1,2)*A(2,3)*A(3,1)+A(1,3)*A(2,1)*A(3,2)-A(1,3)*A(2,2)
-     * *A(3,1)
-       Ainv(1,1)=A(2,2)*A(3,3)-A(2,3)*A(3,2)
-       Ainv(1,2)=A(1,3)*A(3,2)-A(1,2)*A(3,3)
-       Ainv(1,3)=A(1,2)*A(2,3)-A(2,2)*A(1,3)
-       Ainv(2,1)=A(2,3)*A(3,1)-A(3,3)*A(2,1)
-       Ainv(2,2)=A(1,1)*A(3,3)-A(3,1)*A(1,3)
-       Ainv(2,3)=A(1,3)*A(2,1)-A(1,1)*A(2,3)
-       Ainv(3,1)=A(2,1)*A(3,2)-A(2,2)*A(3,1)
-       Ainv(3,2)=A(1,2)*A(3,1)-A(1,1)*A(3,2)
-       Ainv(3,3)=A(1,1)*A(2,2)-A(1,2)*A(2,1)
-       do i=1, 3
-       do j=1, 3
-          Ainv(i,j)=Ainv(i,j)/DET
-       end do
-       end do
-       RETURN
-       END
-SUBROUTINE SpwDer_wrap(x,Sl,SlP,CALCSl,CALCSlP)
-       EXTERNAL SpwDer
-       REAL*8 x, Sl, SlP
-       LOGICAL CALCSl, CALCSlP
-       REAL*8 saA(3),saB(3),RA(3,3),RB(3,3)
-       REAL*8 COMA(3),COMB(3)
-       common / SIMPAR / saA,saB,RA,RB,COMA,COMB
-       CALL SpwDer(x,Sl,SlP,CALCSL,CALCSlP) 
-       RETURN
-       END
-       REAL*8 function Spw_wrap(x)
-       REAL*8 saA(3),saB(3),RA(3,3),RB(3,3)
-       REAL*8 COMA(3),COMB(3),boh, x
-       common / SIMPAR / saA,saB,RA,RB,COMA,COMB
-       Spw_wrap = Spw(x)
-!      print *,"S1=", Spw_wrap
-!       CALL SpwDer(x,Spw_wrap,boh,.TRUE.,.FALSE.) 
-!       ,saA, COMA, RA, saB, COMB, RB) 
-!      print *,'S2=', Spw_wrap
-       RETURN
-       end
-       SUBROUTINE SpwDer(lambda,Sl,SlP,CALCSl,CALCSlP)
-       EXTERNAL MATINV
-       EXTERNAL MATINV33
-       LOGICAL USEDBRENT,USEDIRINV
-       common / LOGICPAR / USEDIRINV,USEDBRENT
-       LOGICAL CALCSl,CALCSlP
-       INTEGER a,i,j,n
-       REAL*8 GinvR(3),saA(3), saB(3),B(3),H(3,3)
-       REAL*8 Ginv(3,3),G(3,3),Ainv(3,3),Binv(3,3)
-       REAL*8 R(3),RA(3,3),RB(3,3),COMA(3),COMB(3) 
-       REAL*8 lambda,Sl,SlP
-       common / SIMPAR / saA,saB,RA,RB,COMA,COMB
-       do a=1,3
-         R(a) = COMB(a)-COMA(a)
-       end do  
-       do i=1,3
-        do j=1,3
-         Ainv(i,j)=0.
-         Binv(i,j)=0.
-         do n=1,3
-          Ainv(i,j)=Ainv(i,j)+saA(n)**2*RA(n,i)*RA(n,j) 
-          Binv(i,j)=Binv(i,j)+saB(n)**2*RB(n,i)*RB(n,j)
-         end do
-         G(i,j)=(1.0D0 - lambda)*Ainv(i,j)+lambda*Binv(i,j) 
-         if (CALCSlP) then
-          H(i,j)=(1.0D0 - lambda)**2*Ainv(i,j)-lambda**2*Binv(i,j)
-         end if
-        end do
-       end do
-       if (USEDIRINV) then
-         CALL MATINV33(G,Ginv)
-       else
-         CALL MATINV(G,Ginv)
-       end if
-       do i=1,3
-        GinvR(i)=0.0d0
-        do j=1,3 
-          GinvR(i)=GinvR(i)+Ginv(i,j)*R(j)
-        end do
-       end do
-!      se il valore di Slp passato è maggiore di 0 calcola la
-!      derivata di S(lambda)
-!      print *,'GinR=', GinvR(1), GinvR(2), GinvR(3)
-       if (CALCSlP) then
-        SlP = 0.0d0
-        do i=1,3
-         do j=1,3
-          SlP = SlP + GinvR(i)*H(i,j)*GinvR(j) 
-         end do
-        end do
-        SlP = -SlP
-       end if
-!      calculate also S(lambda) if Sl > 0.0
-!      print *, 'R=', R(1), R(2), R(3)
-       if (CALCSl) then
-        Sl = 0.0d0
-        do i=1,3
-          Sl = Sl + R(i)*GinvR(i)  
-        end do
-        Sl = lambda*(1.0D0-lambda)*Sl
-        Sl = -Sl
-       end if
-!       print *,'sl=', Sl, ' Slp=', SlP, ' l=',lambda 
-       RETURN
-       END
-       REAL*8 function Spw(lambda)
-!       ,saA,COMA,RA,saB,COMB,RB)
-       LOGICAL USEDBRENT,USEDIRINV
-       common / LOGICPAR / USEDIRINV, USEDBRENT
-       INTEGER a,i,j,n
-       EXTERNAL MATINV
-       EXTERNAL MATINV33
-       REAL*8 saA(3),saB(3),B(3)
-       REAL*8 AB(3,3),ABinv(3,3),Ainv(3,3),Binv(3,3)
-       REAL*8 R(3),RA(3,3),RB(3,3),COMA(3),COMB(3) 
-       common / SIMPAR / saA,saB,RA,RB,COMA,COMB
-       REAL*8 lambda
-       do a=1,3
-       R(a) = COMB(a)-COMA(a)
-       end do  
-       do i=1,3
-       do j=1,3
-       Ainv(i,j)=0.
-       Binv(i,j)=0.
-       do n=1,3
-       Ainv(i,j)=Ainv(i,j)+saA(n)**2*RA(n,i)*RA(n,j) 
-       Binv(i,j)=Binv(i,j)+saB(n)**2*RB(n,i)*RB(n,j)
-       end do
-       ABinv(i,j)=(1.0D0 - lambda)*Ainv(i,j)+lambda*Binv(i,j)
-       end do
-       end do
-!      INVERT ABinv(3,3) matrix
-       if (USEDIRINV) then
-         CALL MATINV33(ABinv,AB)
-       else
-         CALL MATINV(ABinv,AB)
-       end if
-       Spw=0.0D0
-       do i=1,3
-       do j=1,3
-       Spw=Spw+R(i)*AB(i,j)*R(j)
-       end do
-       end do
-       Spw = Spw*lambda*(1.0D0-lambda)
-       Spw = -Spw
-       RETURN
-       END
+void MATINV33(double A[3][3],double Ainv[3][3])
+{
+  double DET;
+  DET = A[0][0]*A[1][1]*A[2][2]-A[0][0]*A[1][2]*A[2][1]-A[0][1]*A[1][0]
+    *A[2][2]+A[0][1]*A[1][2]*A[2][0]+A[0][2]*A[1][0]*A[2][1]-A[0][2]*A[1][1]
+    *A[2][0];
+  Ainv[0][0]=A[1][1]*A[2][2]-A[1][2]*A[2][1];
+  Ainv[0][1]=A[0][2]*A[2][1]-A[0][1]*A[2][2];
+  Ainv[0][1]=A[0][1]*A[1][2]-A[1][1]*A[0][2];
+  Ainv[1][0]=A[1][2]*A[2][0]-A[2][2]*A[1][0];
+  Ainv[1][1]=A[0][0]*A[2][2]-A[2][0]*A[0][2];
+  Ainv[1][2]=A[0][2]*A[1][0]-A[0][0]*A[1][2];
+  Ainv[2][0]=A[1][0]*A[2][1]-A[1][1]*A[2][0];
+  Ainv[2][1]=A[0][1]*A[2][0]-A[0][0]*A[2][1];
+  Ainv[2][2]=A[0][0]*A[1][1]-A[0][1]*A[1][0];
+    for (i=0; i < 3; i++)
+      for (j=0; j < 3; j++)
+	Ainv[i][j]=Ainv[i][j]/DET;
+
+}
+void SpwDer(double lambda,double *Sl, double *SlP, int CALCSl, int CALCSlP);
+void SpwDer_wrap(double x, double *Sl, double *SlP, int CALCSl, int CALCSlP)
+{
+  SpwDer(x,Sl,SlP,CALCSL,CALCSlP);
+}
+double Spw_wrap(double x)
+{
+  double x;
+  return Spw(x);
+}
+void SpwDer(double lambda, double *Sl, double *SlP)
+{
+  int a,i,j,n;
+  double  GinvR[3], B[3],H[3][3];
+  double Ginv[3][3],G[3][3],Ainv[3][3],Binv[3][3];
+  double R[3];
+  for (a=0; a < 3; a++)
+    R[a] = COMB[a]-COMA[a];
+
+  for (i=0; i < 3; i++)
+    {
+      for (j=0; j < 3; j++)
+      	{
+ 	  Ainv[i][j]=0.;
+ 	  Binv[i][j]=0.;
+ 	  for (n=0; n < 3; n++)
+ 	    {
+ 	      Ainv[i][j]=Ainv[i][j]+saA[n]**2*RA[n][i]*RA[n][j];
+ 	      Binv[i][j]=Binv[i][j]+saB[n]**2*RB[n][i]*RB[n][j];
+ 	    }
+ 	  G[i][j]=(1.0 - lambda)*Ainv[i][j]+lambda*Binv[i][j];
+	  H[i][j]=(1.0 - lambda)*(1.0-lambda)*Ainv[i][j]-(lambda*lambda)*Binv[i][j];
+  	}
+    }
+  if (USEDIRINV)
+    MATINV33(G,Ginv);
+  else
+    MATINV(G,Ginv);
+
+  for (i=0; i < 3; i++)
+    {
+      GinvR[i]=0.0;
+      for (j=0; j < 3; j++) 
+	GinvR[i]=GinvR[i]+Ginv[i][j]*R[j];
+    }
+  /*     se il valore di Slp passato è maggiore di 0 calcola la
+	 derivata di S(lambda) */
+  *SlP = 0.0;
+  for (i=0; i < 3; i++)
+    for (j=0; j < 3; j++)
+      *SlP += GinvR[i]*H[i][j]*GinvR[j]; 
+  *SlP = -*SlP;
+
+  /* calculate also S(lambda) if Sl > 0.0*/
+  *Sl = 0.0;
+  for (i=0 i < 3; i++)
+    *Sl += R[i]*GinvR[i];  
+  *Sl = lambda*(1.0-lambda)*(*Sl);
+  *Sl = -(*Sl);
+}
+double Spw(double lambda)
+{
+  int a,i,j,n;
+  double Spw;
+  double AB[3][3],ABinv[3][3],Ainv[3][3],Binv[3][3];
+  double R[3];
+  for (a=0; a < 3; a++)
+    R[a] = COMB[a]-COMA[a];
+  for (i=0; i < 3; i++)
+    {
+      for (j=0; j < 3; j++)
+	{
+	  Ainv[i][j]=0.;
+	  Binv[i][j]=0.;
+	  for (n=0; n < 3; n++)
+	    {
+	      Ainv[i][j]=Ainv[i][j]+saA[n]*saA[n]*RA[n][i]*RA[n][j]; 
+	      Binv[i][j]=Binv[i][j]+saB[n]*saB[n]*RB[n][i]*RB[n][j];
+	    a}
+	  ABinv[i][j]=(1.0 - lambda)*Ainv[i][j]+lambda*Binv[i][j];
+	}
+    }
+  //      INVERT ABinv(3,3) matrix
+    if (USEDIRINV) 
+      MATINV33(ABinv,AB);
+    else
+      MATINV(ABinv,AB);
+  Spw=0.0;
+  for (i=0; i < 3; i++)
+    {
+      for (j=0; j < 3; j++)
+	{
+	  Spw+=R[i]*AB[i][j]*R[j];
+	}
+    }
+  Spw = Spw*lambda*(1.0-lambda);
+  return -Spw;
+}
 int main (int argc, char** argv)
 {
+  FILE *f;
   if (USEDBRENT) 
     printf("Using DBRENT");
   else
     printf("Using BRENT"); 
   if (READFROMFILE) 
     {
-      OPEN(UNIT=11,FILE='ellips.pos', STATUS='OLD', iostat=irr) 
-	if (irr.ne.0) then
-	  print *, 'BOH...'
-	    pause
-	    end if
-	    READ (11,*,IOSTAT=irr) COMA(1), COMA(2), COMA(3), 
-		 *  RA(1,1), RA(1,2), RA(1,3), RA(2,1), RA(2,2), RA(2,3),
-		 *  RA(3,1), RA(3,2), RA(3,3), saA(1), saA(2), saA(3)
-		   READ (11,*,IOSTAT=irr) COMB(1), COMB(2), COMB(3), 
-		 *  RB(1,1), RB(1,2), RB(1,3), RB(2,1), RB(2,2), RB(2,3),
-		 *  RB(3,1), RB(3,2), RB(3,3), saB(1), saB(2), saB(3)
-		   CLOSE(11)
-		   //      it should be the 6th element read 
-		   print *, 'RA(1,...)=', RA(1,1), RA(1,2), RA(1,3)
-		   print *, 'RA(2,...)=', RA(2,1), RA(2,2), RA(2,3)
-		   print *, 'RA(3,...)=', RA(3,1), RA(3,2), RA(3,3)
-		   print *, 'semiaxes=',saA(1), saA(2), saA(3)
+      f = fopen("ellips.pos","r"); 
+      fscanf(f,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", 
+	     &COMA[0], &COMA[1], &COMA[2], 
+	     &RA[0][0], &RA[0][1], &RA[0][2], &RA[1][0], &RA[1][1], &RA[1][2],
+	     &RA[2][0], &RA[2][1], &RA[2][2], &saA[0], &saA[1], &saA[2]);
+      fscanf(f,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", 
+	     &COMB[0], &COMB[1], &COMB[2], 
+	     &RB[0][0], &RB[0][1], &RB[0][2], &RB[1][0], &RB[1][1], &RB[1][2],
+	     &RB[2][0], &RB[2][1], &RB[2][2], &saB[0], &saB[1], &saB[2]);
+      fclose(f);
+      //      it should be the 6th element read 
+      printf("RA(1,...)=%.15G %.15G %.15G\n", RA[0][0], RA[0][1], RA[0][2]);
+      printf("RA(2,...)=%.15G %.15G %.15G\n", RA[1][0], RA[1][1], RA[1][2]);
+      printf("RA(3,...)=%.15G %.15G %.15G\n", RA[2][0], RA[2][1], RA[2][2]); 
+      printf("semiaxes=%.15G %.15G %.15G\n",saA[0], saA[1], saA[2]);
     }
   else
     {
-      saA(1)=2.;
-	saA(2)=2.;
-	saA(3)=1.;
-	saB(1)=2.,
-	saB(2)=2.;
-	saB(3)=1.;
-	COMA(1)=-1.99;
-	COMA(2)=0.;
-	COMA(3)=0.;
-	COMB(1)=1.99;
-	COMB(2)=0.;
-	COMB(3)=0.;
-	RA(1,1)=1.;
-	RA(1,2)=0.;
-	RA(1,3)=0.;
-	RA(2,1)=0.;
-	RA(2,2)=1.;
-	RA(2,3)=0.;
-	RA(3,1)=0.;
-	RA(3,2)=0.;
-	RA(3,3)=1.;
-	RB(1,1)=1.;
-	RB(1,2)=0.;
-	RB(1,3)=0.;
-	RB(2,1)=0.;
-	RB(2,2)=1.;
-	RB(2,3)=0.;
-	RB(3,1)=0.;
-	RB(3,2)=0.;
-	RB(3,3)=1.;
+      saA[0]=2.;
+      saA[1]=2.;
+      saA[2]=1.;
+      saB[0]=2.;
+      saB[1]=2.;
+      saB[2]=1.;
+      COMA[0]=-1.99;
+      COMA[1]=0.;
+      COMA[2]=0.;
+      COMB[0]=1.99;
+      COMB[1]=0.;
+      COMB[2]=0.;
+      RA[0][0]=1.;
+      RA[0][1]=0.;
+      RA[0][2]=0.;
+      RA[1][0]=0.;
+      RA[1][1]=1.;
+      RA[1][2]=0.;
+      RA[2][0]=0.;
+      RA[2][1]=0.;
+      RA[2][2]=1.;
+      RB[0][0]=1.;
+      RB[0][1]=0.;
+      RB[0][2]=0.;
+      RB[1][0]=0.;
+      RB[1][1]=1.;
+      RB[1][2]=0.;
+      RB[2][0]=0.;
+      RB[2][1]=0.;
+      RB[2][2]=1.;
     } 
-  for (ncall=1; ncall < 1000000; ncall++)
+  for (ncall=0; ncall < 1000000; ncall++)
     {
       if (USEDBRENT) 
 	bret=dbrent(LAM0,LAM1,LAM2,SpwDer_wrap,TOL,xmin);
