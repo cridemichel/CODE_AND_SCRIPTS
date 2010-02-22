aa856fac230fff773bc5d954a9e5cb6772efb0d7
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2010-02-22 10:40:02 +0000

    * small fix
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1180 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/PMW/run_one_pmw_MCvsED.sh
+++ SCRIPTS/PMW/run_one_pmw_MCvsED.sh
@@ -1,5 +1,5 @@
 # $1 = temperature $2 = tau_alpha (in reduced units)
-SETPARAMS="../set_params.sh"
+SETPARAMS="../../set_params.sh"
 TEMP="$1"
 PF="PMW.par"
 LN="PMW-T$1"
@@ -13,24 +13,26 @@ EQTA="2"
 PRTA="10"
 STORERATEPR="0.01"
 BASEPR="1.3"
-NNPR=`echo $TAUALPHA $STORERATEPR $BASEPR | gawk '{printf("%d", 1+log($1/$2)/log($3);}'`
+NNPR=`echo $TAUALPHA $STORERATEPR $BASEPR | gawk '{printf("%d", 1+log($1/$2)/log($3));}'`
+echo "BASEPR="  $BASEPR "NNPR=" $NNPR
 #translational and rotational time step to fulfill DSE and SE
 TRATS="0.212207"
 ROTTS="0.063662"
 #10 * tau_alpha
 STEPS=`echo "" | gawk -v ta=$TAUALPHA -v trats=$TRATS -v eqta=$EQTA '{printf("%d",eqta*ta/trats);}'` 
-echo "TEMP=$1 STEPS=$STEPS STORERATE=$STORERATE TAUALFA=$TAUALPHA" 
+echo "TEMP=$1 STEPS=$STEPS STORERATE=$STORERATEPR TAUALFA=$TAUALPHA" 
 if [ ! -e T$TEMP ]
 then
 mkdir T$TEMP
 fi
 cd T$TEMP
+cp ../$INIF .
 cp ../$PF .
-VS=`echo $TAUALPHA 100 $TS | awk '{printf("%d",$1/$2/$3)}'`
+VS=`echo $TAUALPHA 100 $TRATS | awk '{printf("%d",$1/$2/$3)}'`
 #EQUILIBRATURA
-$SETPARAMS $PF  intervalSum 50.0 storerate 0.0 Dt $TRATS DtR $ROTTS stepnum  $STEPS inifile $INIF NN 1 base 1 baksteps 0 VSteps $VS temperat $TEMP
+$SETPARAMS $PF  intervalSum 50.0 storerate -1.0 Dt $TRATS DtR $ROTTS stepnum  $STEPS inifile $INIF NN 1 base 1 baksteps 0 VSteps $VS temperat $TEMP
 ln -sf $EXEF $LNEQ
-./$LN -fa $PF > screenEQ &
+./$LN -fa $PF > screenEQ 
 #store potential energy to later check equilibration
 cp energy.dat energyEQ.dat
 #store parameter file for equilibration
@@ -39,5 +41,5 @@ cp $PF EQ-$PF
 STEPS=`echo "" | gawk -v ta=$TAUALPHA -v trats=$TRATS -v prta=$PRTA '{printf("%d",prta*ta/trats);}'` 
 $SETPARAMS $PF  intervalSum 50.0 storerate $STORERATEPR base $BASEPR NN $NNPR Dt $TRATS DtR $ROTTS stepnum  $STEPS inifile $INIF baksteps 0 VSteps $VS temperat $TEMP
 ln -sf $EXEF $LN 
-./$LN -fa $PF > screen &
+./$LN -fa $PF > screen 
 cd ..
