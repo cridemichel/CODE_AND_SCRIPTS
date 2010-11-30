18b48bf480044fb98492fddd42e2069da5d843c1
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2010-11-30 09:32:55 +0000

    * lo script calc_avg.sh ora calcola anche i dati utili a paolo per i fit
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1420 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/HEFLEX/calc_avg.sh
+++ SCRIPTS/HEFLEX/calc_avg.sh
@@ -1,5 +1,6 @@
 export LC_NUMERIC=C
 FN="all_pts.dat"
+DELOSF="iggres_vs_sig.dat"
 RA="eqconst.dat"
 #numero di Avogadro
 Nav="6.023E23"
@@ -11,6 +12,7 @@ then
 DN="half"
 fi
 echo -n "" > $FN
+echo -n "" > $DELOSF
 for f in `cat $1`
 do
 cd $f
@@ -34,8 +36,11 @@ SIG=`echo $f | awk -F '/' '{print $2}' | awk -F '_' '{print $2}'`
 #echo "CF=" $CF "CFS=" $CFS
 echo "NP="$NP "CF=" $CF " CFS=" $CFS "NANT=" $NANT
 echo $BINI $SIG `cat bi-mono-bonds.dat | LC_NUMERIC=C awk -v np="$NP" -v nant="$NANT" -v cf="$CF" -v cfs="$CFS" '{if (NR >= np) {B+=$2; sigB+=$2*$2; mono+=$3; bi+=$4; cc++}} END{print (B/cc/cf, mono/cc/cfs, bi/cc/cfs, (nant-(mono/cc)-(bi/cc)*2)/cfs, (bi/cc+mono/cc)/nant, nant/cfs, sqrt(sigB/cc-B*B/(cc*cc))/(B/cc),mono/cc/250,bi/cc/250)}'` >> ../../$FN
+echo $SIG `cat bi-mono-bonds.dat | LC_NUMERIC=C awk -v np="$NP" -v nant="$NANT" -v vol="$V" -v sur="$S" 'BEGIN {BINI=-1} {if (NR >= np) {if (BINI==-1) BINI=$2+$3+$4;  B+=$2; sigB+=$2*$2; mono+=$3; bi+=$4; cc++}} END{print (BINI, B/cc, mono/cc+bi/cc, mono/cc, bi/cc, BINI/vol, BINI/sur, (mono/cc+bi/cc)/sur)}'` >> ../../$DELOSF
 cd ..
 cd ..
 done
 cat $FN | sort -k 1 -n > sorted$FN
-cat sorted$FN | LC_NUMERIC=C awk -v LA="$L" -v cf="$CF" -v cfs="$CFS" -v sup="$S" '{D=1-2*33.5*$3*cf/(LA*LA*LA); K1=$4/($3*$6)/(1.0-2.0*33.5*$3*cf/(LA*LA*LA))/(1-0.0*(30.0*$4*cfs/sup-64.0*$5*cfs/sup)); K2=$5/($4*$6*(1-exp(-3.1415*144*$2))); print ($8,K1,K2,$8,$3,D)}' > $RA
+cat $DELOSF|sort -k 1 -n > sorted-$DELOSF
+#cat sorted$FN | LC_NUMERIC=C awk -v LA="$L" -v cf="$CF" -v cfs="$CFS" -v sup="$S" '{D=1-2*33.5*$3*cf/(LA*LA*LA); K1=$4/($3*$6)/(1.0-2.0*33.5*$3*cf/(LA*LA*LA))/(1-0.0*(30.0*$4*cfs/sup-64.0*$5*cfs/sup)); K2=$5/($4*$6*(1-exp(-3.1415*144*$2))); print ($8,K1,K2,$8,$3,D)}' > $RA
+cat sorted$FN | LC_NUMERIC=C awk -v LA="$L" -v cf="$CF" -v cfs="$CFS" -v sup="$S" '{D=1-2*33.5*$3*cf/(LA*LA*LA); K1=$4/($3*$6); K2=$5/($4*$6*(1-exp(-3.1415*144*$2))); print ($8,K1,K2,$8,$3,D)}' > $RA
