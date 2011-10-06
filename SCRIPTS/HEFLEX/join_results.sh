5f40246363eabdc656196b12c953df7d4a82af15
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2011-10-06 13:49:08 +0000

    * smarter method implemented in join_results.sh to estimate equilibration time
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1697 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/HEFLEX/join_results.sh
+++ SCRIPTS/HEFLEX/join_results.sh
@@ -12,6 +12,12 @@ MAXTIME="1000000000"
 else
 MAXTIME="$3"
 fi
+if [ "$4" == "" ]
+then
+PERC="0.30"
+else
+PERC="$4"
+fi
 echo -n "" > _lista_pref_
 echo -n "" > listaJoined
 for f in `cat $1`
@@ -35,7 +41,15 @@ do
 IGG=`echo $f | awk -F '/' '{print $1}'`
 SIG=`echo $f | awk -F '/' '{print $2}'`
 SIGN=`echo $SIG | awk -F R '{print $1}'`
-cat ${f}R*/$FN | awk -v eqtime="$EQTIME" -v maxtime="$MAXTIME" '{if ($1 > eqtime && $1 < maxtime) print $0}'> joined_${IGG}_${SIG}_$FN
+if [ "$EQTIME" == "-1" ]
+then
+LV=`cat ${f}R*/$FN |tail -1| awk '{print $2}'` 
+EQTIME2=`cat ${f}R*/$FN | awk -v lv="$LV" -v perc="$PERC" '{if ($2/lv-1 < perc && $2/lv-1 > -perc) {print $1; exit;}}'`
+echo "LV=" $LV " EQTIME=" $EQTIME
+else
+EQTIME2="$EQTIME"
+fi 
+cat ${f}R*/$FN | awk -v eqtime="$EQTIME2" -v maxtime="$MAXTIME" '{if ($1 > eqtime && $1 < maxtime) print $0}'> joined_${IGG}_${SIG}_$FN
 cd $IGG
 if [ ! -e $SIGN ]
 then
