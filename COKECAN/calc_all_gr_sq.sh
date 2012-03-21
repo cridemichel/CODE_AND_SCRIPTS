fe18433bb03adc6a344ecb8ba177d00b4aa97df5
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2012-03-21 11:48:26 +0000

    * new script
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1764 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ COKECAN/calc_all_gr_sq.sh
@@ -0,0 +1,18 @@
+TF="threshold.dat"
+SQ="../calcSqDNAD"
+GR="../calcgrDNAD"
+LC="listacnf"
+for f in PHI*
+do
+cd $f
+if [ -e "$TF" ]
+then
+THR=`cat $TF`
+else
+THR="10000"
+fi
+ls Cnf*[^gz] | awk -F _ -v thr=$THR '{if ($3 > thr) print $0}' | sort -t _ -k 3 -n > $LC
+$GR -nn -mc $LC
+$SQ  -c $LC 0 150
+cd ..
+done
