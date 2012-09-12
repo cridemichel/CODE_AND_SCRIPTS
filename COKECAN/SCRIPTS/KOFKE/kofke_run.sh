260a4d3ad798f1cbf45a8859ea44ec97f491ce19
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2012-09-12 12:31:55 +0000

    scripts for Kofke
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1769 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ COKECAN/SCRIPTS/KOFKE/kofke_run.sh
@@ -0,0 +1,8 @@
+# $1 = Initial Dens ISO $2 = Initial Dens NEM $3=Initial Pressure 
+# $4 = Initial Temperature
+NP="1000"
+EXE="./sim1statepnt_HC_MCNPT.sh"
+VISOINI="$1"
+VNEMINI="$2"
+PINI="$3"
+TINI="$4"
