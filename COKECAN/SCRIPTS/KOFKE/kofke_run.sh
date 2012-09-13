d8536a2f9936b1c73b78fdfeba93368253cc6360
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2012-09-13 10:51:38 +0000

    working on kofke script
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1770 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- COKECAN/SCRIPTS/KOFKE/kofke_run.sh
+++ COKECAN/SCRIPTS/KOFKE/kofke_run.sh
@@ -1,8 +1,140 @@
-# $1 = Initial Dens ISO $2 = Initial Dens NEM $3=Initial Pressure 
-# $4 = Initial Temperature
+#To start Kofke run create the following files:
+# BetaCur.dat : 1/(initial temperature)
+# BetaEnd.dat : 1/(final temparature)
+# Pcur.dat: initial pressure
+# VolIniIso.dat: initial volume for iso phase
+# VolIniNem.dat: initial volume for nem phase
 NP="1000"
-EXE="./sim1statepnt_HC_MCNPT.sh"
-VISOINI="$1"
-VNEMINI="$2"
-PINI="$3"
-TINI="$4"
+EXES="../sim1statepnt_HC_MCNPT.sh"
+CNFCURI="startCurIso.cnf"
+CNFCURN="startCurNem.cnf"
+SCNF="start.cnf"
+BFN="BetaCur.dat"
+PFN="Pcur.dat"
+DELB="0.1" #delta beta = h in kofke paper
+KFN="EneVolKofke.dat"
+KFNPRED="EneVolKofkePred.dat"
+PCUR=`cat $PFN`
+KSFN="kstat.dat"
+EQSTEPS="50000"
+PARF="ellipsoid_flex_mc.par"
+VINI_ISO=`cat VolIniIso.dat`
+VINI_NEM=`cat VolIniNem.dat`
+echo "predictor" > $KSFN 
+BETACUR=`cat $BFN`
+BETAEND=`cat BetaEnd.dat`
+while [ $BCUR -lt $BETAEND ]
+do 
+LAST=`tail -1 $KFN`
+PREV=`tail -2 $KFN | head -1`
+PPREV=`tail -3 $KFN | head -1`
+P0=`echo $LAST | awk '{print $2}'`
+V0I=`echo $LAST | awk '{print $3}'`
+U0I=`echo $LAST | awk '{print $4}'`
+V0N=`echo $LAST | awk '{print $5}'`
+U0N=`echo $LAST | awk '{print $6}'`
+NP=`echo $LAST | awk '{print $7}'`
+BETA0=`echo $LAST | awk '{print $1}'`
+F0=`echo "-($U0N-$U0I + $P0*($V0N-$V0I))/($BETA0*$P0*($V0N-$V0I))"|bc -l`
+Pm1=`echo $PREV | awk '{print $2}'`
+Vm1I=`echo $PREV | awk '{print $3}'`
+Um1I=`echo $PREV | awk '{print $4}'`
+Vm1N=`echo $PREV | awk '{print $5}'`
+Um1N=`echo $PREV | awk '{print $6}'`
+BETAm1=`echo $PREV | awk '{print $1}'`
+Fm1=`echo "-($Um1N-$Um1I + $Pm1*($Vm1N-$Vm1I))/($BETAm1*$Pm1*($Vm1N-$Vm1I))"|bc -l`
+Pm2=`echo $PREV | awk '{print $2}'`
+Vm2I=`echo $PREV | awk '{print $3}'`
+Um2I=`echo $PREV | awk '{print $4}'`
+Vm2N=`echo $PREV | awk '{print $5}'`
+Um2N=`echo $PREV | awk '{print $6}'`
+BETAm2=`echo $PREV | awk '{print $1}'`
+Fm2=`echo "-($Um2N-$Um2I + $Pm2*($Vm2N-$Vm2I))/($BETAm2*$Pm2*($Vm2N-$Vm2I))"|bc -l`
+if [ "$KSTAT" == "predictor" ]
+then
+if [ "$PREV" == "" ]
+then
+#use trapezoid formula at begin
+NEWP=`echo "$P0*e($DELB*$F0*0.5)" | bc -l`
+echo $NEWP > $PFN
+NEWBETA=`echo "$BETA0+$DELB"| bc -l`
+echo "$NEWBETA" > $BFN
+else
+#use midpoint formula 
+NEWP=`echo "$Pm1*e($DELB*$Fm1*2.0)" | bc -l`
+echo $NEWP > $PFN
+NEWBETA=`echo "$BETA0+$DELB"| bc -l`
+echo "$NEWBETA" > $BFN
+fi
+else
+#CORRECTOR HERE
+PPREV="`tail -3 $KFN | head -1`"
+Pp1=`echo $KFNPRED | awk '{print $2}'`
+Vp1I=`echo $KFNPRED | awk '{print $3}'`
+Up1I=`echo $KFNPRED | awk '{print $4}'`
+Vp1N=`echo $KFNPRED | awk '{print $5}'`
+Up1N=`echo $KFNPRED | awk '{print $6}'`
+if [ "$PREV" == "" ]
+then
+#use trapezoid formula at begin
+NEWP=`echo "$P0*e($DELB*($Fp1+$F0)*0.5)" | bc -l`
+echo $NEWP > $PFN
+#NOTE: beta is the same as in predictor stage 
+else
+#use midpoint formula 
+NEWP=`echo "$Pm1*e($DELB*($Fp1+4.0*$F0+$Fm1)/3.0)" | bc -l`
+echo $NEWP > $PFN
+#NOTE: beta is the same as in predictor stage 
+fi
+fi
+BETACUR=`cat $BFN`
+ISOD="ISO-Beta-$BETACUR"
+NEMD="ISO-Beta-$BETACUR"
+EXEI="kofkeI-$BETACUR"
+EXEN="kofkeN-$BETACUR"
+RUNI=`ps ax | grep $EXEI`
+RUNN=`ps ax | grep $EXEN`
+PCUR=`cat $PFN`
+KSTAT=`cat $KSFN`
+cd $ISOD
+if [ "$RUNI" == "" ] then
+if [ \( -e "COORD_TMP0" \) -o \( -e "COORD_TMP1" \) ]
+then
+$EXEI -c >> screen
+else
+cp ../$CNFCURI $SCNF
+TCUR=`echo "1.0/$BETACUR"| bc -l`
+$EXES $PCUR $TCUR $EQSTEPS
+fi
+fi
+cd ..
+cd $NEMD
+if [ "$RUNM" == "" ] then
+if [ \( -e "COORD_TMP0" \) -o \( -e "COORD_TMP1" \) ]
+then
+$EXEM -c >> screen
+else
+cp ../$CNFCURN $SCNF
+$EXES $PCUR $TCUR $EQSTEPS
+fi
+cd ..
+#wait for current iso and nem sims to finish here
+do
+IS=`cat $ISOD/status.dat`
+NS=`cat $NEMD/status.dat`
+sleep 30
+while [ "$IS" == "running" -o "$NS" == "running" ]
+#calculate running averages here and update Kofke file
+
+#calculate new pressure here according to some integration scheme (e.g. trapezoid, midpoint, etc.)
+#KFN:
+#<beta> <pressure> <av. volume ISO> <av. energy ISO> <av. vol. NEM> <av. energy NEM> <#particles> 
+if [ "$KSTAT" == "predictor" ]
+then
+echo "$BETACUR $PCUR $AVVOLISO $AVENEISO $AVVOLNEM $AVENENEM $NP" > $KFNPRED
+echo "corrector" > $KSTAT
+else
+echo "$BETACUR $PCUR $AVVOLISO $AVENEISO $AVVOLNEM $AVENENEM $NP" >> $KFN
+echo "predictor" > $KSTAT
+fi
+done
--- COKECAN/SCRIPTS/KOFKE/sim1statepnt_HC_MCNPT.sh
+++ COKECAN/SCRIPTS/KOFKE/sim1statepnt_HC_MCNPT.sh
@@ -1,59 +1,27 @@
 # $1 =pressure
-# $2 = cicli di produzione (1 ciclo = 1 tempo di equilibratura) (se < 0 non fa la produzione)
-# $3 = elongazione
-# $4 = temperatura
+# $2 = temperature 
+# $3 = steps
 FIXVB="1"
 if [ "$1" = "" ]
 then
-echo "Syntax: sim1statepnt <pressure> <steps> [elongation] [temperature]"
+echo "Syntax: sim1statepnt <pressure> <temperature> <steps>"
 exit 0
 fi
+PRESS="$1"
 if [ "$2" = "" ]
 then
-echo "You have to supply the number of steps"
+echo "You have to supply the temperature"
 exit 0
 fi
+TEMP="$2"
 if [ "$3" = "" ]
 then 
-CDIR=`pwd`
-DIR=`basename $CDIR`
-EL=`echo $DIR | awk -F _ '{print $2}'`
-#echo "You have to supply an elongation!"
-#echo "Elongazione: $EL"
-#exit 0
+STEPS="10000"
 else
-EL=$3
+STEPS="$3"
 fi
-if [ "$4" == "" ]
-then
-TEMP="0.15"
-else
-TEMP="$4"
-fi
-if [ "$5" == "" ]
-then
-SIGMA="0.9"
-else
-SIGMA="$5"
-fi
-if [ "$6" != "" ]
-then
-FIXVB="$6"
-fi
-if [ "$FIXVB" == "0" ]
-then
-DIRSIM="sigma_${SIGMA}_noFixVb_Press$1_T$TEMP"
-elif [ "$FIXVB" == "2" ]
-then
-DIRSIM="sigma_${SIGMA}_modB_Press$1_T$TEMP"
-if [ "$SIGMA" == "-1" ]
-then
-SIGMA="0.608333"
-fi
-else
-DIRSIM="sigma_${SIGMA}_Press$1_T$TEMP"
-fi
-PARFILE="ellipsoid_flex.par"
+PARFILE="ellipsoid_flex_mc.par"
+DIRSIM="T-$TEMP"
 if [ ! -e $DIRSIM ]
 then 
 mkdir $DIRSIM
@@ -65,101 +33,20 @@ rm -f Store-*
 PRESS="$1"
 ELLEXE="../../ellipsoid"
 INITEMP="2.0"
-SIMPR="ellMC-NPT${EL}PR$1T${TEMP}SIG$SIGMA"
+SIMPR="HCNPT-X0-${EL}-P${PRESS}-T${TEMP}"
 MOSRUN="mosrun"
 #per ora il salvataggio è lineare
 #=========== >>> PARAMETRI <<< =============
 STORERATE="50.0"
 USENNL=1
 INIFILE="start.cnf"
-PARNUM=1000
-DT="0.05"
-NNPR=1
-BASEPR=1
-INTSUM="20.0"
-OCTAVE="octave"
-OCTFILE="script.oct"
+PARNUM="1000"
 #============================================
 #N.B. it's supposed that we use NNL here!!
-PROL=`echo $EL | awk '{if ($0 >= 1.0) printf("1"); else printf("0");}'`
-if [ $PROL -eq 1 ]
-then
-A0=$EL
-B0=1.0
-C0=1.0
-RNNL=0.3
-#INIL=`echo "2.0*e(1.0/3.0*l($PARNUM))*$A0" | bc -l`
-#echo "qui INIL=" $INIL
-if [ $USENNL -eq 0 ]
-then
-RCUT=`echo "2.0*$A0*1.01" | bc -l` 
-fi
-else
-A0="1.0"
-B0="$EL" 
-C0="$EL"
-RNNL=0.3
-#INIL=`echo "5.0*e(1.0/3.0*l($PARNUM))*$B0" | bc -l`
-if [ $USENNL -eq 0 ]
-then
-RCUT=`echo "2.0*$B0*1.01" | bc -l` 
-NNLPAR="0"
-fi
-fi
 cp ../$INIFILE .
-#============ >>> set particles number and elongation in $INIFILE <<< ==================
-cat $INIFILE | awk -v np=$PARNUM -v a=$A0 -v b=$B0  -v c=$C0 ' BEGIN {NAT=0} {if (NAT==1 && NR==NL+1) {print np;} else if (NAT==1 && NR==NL+2) { print (a,b,c);} else {print $0;} if ($0=="@@@") {NAT+=1; NL=NR} }' > _aaa_
-mv _aaa_ $INIFILE
-#============ >>> adjust spot according to $SIGMA <<< =============
-if [ "$6" != "" ]
-then
-VB="0.00706858347057703"
-echo 'function y=sphCap(h)' > $OCTFILE
-echo 'y=2*acos(0)*h^2*(3*'$SIGMA'/2-h)/3-'$VB';' >> $OCTFILE
-echo "endfunction" >> $OCTFILE
-echo '[h, info]'"= fsolve (\"sphCap\",""$SIGMA"'/4)' >> $OCTFILE
-echo "printf(\"hh %.8G\n\",h);" >> $OCTFILE
-$OCTAVE -q $OCTFILE > _aaa_
-SIG2=`echo $SIGMA | awk '{print $1/2}'`
-HVAL=`cat _aaa_ | awk '{if ($1=="hh") print $2}'` 
-ISNEG=`echo $HVAL |awk '{if ($1 <= 0) print "1"; else print "0";}'`
-ISBIG=`echo $HVAL |awk -v sig2=$SIG2 '{if ($1 > sig2) print "1"; else print "2";}'`
-if [ "$ISNEG" == "1" ]
-then
-echo "HVAL is negative ( HVAL=" $HVAL " ) exiting!"
-exit
-fi
-if [ "$ISBIG" == "1" ]
-then
-echo "HVAL is too big ( HVAL=" $HVAL " ) exiting!"
-exit
-fi
-if [ "$FIXVB" == "0" ]
-then
-DEL="$EL"
-elif [ "$FIXVB" == "2" ]
-then
-DEL=`echo "0.15"|awk -v el=$EL -v sig=$SIGMA '{printf("%.8G",el-(sig/2-$1));}'`
-else
-DEL=`echo $HVAL | awk -v el=$EL -v sig=$SIGMA '{printf("%.8G",el-(sig/2-$1));}'`
-fi
-echo "DEL= " $DEL " HVAL= " $HVAL
-cat $INIFILE | awk -v del=$DEL -v sig=$SIGMA -v el=$EL 'BEGIN {NAT=0} {if (NAT==1 && NR==NL+6) {printf("%.8G 0.0 0.0 %.8G\n", del, sig);} else if (NAT==1 && NR==NL+7) { printf("%.8G 0.0 0.0 %.8G\n", -del, sig);} else {print $0;}; if ($0=="@@@") {NAT+=1; NL=NR}}' > _aaa_
-mv _aaa_ $INIFILE
-fi
 #==================================================================
-if [ $USENNL -eq 1 ]
-then
-#usa le NNL con sticky spots!
-NNLPAR="3"
-#RCUT=`echo "2.0*e(0.5*l(($A0+$RNNL)*($A0+$RNNL)+($B0+$RNNL)*($B0+$RNNL)+($C0+$RNNL)*($C0+$RNNL)))" | bc -l`
-RCUT="-1"
-fi
-echo "RCUT=" $RCUT " " "A=" $A0 "B=" $B0 "C=" $C0 "RNNL=" $RNNL "EL=" $EL
-#RANDOMIZZAZIONE INIZIALE
-#cp $PARFILE rand_$PARFILE
-rm -f $OCTFILE
-../set_params.sh $PARFILE temperat $TEMP P $PRESS bakStepAscii 5000 stepnum $2 inifile $INIFILE endfile ${SIMPR}.cor
+echo "Simulating T=" $TEMP
+../set_params.sh $PARFILE temperat $TEMP ensembleMC 1 useNNL 0 rcut $RCUT P $PRESS bakStepAscii 5000 stepnum $STEPS inifile $INIFILE endfile ${SIMPR}.cor
 ln -sf $ELLEXE $SIMPR
 $MOSRUN ./$SIMPR -fa ./$PARFILE > screen_$SIMPR 
 cd ..
