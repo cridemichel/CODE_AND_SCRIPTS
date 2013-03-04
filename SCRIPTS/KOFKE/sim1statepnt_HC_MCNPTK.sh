c0ec74312cc2d76479cc00fe5f9eb338dbe91872
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2013-03-04 20:53:44 +0000

    * now perform vol e tra/rot adjustment at begin
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1822 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/KOFKE/sim1statepnt_HC_MCNPTK.sh
+++ SCRIPTS/KOFKE/sim1statepnt_HC_MCNPTK.sh
@@ -1,7 +1,14 @@
 # $1 =pressure
-# $2 = temperature 
-# $3 = steps
+# $2 = inverse temperature
+# $3 = executable name 
+# $4 = steps
+# $5 = 1 (default) -> adjust volume and transl/rot displac to have 50% acceptance, 0 -> do not do that
 VSTEPS="50"
+TARGVOL="0.50"
+TARGRT="0.50"
+ADJSTEPS="10000"
+RESETRT="500"
+RESETVOL="500"
 alias awk='LANG=C awk'
 if [ "$1" = "" ]
 then
@@ -27,6 +34,12 @@ STEPS="10000"
 else
 STEPS="$4"
 fi
+if [ "$4 = "" ]
+then
+DOADJ="1"
+else
+DOADJ="$4"
+fi
 PARFILE="ellipsoid_flex_mc.par"
 DIRSIM="P-$PRESS"
 INIFILE="start.cnf"
@@ -49,10 +62,18 @@ PARNUM="1000"
 #cp ../$INIFILE .
 #==================================================================
 echo "[ " EXE:$EXENAME " ] Simulating BETA=" $BETA " P=" $PRESS
-../set_params.sh $PARFILE temperat $TEMP ensembleMC 1 useNNL 0 rcut -1 P $PRESS bakStepsAscii 100000 stepnum $STEPS inifile $INIFILE endfile ${EXENAME}.cor VSteps $VSTEPS
+if [ "$DOADJ" == "1" ]
+then
+../set_params.sh $PARFILE temperat $TEMP adjstepsMC $ADJSTEPS targetAccept $TARGVOL targetAcceptVol $TARGRT ensembleMC 1 useNNL 0 rcut -1 P $PRESS bakStepsAscii 100000 stepnum $STEPS resetacceptVol $RESETVOL resetaccept $RESETRT inifile $INIFILE endfile ${EXENAME}.cor VSteps $VSTEPS
+#../set_params.sh $PARFILE inifile start.cnf
+ln -sf $ELLEXE $EXENAME
+$MOSRUN ./$EXENAME -fa ./$PARFILE > screen_$SIMPR 
+else
+../set_params.sh $PARFILE temperat $TEMP targetAccept -1 targetAcceptVol -1 ensembleMC 1 useNNL 0 rcut -1 P $PRESS bakStepsAscii 100000 stepnum $STEPS inifile $INIFILE endfile ${EXENAME}.cor VSteps $VSTEPS
 #../set_params.sh $PARFILE inifile start.cnf
 ln -sf $ELLEXE $EXENAME
 $MOSRUN ./$EXENAME -fa ./$PARFILE > screen_$SIMPR &
+fi
 if [ "$MOSRUN" = "mosrun" ] 
 then
 sleep 5
