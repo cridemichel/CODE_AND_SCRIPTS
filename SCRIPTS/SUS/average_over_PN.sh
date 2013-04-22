3b5d60d5efe7394807510006eaa993bfdbf77b82
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2013-04-22 10:31:27 +0000

    * new check-in
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1830 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/SUS/average_over_PN.sh
+++ SCRIPTS/SUS/average_over_PN.sh
@@ -7,7 +7,7 @@ cat $1  | LC_NUMERIC=C gawk  -v min=$2  'BEGIN {s=0} {if ($1 < min){s+=$1*$2; no
 else
 cat $1 | awk '{printf("%d %s\n",$1,$2)}' > $PNN
 join -j 1 $PNN $3 > $JF
-cat $JF | LC_NUMERIC=C gawk  -v min=$2  'BEGIN {s=0; s2=0} {if ($1 < min){s+=$3*$2; norm+=$2;} else {s2+=$3*$2; norm2+=$2}} END {printf("iso avg=%f nem avg=%f\n",s/norm,s2/norm2)}'
+cat $JF | LC_NUMERIC=C gawk  -v min=$2  'BEGIN {norm=0; norm2=0; s=0; s2=0; Niso=0; Nnem=0;} {if ($1 < min){s+=$3*$2; norm+=$2; Niso+=$2*$1} else {s2+=$3*$2; norm2+=$2; Nnem+=$2*$1}} END {printf("iso avg=%f nem avg=%f\n",s/norm,s2/norm2); printf("avg N iso=%f  avg N nem=%f\n", Niso/norm, Nnem/norm2);}'
 #rm -f $JF
 #rm -f $PNN
 fi
