fd33f376a4b813b643b33c4b9b25f606c9a666fc
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2013-11-06 15:01:41 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@1951 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- BENTCYL/calc_vol.c
+++ BENTCYL/calc_vol.c
@@ -38,9 +38,10 @@ double calc_norm(double *vec)
 double Lhc, Dhc=2.0;
 double PI;
 double Lx=5, Ly=5, Lz=5;
-void main(int argc, char** argv)
+int main(int argc, char** argv)
 {
   FILE *f;
+  int dontcheck;
   char fn[255];
   double angle, norm, pos1[3], pos2[3];
   double n1[3], n2[3], vp[3], fact, delx, rp[3], drp[3];
@@ -55,7 +56,7 @@ void main(int argc, char** argv)
     maxtrials=atoi(argv[2]);
   if (argc>2)
     outits=atoi(argv[3]);
-  printf("maxtrials=%ld\n", maxtrials);
+  printf("maxtrials=%lld\n", maxtrials);
   PI = 2.0*acos(0.0);
   angle=PI*(180.0 - atof(argv[1]))/180.0;
   n1[0] = cos(angle);
@@ -67,21 +68,24 @@ void main(int argc, char** argv)
 
   delx = tan(angle/2.0)*Dhc/2.0; 
   Lhc = (Dhc + delx);
+  printf("delx=%f\n", delx);
   /* com1 = uvec[\[Theta]] (X0*D*0.5 - delx*0.5); */
   fact= Dhc*0.5 - delx*0.5;
   pos1[0]=n1[0]*fact;
   pos1[1]=n1[1]*fact;
   pos1[2]=n1[2]*fact;
+  printf("n=%f %f %f pos1=%f %f %f (norm=%f)\n", n1[0], n1[1], n1[2], pos1[0], pos1[1], pos1[2], calc_norm(pos1));
   /* com2 = {-(X0 D /2 - delx*0.5), 0, 0};*/
   pos2[0]=-(Dhc/2.0 - delx*0.5);
   pos2[1]=0.0;
   pos2[2]=0.0;
   
+  printf("n=%f %f %f pos2=%f %f %f (norm=%f)\n", n2[0], n2[1], n2[2], pos2[0], pos2[1], pos2[2], calc_norm(pos2));
   overlaps=0;
   while (tt < maxtrials)
     {
       if (tt % outits == 0 && overlaps!=0)
-	printf("bhc vol= %.15G ( %ld/%ld) \n", (((double)overlaps)/((double)tt))*Lx*Ly*Lx, overlaps, tt);
+	printf("bhc vol= %.15G ( %lld/%lld) \n", (((double)overlaps)/((double)tt))*Lx*Ly*Lz, overlaps, tt);
 
       /* pick a random point */
       rp[0] = 0.5*Lx*(2.0*ranf()-1.0);
@@ -90,26 +94,34 @@ void main(int argc, char** argv)
       /* check overlap here */
       for (i=0; i < 3; i++)
 	drp[i] = rp[i] - pos1[i];
+      dontcheck=0;
       if (fabs(scalProd(drp, n1)) < Lhc * 0.5)
 	{
 	  vectProdVec(drp, n1, vp);
 	  norm=calc_norm(vp);
 	  if (norm < Dhc*0.5)
-	    overlaps+=1;
+	    {
+	      overlaps+=1;
+	      dontcheck=1;
+	    }
 	}
       for (i=0; i < 3; i++)
 	drp[i] = rp[i] - pos2[i];
-      if (fabs(scalProd(drp, n2)) < Lhc * 0.5)
-	{
-	  vectProdVec(drp, n2, vp);
-	  norm=calc_norm(vp);
-	  if (norm < Dhc*0.5)
-	    overlaps+=1;
+      if (dontcheck==0)
+	{	
+	  if (fabs(scalProd(drp, n2)) < Lhc * 0.5)
+	    {
+	      vectProdVec(drp, n2, vp);
+	      norm=calc_norm(vp);
+	      if (norm < Dhc*0.5)
+		overlaps+=1;
+	    }
 	}
       tt++;
     }
   sprintf(fn, "volume-deg%3.0f.dat",angle);
   f = fopen(fn, "w+");
-  fprintf(f, "%.15G\n", (((double)overlaps)/((double)tt))*Lx*Ly*Lx);
+  fprintf(f, "%.15G\n", (((double)overlaps)/((double)tt))*Lx*Ly*Lz);
   fclose(f);
+  return 0;
 }
