2f5315f154eb45c9891eae1e665a45bacad283c7
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2013-12-13 11:45:50 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2036 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- CROSSPOLY/create_KFpoly_conf.c
+++ CROSSPOLY/create_KFpoly_conf.c
@@ -1,18 +1,29 @@
 #include<stdlib.h>
 #include<stdio.h>
 double L, *rx, *ry, *rz;
-
+double *rxc, *ryc, *rzc, rxl , drx, dry, drz, ryl, rzl;
+int full, ibeg, numpoly;
 int main(int argc, char **argv)
 {
   FILE *f;
   double nx, ny, nz, del, del0;
-  int parnum=2800, i, polylen=4;
+  int parnum=2800, i, j, polylen=4;
   f = fopen(argv[1], "w+");
   L = 30.75;
   rx = malloc(sizeof(double)*parnum);
   ry = malloc(sizeof(double)*parnum);
   rz = malloc(sizeof(double)*parnum);
-
+  rxc =malloc(sizeof(double)*polylen);
+  ryc =malloc(sizeof(double)*polylen);
+  rzc =malloc(sizeof(double)*polylen);
+  del=1.51;
+  del0 = 0.0001;
+  for (i=0; i < polylen; i++)
+    {
+      rxc[i] = i;
+      ryc[i] = 0.0;
+      rzc[i] = 0.0;
+    }
   fprintf(f, "parnum: %d\n", parnum);
   fprintf(f,"ninters: 4\n");
   fprintf(f,"nintersIJ: 0\n");
@@ -20,13 +31,14 @@ int main(int argc, char **argv)
   fprintf(f,"saveBonds: 0\n");
   fprintf(f,"maxbondsSaved: -1\n");
   fprintf(f,"@@@\n");
-  fprintF(f, "%d\n", parnum);
+  fprintf(f, "%d\n", parnum);
   fprintf(f,"0.5 0.5 0.5\n");
   fprintf(f,"2 2 2\n");
   fprintf(f, "1 1 1 1 2 0\n");
   fprintf(f,"3 0\n");
   fprintf(f,"0.5 0 0 0.5\n");
   fprintf(f,"-0.5 0 0 0.5\n");
+  fprintf(f,"0.0 0.5 0 0.25\n");
   fprintf(f,"0 0 0 0 1 0 0 100000\n");
   fprintf(f,"0 0 0 1 1 0 0 100000\n");
   fprintf(f,"0 1 0 1 1 0 0 100000\n");
@@ -34,63 +46,58 @@ int main(int argc, char **argv)
   fprintf(f,"@@@\n");  
   nx=ny=nz=0;
   full=0;
-  del=0.0;
-  del0 = 0.0001;
   ibeg = 0;
-  for (i=0; i < parnum; i++)
+  numpoly=parnum/polylen;
+  drx = 4.6;
+  dry = 1.01;
+  drz = 1.01;
+  if (parnum%polylen != 0)
     {
-      if (parnum % polylen == 0)
-	{
-	  del = 1.51;
-	  ibeg = i+1;
-	}
-      else
-	{ 
-	  del = 0.0;
-	}
-      rx[i] = nx+del0+del;
+      printf("number of particles must a multiple of %d\n", polylen);
+      exit(-1);	
+    }
+  for (i=0; i < numpoly; i++)
+    {
+      rxl = rxc[polylen-1]+nx*drx+del;
       nx++;
-      if (rx[i] > L*0.5)
-	{ 
-	  if (del==0.0)
-	    {
-	      for (j=ibeg; j < i; j++)
-		{
-		  ry[j] += 1.0+del0;
- 		}
-	    }
-	  nx--;
-	  rx[i] = nx+del0+del;
-
+      if (rxl > L*0.5)
+	{
 	  ny++;
-	  ry[i] = ny+del0;
-	  if (ry[i] > L*0.5)
-	    {
-	      ny--;
-	      ry[i] = ny+del0;
-	      nz++; 
-	      if (del==0.0)
-		{
-		  for (j=ibeg; j < i; j++)
-		    {
-		      rz[j] += 1.0+del0;
-		    }
-		}
-	      rz[i] = nz+del0;
-	      if (rz[i] > L*0.5)
-		full=1;
-	    }
+	  nx=0;
+	}
+      ryl = ryc[polylen-1]+ny*dry;
+      if (ryl > L*0.5)
+	{
+	  nz++;
+	  ny=0;
+      	}
+      rzl = rxc[polylen-1]+nz*drz;
+      if (rzl  > L*0.5)
+	{
+	  full=1;
+	}
+
+      for (j = 0; j < polylen; j++)
+	{
+	  rx[i*polylen+j] =  rxc[j]+nx*drx+del;
+	  ry[i*polylen+j] =  ryc[j]+ny*dry;
+	  rz[i*polylen+j] =  rzc[j]+nz*drz;
 	}
       if (full==1)
 	{
 	  printf("I could only place %d particles!\n", i);
 	  break;
 	}
-      rx -= L*0.5;
-      ry -= L*0.5;
-      rz -= L*0.5;
-      fprintf("%f %f %f 1 0 0 0 1 0 0 0 1 0\n", rx, ry, rz);
     }
-  fprintf(f, "%.15G %.15G %15G\n", L, L, L);
+//  if (full)
+ //   exit(-1);
+  for (i=0; i < parnum; i++)
+    {
+      rx[i] -= L*0.5;
+      ry[i] -= L*0.5;
+      rz[i] -= L*0.5;
+      fprintf(f, "%f %f %f 1 0 0 0 1 0 0 0 1 0\n", rx[i], ry[i], rz[i]);
+    }
+  fprintf(f, "%.15G %.15G %.15G\n", L, L, L);
   return 1;
 } 
