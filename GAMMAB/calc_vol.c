e99910bb95242ba68200396ca2e033907dc8488e
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2014-03-03 10:44:11 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2127 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- GAMMAB/calc_vol.c
+++ GAMMAB/calc_vol.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<math.h>
 
 #define Sqr(x) ((x)*(x))
 
@@ -7,10 +9,10 @@ char inputfile[4096];
 int numat;
 double *pos[3];
 double *rad, proberad, cells[3];
-double com[3], maxdist[3], L[3], maxrad;
+double com[3], maxdist[3], L[3], L2[3], maxrad;
 double *cellList, *inCell[3];
-long long int outits=100000;
-void rebuildLinkedList(void)
+long long int outits=100000, maxtrials=1000000;
+void build_linked_lists(void)
 {
   int j, n;
 
@@ -20,9 +22,9 @@ void rebuildLinkedList(void)
  
   for (n = 0; n < numat; n++)
     {
-      inCell[0][n] =  (pos[n] + L2[0]) * cells[0] / L[0];
-      inCell[1][n] =  (pos[n] + L2[1]) * cells[1] / L[1];
-      inCell[2][n] =  (pos[n] + L2[2]) * cells[2] / L[2];
+      inCell[0][n] =  (pos[0][n] + L2[0]) * cells[0] / L[0];
+      inCell[1][n] =  (pos[1][n] + L2[1]) * cells[1] / L[1];
+      inCell[2][n] =  (pos[2][n] + L2[2]) * cells[2] / L[2];
       j = (inCell[2][n]*cells[1] + inCell[1][n])*cells[0] + 
 	inCell[0][n] + numat;
       cellList[n] = cellList[j];
@@ -64,7 +66,7 @@ void parse_param(int argc, char** argv)
           cc++;
 	  if (cc == argc)
 	    print_usage();
-	  tt = atoi(argv[cc]);
+	  maxtrials = atoi(argv[cc]);
         }
       else if (!strcmp(argv[cc],"--outits") || !strcmp(argv[cc],"-oi" ))
      	{
@@ -93,7 +95,7 @@ void parse_param(int argc, char** argv)
 void read_file(void)
 { 
   FILE* f;
-  int cc=0;
+  int cc=0, kk;
   f = fopen(inputfile, "r");
   /* count atoms */
   while (!feof(f))
@@ -117,10 +119,12 @@ void read_file(void)
 
 int check_overlap_ij(int i, int j)
 {
-  double rad, radSq;
-  rad = 0.5*(rad[i]+rad[j]);
+  double distSq, radSq, avrad;
+  int kk;
 
-  radSq = Sqr(rad);
+  avrad = 0.5*(rad[i]+rad[j]);
+
+  radSq = Sqr(avrad);
   distSq = 0.0;
   for (kk=0; kk < 3; kk++)
     distSq += Sqr(pos[kk][i] - pos[kk][j]);
@@ -134,6 +138,7 @@ int check_overlap(int ip)
 {
   int kk, nb, k, iZ, jZ, iX, jX, iY, jY, n, na;
   int cellRangeT[6];
+  int cellRange[6];
   double shift[3];
   na=ip;
   nb=-1;
@@ -153,10 +158,10 @@ int check_overlap(int ip)
        * fiels is not present */
       if (jZ == -1) 
 	{
-	  jZ = cellsz - 1;    
+	  jZ = cells[2] - 1;    
 	  shift[2] = - L[2];
 	} 
-      else if (jZ == cellsz) 
+      else if (jZ == cells[2]) 
 	{
 	  jZ = 0;    
 	  shift[2] = L[2];
@@ -168,10 +173,10 @@ int check_overlap(int ip)
 	  shift[1] = 0.0;
 	  if (jY == -1) 
 	    {
-	      jY = cellsy - 1;    
+	      jY = cells[1] - 1;    
 	      shift[1] = -L[1];
 	    } 
-	  else if (jY == cellsy) 
+	  else if (jY == cells[1]) 
 	    {
 	      jY = 0;    
 	      shift[1] = L[1];
@@ -182,15 +187,15 @@ int check_overlap(int ip)
 	      shift[0] = 0.0;
 	      if (jX == -1) 
 		{
-		  jX = cellsx - 1;    
+		  jX = cells[0] - 1;    
 		  shift[0] = - L[0];
 		} 
-	      else if (jX == cellsx) 
+	      else if (jX == cells[0]) 
 		{
 		  jX = 0;   
 		  shift[0] = L[0];
 		}
-	      n = (jZ *cellsy + jY) * cellsx + jX + Oparams.parnum;
+	      n = (jZ *cells[1] + jY) * cells[0] + jX + numat;
 	      for (n = cellList[n]; n > -1; n = cellList[n]) 
 		{
 		  if (n != na && n != nb && (nb >= -1 || n < na)) 
@@ -208,9 +213,12 @@ int check_overlap(int ip)
 }
 int main(int argc, char **argv)
 {
+  FILE *f;
   long long int tt;
   double totov, vol;
-  int MAXTRIALS, kk, i, j;
+  int kk, i, j;
+
+  parse_param(argc, argv);
 
   read_file();
 
@@ -219,7 +227,7 @@ int main(int argc, char **argv)
   for (i = 0; i < numat; i++)
     {
       for (kk=0; kk < 3; kk++)
-	com[kk] + = pos[kk];
+	com[kk] += pos[kk][i];
     }
   for (kk=0; kk < 3; kk++)
     com[kk] /= ((double)numat);
@@ -248,6 +256,7 @@ int main(int argc, char **argv)
   for (kk=0; kk < 3; kk++)
     {
       L[kk] = maxdist[kk] + 2.1*proberad;
+      L2[kk] = 0.2*L[kk];
       cells[kk] = L[kk]/maxrad;
     }
   /* set probe radius */
@@ -255,11 +264,11 @@ int main(int argc, char **argv)
   cellList = malloc(sizeof(int)*(cells[0]*cells[1]*cells[2]+numat));
   inCell[0] = malloc(sizeof(int)*numat);
   inCell[1] = malloc(sizeof(int)*numat);
-  inCell[2] = malloc(sizeof(int)*numar);
+  inCell[2] = malloc(sizeof(int)*numat);
   f = fopen("volume.dat","w+");
   fclose(f); 
   totov=0.0;
-  for (tt=0; tt < MAXTRIALS; tt++)
+  for (tt=0; tt < maxtrials; tt++)
     {
       for (kk = 0; kk < 3; kk++)
 	pos[kk][numat-1] = L[kk]*(drand48()-0.5); 
@@ -273,7 +282,7 @@ int main(int argc, char **argv)
 	      vol = (totov/((double)tt))*(L[0]*L[1]*L[2]);
 	      f=fopen("volume.dat", "a");
 	      printf("volume=%.10f (totov=%f/%lld)\n", vol, totov, tt);
-	      fprintf(f, "%lld %.15G %.15G\n", tt, vol, totene);
+	      fprintf(f, "%lld %.15G %.15G\n", tt, vol, totov);
 	      fclose(f);
 	      sync();
 	    }
