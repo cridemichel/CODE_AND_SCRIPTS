8359028e10ab0fbaf164999d033e379a8b6213ef
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2014-10-16 13:26:33 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2332 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- OPEP/xyz2cri.c
+++ OPEP/xyz2cri.c
@@ -16,45 +16,43 @@ int cmpfunc(const void *a, const void *b)
    return ((struct anatom*)a)->idx - ((struct anatom*)b)->idx;
 }
 char fname[256], strout[1024], line[1024], fnout[1024];
-struct anatom p;
+struct anatom *p;
 int numatoms, natprot;
 
 int main(int argc, char **argv)
 {
   FILE *fin, *fout;
-  int nr=0, i, ifirst, jj, j, nframe;
+  int i, ifirst, jj, j, nframe, nhdr;
   double dx, dy, dz;
   strcpy(fname,argv[1]);
   fin=fopen(fname,"r");
   nframe=0;
 
+  nhdr=2; /* numero di linee che costituiscono l'header del file OPEP */
   fscanf(fin, "%d ", &numatoms);
   fscanf(fin,"%[^\n] ",line);
   rewind(fin);
+  p=(struct anatom*)malloc(sizeof(struct anatom)*numatoms);
+
   while (!feof(fin))
     {
-      if (nr % numatoms == 0)
-	{
-	  sprintf(fnout,"Store-%d", nframe);
-	  fout = fopen(fnout, "w+");
-	  fprintf(fout, "parnum:%d\n", numatoms);
-	  fprintf(fout, "@@@\n");
-	}
+      sprintf(fnout,"Store-%d", nframe);
+      fout = fopen(fnout, "w+");
+      fprintf(fout, "parnum:%d\n", numatoms);
+      fprintf(fout, "@@@\n");
+	
       fscanf(fin,"%[^\n] ",line);
-      
-      if (nr % numatoms > 1)
-	{
-	  sscanf(line, "%s %lf %lf %lf %d ", p.resname, &(p.x), &(p.y), &(p.z),
-		 &(p.idx));
-  	  fprintf(fout, "%lf %lf %lf\n", p.x, p.y, p.z);
-	}
+      fscanf(fin,"%[^\n] ",line);
+
+      for (i=0; i < numatoms; i++) 
+	sscanf(line, "%s %lf %lf %lf %d ", p[i].resname, &(p[i].x), &(p[i].y), &(p[i].z),
+	       &(p[i].idx));
+      qsort(p, numatoms,sizeof(struct anatom), cmpfunc);
+      for (i=0; i < numatoms; i++) 
+	fprintf(fout, "%12s %12lf %12lf %12lf %12d\n", p[i].resname, p[i].x, p[i].y, p[i].z, p[i].idx);
       //fprintf(fout, "%s\n", line);
-      nr++;
-      if (nr % numatoms ==0)
-	{
-	  fclose(fout);
-	  nframe++;
-	}	
+      fclose(fout);
+      nframe++;
     }
   /* sorting particles */
   fclose(fin);
