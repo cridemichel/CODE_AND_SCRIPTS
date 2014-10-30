1d6e63db2eb41768463767e036bc7352d4081d8c
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2014-10-30 10:13:47 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2341 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- OPEP/xyz2cri.c
+++ OPEP/xyz2cri.c
@@ -20,7 +20,7 @@ int cmpfunc(const void *a, const void *b)
 char fname[256], strout[1024], line[1024], fnout[1024];
 struct anatom *p;
 int numatoms, natprot;
-
+double comx, comy, comz;
 int main(int argc, char **argv)
 {
   FILE *fin, *fout;
@@ -36,13 +36,19 @@ int main(int argc, char **argv)
   rewind(fin);
   p=(struct anatom*)malloc(sizeof(struct anatom)*numatoms);
 
+  natprot=numatoms/numprot;
+
   while (!feof(fin))
     {
       sprintf(fnout,"Store-%d", nframe);
       fout = fopen(fnout, "w+");
       fprintf(fout, "initFormat:0\n");
       fprintf(fout, "@@@\n");
+#ifdef COM
+      fprintf(fout, "parnum:%d\n", numprot);
+#else
       fprintf(fout, "parnum:%d\n", numatoms);
+#endif
       fprintf(fout, "steplength:%f\n", dt);
       fprintf(fout, "curStep: %d\n", stepinterval*nframe); 
       fprintf(fout, "@@@\n");
@@ -56,10 +62,29 @@ int main(int argc, char **argv)
 	     	 &(p[i].idx));
 	}
       qsort(p, numatoms, sizeof(struct anatom), cmpfunc);
+#ifdef COM
+      for (i=0; i < numprot; i++)
+	{
+	  comx=comy=comz=0.0;
+	  for (j=0; j < natprot; j++)
+	    {
+	      comx+=p[i*natprot+j].x;
+	      comy+=p[i*natprot+j].y;
+	      comz+=p[i*natprot+j].z;
+	    }
+	  comx /= (double) natprot;
+	  comy /= (double) natprot;
+	  comz /= (double) natprot;
+	  fprintf(fout, "%.12G %.12G %.12G\n", comx, comy, comz);
+	}
+      for (i=0; i < numprot; i++) 
+	fprintf(fout, "0 0 0\n");
+#else
       for (i=0; i < numatoms; i++) 
 	fprintf(fout, "%.12G %.12G %.12G\n", p[i].x, p[i].y, p[i].z);
       for (i=0; i < numatoms; i++) 
 	fprintf(fout, "0 0 0\n");
+#endif
       fprintf(fout, "%f\n", L[0]);
       //fprintf(fout, "%s\n", line);
       fclose(fout);
