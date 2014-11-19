4b8a6dff70719f6cb12ffd6fd0d776b777fdcccb
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2014-11-19 10:45:39 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2355 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- IGG/restart_all_from_iniconf.sh
+++ IGG/restart_all_from_iniconf.sh
@@ -3,10 +3,13 @@ do
 cd $f 
 EXE=`ls ellips-IgG*`
 EXEN="N-$EXE"
-rm COORD_TMP[0,1]
+cp $HOME/IGG/EDBD/ellipsoid-new .
+rm COORD_TMP[0,1] 2> /dev/null
+rm Store-*-* 2> /dev/null
 cat COORD_TMP_ASCII0| awk 'BEGIN{skip=0} {if (doout==1 && $1!="RF" && skip==0 && $1!="saveBonds:" && $1!="maxbondsSaved:" && $1!="nintersIJ:") print $0; if ($0=="1 1 4 0 1 0 0 1") skip=1; if ($1=="@@@") {at++; if (skip==1) {skip=0; print ("@@@");}}; if ($1=="@@@" && at==1) doout=1;}' > CorIni
-ln -sf ./ellipsoid $EXEN 
+ln -sf ./ellipsoid-new $EXEN 
 echo "EXEN=" $EXEN
+../../set_params.sh ellipsoid_flex.par seed -1 intervalSum 5000.0
 nohup mosrun ./$EXEN -fa ellipsoid_flex.par > screen &
 sleep 0.5
 cd ..
