6bfdb960c9bc78acd5edaf4185b634e7693b6c05
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2015-05-15 08:02:03 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2581 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- CHIRALITY/calc_v1_vs_s.sh
+++ CHIRALITY/calc_v1_vs_s.sh
@@ -9,7 +9,7 @@ do
 cd $f
 cd alpha_$A
 #echo "pwd=" `pwd`
-AV=`tail -1 v1.dat| awk '{print $2/1.97^5}'`
+AV=`tail -1 v1.dat| awk '{print $2/1.97^4}'`
 S=`echo $f| awk -F s '{print $2}'`
 echo "AV= " $AV " S=" $S
 echo "$S $AV" >> ../../${FN}
