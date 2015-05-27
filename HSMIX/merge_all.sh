bf968811af960bf2c5ccd924918fa01ed854d752
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2015-05-27 15:05:36 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2672 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HSMIX/merge_all.sh
+++ HSMIX/merge_all.sh
@@ -3,14 +3,17 @@ P1="part1"
 P2="part2"
 echo -n > $P2
 cc=0
-NP=`cat `head -n 1 lista`| awk '{if (NR==2) {print $2}; }'`
-BOX=`cat `head -n 1 lista`| awk '{if (NR==3) {print $0}; }'`
+FF=`head -n 1 lista`
+echo "Primo file=" $FF
+NP=`cat $FF| awk '{if (NR==2) {print $0}; }'`
+BOX=`cat $FF| awk '{if (NR==3) {print $0}; }'`
 for f in `cat lista`
 do
-cat $f | awk '{if (NR==2) {NP=$2}; if (NR>5 && NR-5 <= NP) print $0 }' >> $P2
+cat $f | awk '{if (NR==2) {NP=$2}; if (NR>5 && NR-5 <= NP) print $0; }' >> $P2
 cc=$[${cc}+1]
 done
-NTOT=`echo $cc*$NP| bc`
+echo "QUI cc=$cc NP=$NP"
+NTOT=`echo $cc*$NP | bc`
 echo "0 0 " > $P1
 echo $NTOT >> $P1
 echo $BOX >> $P1
