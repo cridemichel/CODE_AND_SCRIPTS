b8cd7a59680ff6dd59a3d0b6d43d76e0c6c9d6c5
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2015-05-27 15:25:43 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2674 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HSMIX/merge_all.sh
+++ HSMIX/merge_all.sh
@@ -1,23 +1,34 @@
 FN="super-story"
 P1="part1"
 P2="part2"
+P3="part3"
 echo -n > $P2
 cc=0
 FF=`head -n 1 lista`
 echo "Primo file=" $FF
 NP=`cat $FF| awk '{if (NR==2) {print $0}; }'`
+NPA=`cat mols.dat | awk '{if (NR==2) print $2}'`
 BOX=`cat $FF| awk '{if (NR==3) {print $0}; }'`
+echo "NP=" $NP " NPA=" $NPA
 for f in `cat $1`
 do
 echo "file=" $f
-cat $f | awk '{if (NR==2) {NP=$0}; if (NR>5 && NR-5 <= NP) print $0; }' >> $P2
+cat $f | awk -v np=$NP -v npa=$NPA '{if (NR>5 && NR-5 <= npa) print $0; }' >> $P2
 cc=$[${cc}+1]
 done
-echo "QUI cc=$cc NP=$NP"
-NTOT=`echo $cc*$NP | bc`
+for f in `cat $1`
+do
+echo "file=" $f
+cat $f | awk -v np=$NP -v npa=$NPA '{if (NR>5+npa && NR-5 <= np) print $0; }' >> $P3
+done
+NPTOT=`echo "${cc}*${NP}" | bc`
+NPATOT=`echo "${cc}*${NPA}" | bc`
+echo "NPTOT= " $NPTOT " NPATOT= " $NPATOT
+echo "Ntot $NPTOT" >  supermols.dat
+echo "Nlarge $NPATOT" >> supermols.dat
 echo "0 0 " > $P1
-echo $NTOT >> $P1
+echo $NPTOT >> $P1
 echo $BOX >> $P1
 echo "0 0 0" >> $P1
 echo "0 0 0 0" >> $P1
-cat $P1 $P2 > $FN
+cat $P1 $P2 $P3 > $FN
