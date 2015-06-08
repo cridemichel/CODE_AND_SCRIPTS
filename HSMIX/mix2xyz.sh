e241dcb4a2ff12cf7e02531786cb4945a0b89843
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2015-06-08 10:33:44 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2683 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HSMIX/mix2xyz.sh
+++ HSMIX/mix2xyz.sh
@@ -1,21 +1,22 @@
-FN="super-story"
+FN="super-story.xyz"
 P1="part1"
 P2="part2"
 echo -n > $P2
 cc=0
 FF=`head -n 1 lista`
 echo "Primo file=" $FF
-NP=`cat $FF| awk '{if (NR==2) {print $0}; }'`
+NP=`cat mols.dat| awk '{if (NR==1) {print $2}; }'`
 NPA=`cat mols.dat | awk '{if (NR==2) print $2}'`
-BOX=`cat $FF| awk '{if (NR==3) {print $0}; }'`
+#BOX=`cat $FF| awk '{if (NR==3) {print $0}; }'`
 echo "NP=" $NP " NPA=" $NPA
 # only small particles
 for f in `cat $1`
 do
 echo "file=" $f
-cat $f | awk -v np=$NP -v npa=$NPA '{if (NR>5+npa && NR-5 <= np) print ("O ", $0);}' >> $P2
+cat $f | awk -v np=$NP -v npa=$NPA '{if (NR>5+npa && NR-5 <= np) print ("H ", $0);}' >> $P2
 done
-echo $NPTOT > $P1
-echo  "superfile" ">> $P1
+NPS=`echo "$NP-$NPA" | bc`
+echo $NPS > $P1
+echo  "superfile" >> $P1
 cat $P1 $P2 > $FN
 rm -f $P1 $P2
