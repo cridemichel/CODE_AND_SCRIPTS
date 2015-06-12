2663b9b2f202a9f141e9201ac3a62b64ac4cb041
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2015-06-12 13:33:46 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2701 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ HSMIX/merge_all_linear_block.sh
@@ -0,0 +1,48 @@
+P1="part1"
+P2="part2"
+P3="part3"
+cc=0
+BLOCK="$2"
+FF=`head -n 1 lista`
+echo "Primo file=" $FF
+#NP=`cat $FF| awk '{if (NR==2) {print $0}; }'`
+NP=`cat mols.dat | gawk '{if (NR==1) {print $2};}'`
+NPA=`cat mols.dat | gawk '{if (NR==2) print $2}'`
+BOX=`cat $FF| gawk '{if (NR==6) {print $0}; }'`
+echo "NP=" $NP " NPA=" $NPA
+#for f in `cat $1`
+#do
+#echo "file=" $f
+#cat $f | awk -v np=$NP -v npa=$NPA '{if (NR>319 && NR-319 <= npa) print ($3, $4, $5); }' >> $P2
+#cc=$[${cc}+1]
+#done
+NFILES=`wc -l lista| gawk '{print $1}'`
+while [ $cc -lt $NFILES ]
+do
+dd=0
+FN="super-story-${cc}"
+echo -n > $P1
+echo -n > $P2
+echo -n > $P3
+while [ $dd -lt $BLOCK ]
+do
+num=`echo "$cc*$BLOCK+$dd"|bc`
+f=`echo $num | gawk '{printf("story%04d",$1)}'`
+echo "file=" $f
+cat $f | gawk -v np=$NP -v npa=$NPA '{if (NR>319+npa && NR-319 <= np) print ($3, $4, $5); }' >> $P3
+dd=$[${dd}+1]
+done
+NPTOT=`echo "${BLOCK}*${NP}" | bc`
+NPATOT=`echo "${BLOCK}*${NPA}" | bc`
+NPSMALL=$[${NPTOT}-${NPATOT}]
+echo "NPTOT= " $NPTOT " NPATOT= " $NPATOT " NPSMALL=" $NPSMALL
+echo "Ntot $NPTOT" >  supermols.dat
+#echo "Nlarge $NPATOT" >> supermols.dat
+echo "0 0 " > $P1
+echo $NPSMALL >> $P1
+echo $BOX >> $P1
+echo "0 0 0" >> $P1
+echo "0 0 0 0" >> $P1
+cat $P1 $P3 > $FN
+cc=$[${cc}+1]
+done
