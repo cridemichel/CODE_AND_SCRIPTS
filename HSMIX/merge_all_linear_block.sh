d3097dd8630d80b752e9ff49d22aa0ca440c041b
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2015-06-12 13:50:26 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2702 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HSMIX/merge_all_linear_block.sh
+++ HSMIX/merge_all_linear_block.sh
@@ -16,17 +16,23 @@ echo "NP=" $NP " NPA=" $NPA
 #cat $f | awk -v np=$NP -v npa=$NPA '{if (NR>319 && NR-319 <= npa) print ($3, $4, $5); }' >> $P2
 #cc=$[${cc}+1]
 #done
+aa=0
 NFILES=`wc -l lista| gawk '{print $1}'`
 while [ $cc -lt $NFILES ]
 do
 dd=0
-FN="super-story-${cc}"
+FN="super-story-${aa}"
 echo -n > $P1
 echo -n > $P2
 echo -n > $P3
+num=`echo "$aa*$BLOCK+$BLOCK-1"|bc`
+if [ $num -ge $NFILES ]
+then
+break
+fi
 while [ $dd -lt $BLOCK ]
 do
-num=`echo "$cc*$BLOCK+$dd"|bc`
+num=`echo "$aa*$BLOCK+$dd"|bc`
 f=`echo $num | gawk '{printf("story%04d",$1)}'`
 echo "file=" $f
 cat $f | gawk -v np=$NP -v npa=$NPA '{if (NR>319+npa && NR-319 <= np) print ($3, $4, $5); }' >> $P3
@@ -44,5 +50,6 @@ echo $BOX >> $P1
 echo "0 0 0" >> $P1
 echo "0 0 0 0" >> $P1
 cat $P1 $P3 > $FN
-cc=$[${cc}+1]
+cc=$[${cc}+${BLOCK}]
+aa=$[${aa}+1]
 done
