0722b4d85fc852cdb5dc6d3db70d586053f51d79
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2015-06-15 07:16:32 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@2712 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- CHIRALITY/CG-DNA-k2K22-INT.c
+++ CHIRALITY/CG-DNA-k2K22-INT.c
@@ -34,26 +34,154 @@ int nat, atnum, nbnum, len;
 long long int tot_trials, tt=0, ttini=0;
 double L, rx, ry, rz, alpha, dfons_sinth_max, fons_sinth_max;
 const double thetapts=100000;
-
-double qgaus(double (*func)(double), double a, double b)
+#ifdef GAUSS
+#define EPSJAC 3.0e-14 /*Increase EPS if you don’t have this precision */ 
+#define MAXITJAC 10 
+void gaujac(double x[], double w[], int n, double alf, double bet)
+/*Given alf and bet, the parameters α and β of the Jacobi polynomials, this routine returns arrays x[1..n] and w[1..n] containing the abscissas and weights of the n-point Gauss-Jacobi quadrature formula. The largest abscissa is returned in x[1], the smallest in x[n].*/
 {
+  double gammln(double xx);
+  /*void nrerror(char error_text[]); */
+  int i,its,j;
+  double alfbet,an,bn,r1,r2,r3;
+  double a,b,c,p1,p2,p3,pp,temp,z,z1;
+  for (i=1;i<=n;i++) { 
+    if (i == 1) { 
+      an=alf/n;
+      /* High precision is a good idea for this rou- tine.
+	 Loop over the desired roots. Initial guess for the largest root.*/
+      bn=bet/n; 
+      r1=(1.0+alf)*(2.78/(4.0+n*n)+0.768*an/n); 
+      r2=1.0+1.48*an+0.96*bn+0.452*an*an+0.83*an*bn; 
+      z=1.0-r1/r2;
+    } 
+    else if (i == 2) 
+      { /* Initial guess for the second largest root.*/
+	r1=(4.1+alf)/((1.0+alf)*(1.0+0.156*alf)); 
+	r2=1.0+0.06*(n-8.0)*(1.0+0.12*alf)/n; 
+	r3=1.0+0.012*bet*(1.0+0.25*fabs(alf))/n;
+	z -= (1.0-z)*r1*r2*r3;
+      } 
+    else if (i == 3) 
+      { /* Initial guess for the third largest root.*/
+	r1=(1.67+0.28*alf)/(1.0+0.37*alf); 
+	r2=1.0+0.22*(n-8.0)/n; 
+	r3=1.0+8.0*bet/((6.28+bet)*n*n);
+	z -= (x[1]-z)*r1*r2*r3;
+      } 
+    else if (i == n-1) 
+      { /* Initial guess for the second smallest root. */ 
+	r1=(1.0+0.235*bet)/(0.766+0.119*bet); 
+	r2=1.0/(1.0+0.639*(n-4.0)/(1.0+0.71*(n-4.0))); 
+	r3=1.0/(1.0+20.0*alf/((7.5+alf)*n*n));
+	z += (z-x[n-3])*r1*r2*r3;
+      } 
+    else if (i == n) 
+      { /* Initial guess for the smallest root.*/
+	r1=(1.0+0.37*bet)/(1.67+0.28*bet); 
+	r2=1.0/(1.0+0.22*(n-8.0)/n); 
+	r3=1.0/(1.0+8.0*alf/((6.28+alf)*n*n)); 
+	z += (z-x[n-2])*r1*r2*r3;
+      } 
+    else 
+      { /* Initial guess for the other roots.*/
+       	z=3.0*x[i-1]-3.0*x[i-2]+x[i-3];
+      }
+    alfbet=alf+bet;
+    for (its=1;its<=MAXITJAC;its++) 
+      {
+	temp=2.0+alfbet; p1=(alf-bet+temp*z)/2.0; p2=1.0;
+	for (j=2;j<=n;j++) {
+	  p3=p2;
+	  p2=p1;
+	  temp=2*j+alfbet;
+	  a=2*j*(j+alfbet)*(temp-2.0);
+	  b=(temp-1.0)*(alf*alf-bet*bet+temp*(temp-2.0)*z); 
+	  c=2.0*(j-1+alf)*(j-1+bet)*temp;
+	  p1=(b*p2-c*p3)/a;
+	} 
+	pp=(n*(alf-bet-temp*z)*p1+2.0*(n+alf)*(n+bet)*p2)/(temp*(1.0-z*z)); 
+	/* p1 is now the desired Jacobi polynomial. We next compute pp, its derivative, 
+	   by a standard relation involving also p2, the polynomial of one lower order. */
+	z1=z;
+	z=z1-p1/pp; /*Newton’s formula.*/
+	if (fabs(z-z1) <= EPSJAC) break;
+      }
+    if (its > MAXIT)
+      {
+	printf("too many iterations in gaujac"); 
+	exit(-1);
+      }
+    x[i]=z; /* Store the root and the weight.*/
+    w[i]=exp(gammln(alf+n)+gammln(bet+n)-gammln(n+1.0)-
+	     gammln(n+alfbet+1.0))*temp*pow(2.0,alfbet)/(pp*p2);
+  }
+}
+int ntheta, nphi;
+double *xtheta, *xphi, *wtheta, *wphi;
+#define EPSGAULEG 3.0e-11 
+/*EPS is the relative precision.*/
+void gauleg(double x1, double x2, double x[], double w[], int n)
+  /*Given the lower and upper limits of integration x1 and x2, and given n, this routine returns arrays x[1..n] and w[1..n] of length n, containing the abscissas and weights of the Gauss- Legendre n-point quadrature formula.*/
+{
+  int m,j,i;
+  double z1,z,xm,xl,pp,p3,p2,p1;
+  m=(n+1)/2;
+  xm=0.5*(x2+x1);
+  xl=0.5*(x2-x1);
+  /*
+     High precision is a good idea for this rou- tine.
+     The roots are symmetric in the interval, so we only have to find half of them.
+     Loop up the recurrence relation to get the Legendre polynomial evaluated at z.
+     Loop over the desired roots. */
+  for (i=1;i<=m;i++) 
+    {
+      z=cos(3.141592654*(i-0.25)/(n+0.5));
+      /*Starting with the above approximation to the ith root, we enter the main loop of refinement by Newton’s method.*/
+      do {
+	p1=1.0;
+	p2=0.0;
+	for (j=1;j<=n;j++) {
+	  p3=p2;
+	  p2=p1; p1=((2.0*j-1.0)*z*p2-(j-1.0)*p3)/j;
+	}
+	/*
+	   p1 is now the desired Legendre polynomial. We next compute pp, its derivative, by a 
+	   standard relation involving also p2, the polynomial of one lower order. 
+	 */	   
+	pp=n*(z*p1-p2)/(z*z-1.0);
+	z1=z;
+	z=z1-p1/pp;
+      } while (fabs(z-z1) > EPSGAULEG); 
+      x[i]=xm-xl*z;
+      x[n+1-i]=xm+xl*z; 
+      w[i]=2.0*xl/((1.0-z*z)*pp*pp); 
+      w[n+1-i]=w[i];
+      /*Scale the root to the desired interval, and put in its symmetric counterpart. 
+	Compute the weight and its symmetric counterpart.*/
+    }
+}
+double qgaus(double (*func)(double), double a, double b, double *x, double *w, int np)
+{
+#if 0
   static const double x[]={0.1488743389816312,0.4333953941292472,
     0.6794095682990244,0.8650633666889845,0.9739065285171717};
   static const double w[]={0.2955242247147529,0.2692667193099963,
     0.2190863625159821,0.1494513491505806,0.0666713443086881};
+#endif
   double xm, xr, s, dx;
   int j;
   xm=0.5*(b+a);
   xr=0.5*(b-a);
   s=0.;
-  for (j=0;j<5;j++) 
+  for (j=1;j<=np;j++) 
     {
       dx=xr*x[j];
       s += w[j]*(func(xm+dx)+func(xm-dx));
     }
   return s *= xr;
 }
-
+#endif
 #include <math.h> 
 #define EPS 1.0e-2
 #define JMAX 20 
@@ -1006,7 +1134,7 @@ double quad4d(double (*func)(double,double,double,double),
   double fphi1(double phi1);
   nrfunc=func;
 #ifdef GAUSS
-  return qgaus(fphi1,phi1_1,phi1_2);
+  return qgaus(fphi1,phi1_1,phi1_2, xphi, wphi, nphi);
 #else
   return qromb(fphi1,phi1_1,phi1_2);
 #endif
@@ -1016,7 +1144,7 @@ double fphi1(double phi1)
   double fphi2(double phi2);
   phi1sav=phi1;
 #ifdef GAUSS
-  return qgaus(fphi2,0.0,2.0*M_PI); 
+  return qgaus(fphi2,0.0,2.0*M_PI, xphi, wphi, nphi); 
 #else
   return qromb(fphi2,0.0,2.0*M_PI); 
 #endif
@@ -1026,7 +1154,7 @@ double fphi2(double phi2)
   double ftheta1(double theta1);
   phi2sav=phi2;
 #ifdef GAUSS
-  return qgaus(ftheta1,0.0,2.0*M_PI); 
+  return qgaus(ftheta1,0.0,2.0*M_PI, xtheta, wtheta, ntheta); 
 #else
   return qromb(ftheta1,0.0,2.0*M_PI); 
 #endif
@@ -1036,7 +1164,7 @@ double ftheta1(double theta1)
   double ftheta2(double theta2);
   theta1sav=theta1;
 #ifdef GAUSS
-  return qgaus(ftheta2,0.0,M_PI); 
+  return qgaus(ftheta2,0.0,M_PI, xtheta, wtheta, ntheta); 
 #else
   return qromb(ftheta2,0.0,M_PI); 
 #endif
@@ -1562,6 +1690,14 @@ int main(int argc, char**argv)
   exit(-1);
 #endif
   nrfunc = intfunc;
+#ifdef GAUSS
+  ntheta=20;
+  nphi=20;
+  xtheta = malloc(sizeof(double)*(ntheta+1));
+  xphi=malloc(sizeof(double)*(nphi+1));
+  gauleg(0.0, M_PI, xtheta, wtheta, ntheta);
+  gauleg(0.0, 2.0*M_PI, xphi, wphi, nphi);
+#endif
 #ifdef QUASIMC
   /* initialization */
   nsv = -1;  
