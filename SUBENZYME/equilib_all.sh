d7c3a48cc91782e1feaa3dbf6992db6f05271e1f
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2016-02-11 14:22:05 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@3057 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SUBENZYME/equilib_all.sh
+++ SUBENZYME/equilib_all.sh
@@ -1,17 +1,18 @@
 if [ "$1" == "" ]
 then
-LD=`ls -f PHI_*`
+LD=`ls -d PHI_*`
 else
 LD=`cat $1`
 fi
 SIG=`pwd | awk -F _ '{print $(NF-1)}'`
+echo "SIG =" $SIG
 for f in `echo $LD`
 do
 cd $f
-rm COORD_TMP[0,1]
+rm COORD_TMP[0,1] 2> /dev/null
 cp CorFinal startGR.cnf
 PHI=`echo $f | awk -F _ '{print $2}'`
-EN="subenz_sig_$SIG_phi_$PHI"
+EN="subenz_sig_${SIG}_phi_$PHI"
 ln -sf ../subenz $EN
 ../set_params.sh ellipsoid_flex_eq.par stepnum 5000
 nohup mosrun ./$EN -fa ellipsoid_flex_eq.par > screen &
--- SUBENZYME/grow_all.sh
+++ SUBENZYME/grow_all.sh
@@ -1,18 +1,23 @@
 if [ "$1" == "" ]
 then
-LD=`ls -f PHI_*`
+LD=`ls -d PHI_*`
 else
 LD=`cat $1`
 fi
 SIG=`pwd| awk -F _ '{print $(NF-1)}'`
+echo "SIG= " $SIG
 for f in `echo $LD`
 do
+if [ ! -e $f ]
+then
+mkdir $f
+fi
 cd $f
-rm COORD_TMP[0,1]
+rm COORD_TMP[0,1] 2> /dev/null
 cp ../ellipsoid*par .
 cp ../start.cnf .
 PHI=`echo $f | awk -F _ '{print $2}'`
-EN="subenz_sig_$SIG_phi_$PHI"
+EN="subenz_sig_${SIG}_phi_$PHI"
 ln -sf ../subenz $EN
 ../set_params.sh ellipsoid_flex_growth.par intervalSum 1.0 targetPhi $PHI
 nohup mosrun ./$EN -fa ellipsoid_flex_growth.par > screen &
