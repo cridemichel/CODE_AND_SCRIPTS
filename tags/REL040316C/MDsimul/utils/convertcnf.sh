5b2c4f8947206dc9caae0c7c0b4577d49e28eca1
(no author) <(no author)@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2016-03-04 21:00:48 +0000

    This commit was manufactured by cvs2svn to create tag 'REL040316C'.
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@12877 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ tags/REL040316C/MDsimul/utils/convertcnf.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ "$1" == "" ]
+then
+PREPATH=""
+else
+PREPATH=$1
+fi
+for FF in `ls Cnf*_R*` 
+do
+if [ "$PREPATH" == "" ]
+then
+$HOME/MDsimul/bin/cri2fraMIX $FF ${FF}_FRA
+else
+$HOME/MDsimul/bin/cri2fraMIX $FF $PREPATH/${FF}_FRA
+fi
+done
