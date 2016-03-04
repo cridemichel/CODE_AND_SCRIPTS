dc5e704bc7115bb23177e7dda6fae09b872eefb1
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2016-03-04 10:05:57 +0000

    * switch to disable OPT BS and OPT ALMARZA
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@12872 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- trunk/MDsimul/ellipsoid/init.c
+++ trunk/MDsimul/ellipsoid/init.c
@@ -31,9 +31,11 @@ int totspots;
 int totspotsBS;
 #ifdef MC_OPT_CHECK_BS
 int **checkBS, *numcheckBS;
+#ifdef MC_OPT_ALMARZA
 int **checkBS_MC, *numcheckBS_MC;
 int **bondsYN_BS;
 int **checkBS_P, *numcheckBS_P;
+#endif
 #else
 int *checkBS;
 #endif
@@ -5714,11 +5716,13 @@ void usrInitAft(void)
 #ifdef MC_OPT_CHECK_BS
   checkBS = AllocMatI(Oparams.parnum, OprogStatus.maxbonds);
   numcheckBS = (int *) malloc(Oparams.parnum*sizeof(int));
+#ifdef MC_OPT_ALMARZA
   checkBS_P = AllocMatI(Oparams.parnum*8, OprogStatus.maxbonds);
   numcheckBS_P = (int *) malloc(Oparams.parnum*8*sizeof(int));
   checkBS_MC = AllocMatI(Oparams.parnum, OprogStatus.maxbonds);
   numcheckBS_MC = (int *) malloc(Oparams.parnum*sizeof(int));
   bondsYN_BS= AllocMatI(Oparams.parnum, OprogStatus.maxbonds);
+#endif
   for (i=0; i < Oparams.parnum; i++)
     numcheckBS[i] = 0;
 #else
