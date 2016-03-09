7b61d053ce98b7755b435fb857e405d662d3e538
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2016-03-09 21:14:12 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@3091 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SUBENZYME/create_confs_from_high_dens.sh
+++ SUBENZYME/create_confs_from_high_dens.sh
@@ -15,11 +15,11 @@ FF=`echo "1.0/0.1/${Nav}/${L}^3" | bc -l`
 echo "NS=" $NS " FF=" $FF " L=" $L
 MS=`echo "${FF}*${NS}"| bc -l | awk '{printf("%G",$1)}'`
 echo "MS= " $MS
-cat $SC | gawk -v nS=$NS -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA) {NE=$1; NS=$2}; if ("$0"=="@@@"){\
+cat $SC | gawk -v nS=$NS -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA) {NE=$1; NS=$2}; if ($0=="@@@"){\
 at=at+1; if (at==1) ATA=NR;\
-else ATB=NR}; if (at==0 && "$1"=="parnum:") print ("parnum:",nS+nE); else if (at==0) print $0; else \
-if (at==1) print $0; else if (at==2) {if (NR-(ATB+1)<nE) print $0; else if (NR-(ATB+1)<nE+nS) print $0; \
-if (NR-(ATB+1) > nT) { if (NR-(ATB+1) < nT+nE) print $0; else if (NR-(ATB+1) < nT+nE+nS) print $0; \
-if (NR-(ATB+1) == nT+1) print $0}}}' > start_NOCROWD_MS_${MS}.cnf
+else ATB=NR}; if (at==0 && $1=="parnum:") {print ("parnum:",nS+nE);} else if (at==0) print $0; else \
+if (at==1) {if (NR==ATA+1) print(nE, nS, "0"); else print $0;} else if (at==2) {if (NR-ATB<=nE) print $0; else if (NR-ATB<=nE+nS) print $0; \
+if (NR-ATB > nT) { if (NR-ATB <= nT+nE) print $0; else if (NR-ATB <= nT+nE+nS) print $0; \
+if (NR-ATB == 2*nT+1) print $0}}}' > start_NOCROWD_MS_${MS}_cnf
 i=$[$i+1]
 done
