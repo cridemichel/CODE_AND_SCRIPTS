e4121c7ad2653fe409017be81e3c707f49c3203b
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2016-04-28 08:24:34 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@3124 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SUBENZYME/create_confs_from_high_dens_crowd.sh
+++ SUBENZYME/create_confs_from_high_dens_crowd.sh
@@ -1,7 +1,7 @@
 SC="$1"
 NI="65278"
 NE="338"
-NC="110000"
+NC="160000"
 DNS="3000"
 DNS2="500"
 SIGE="4.5"
@@ -18,9 +18,9 @@ FF=`echo "1.0/0.1/${Nav}/${L}^3" | bc -l`
 echo "NS=" $NS " FF=" $FF " L=" $L
 MS=`echo "${FF}*${NS}"| bc -l | awk '{printf("%G",$1)}'`
 echo "MS= " $MS
-cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA) {NE=$1; NS=$2; NC=$3}; if ($0=="@@@"){\
+cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA+1) {NE=$1; NS=$2; NC=$4;}; if ($0=="@@@"){\
 at=at+1; if (at==1) ATA=NR;\
-else ATB=NR}; if (at==0 && $1=="parnum:") {print ("parnum:",nS+nE);} else if (at==0) print $0; else \
+else ATB=NR}; if (at==0 && $1=="parnum:") {print ("parnum:",nS+nE+nC);} else if (at==0) print $0; else \
 if (at==1) {if (NR==ATA+1) print(nE, nS, "0", nC); else print $0;} else if (at==2) {if (NR-ATB<=nE) print $0; else if (NR-ATB<=nE+nS) print $0; else if (NR-ATB > NE+NS && NR-ATB <= nT) print $0; \
 if (NR-ATB > nT) { if (NR-ATB <= nT+nE) print $0; else if (NR-ATB <= nT+nE+nS) print $0; else if (NR-ATB > nT+NE+NS && \
 NR-ATB <= 2*nT) print $0;\
@@ -37,7 +37,7 @@ FF=`echo "1.0/0.1/${Nav}/${L}^3" | bc -l`
 echo "NS=" $NS " FF=" $FF " L=" $L
 MS=`echo "${FF}*${NS}"| bc -l | awk '{printf("%G",$1)}'`
 echo "MS= " $MS
-cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA) {NE=$1; NS=$2; NC=$3}; if ($0=="@@@"){\
+cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA) {NE=$1; NS=$2; NC=$4}; if ($0=="@@@"){\
 at=at+1; if (at==1) ATA=NR;\
 else ATB=NR}; if (at==0 && $1=="parnum:") {print ("parnum:",nS+nE);} else if (at==0) print $0; else \
 if (at==1) {if (NR==ATA+1) print(nE, nS, "0", nC); else print $0;} else if (at==2) {if (NR-ATB<=nE) print $0; else if (NR-ATB<=nE+nS) print $0; else if (NR-ATB > NE+NS && NR-ATB <= nT) print $0; \
