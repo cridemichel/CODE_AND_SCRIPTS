6c244a4c148ed6d3bf1abff87f7a21b96446c3e9
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2016-04-28 09:13:28 +0000

    * upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@3125 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SUBENZYME/create_confs_from_high_dens_crowd.sh
+++ SUBENZYME/create_confs_from_high_dens_crowd.sh
@@ -1,5 +1,5 @@
 SC="$1"
-NI="65278"
+NI="66643"
 NE="338"
 NC="160000"
 DNS="3000"
@@ -18,11 +18,11 @@ FF=`echo "1.0/0.1/${Nav}/${L}^3" | bc -l`
 echo "NS=" $NS " FF=" $FF " L=" $L
 MS=`echo "${FF}*${NS}"| bc -l | awk '{printf("%G",$1)}'`
 echo "MS= " $MS
-cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA+1) {NE=$1; NS=$2; NC=$4;}; if ($0=="@@@"){\
+cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0;} {if (at==1 && NR==ATA+1) {NNE=$1; NNS=$2; NNC=$4;}; if ($0=="@@@"){\
 at=at+1; if (at==1) ATA=NR;\
 else ATB=NR}; if (at==0 && $1=="parnum:") {print ("parnum:",nS+nE+nC);} else if (at==0) print $0; else \
-if (at==1) {if (NR==ATA+1) print(nE, nS, "0", nC); else print $0;} else if (at==2) {if (NR-ATB<=nE) print $0; else if (NR-ATB<=nE+nS) print $0; else if (NR-ATB > NE+NS && NR-ATB <= nT) print $0; \
-if (NR-ATB > nT) { if (NR-ATB <= nT+nE) print $0; else if (NR-ATB <= nT+nE+nS) print $0; else if (NR-ATB > nT+NE+NS && \
+if (at==1) {if (NR==ATA+1) print(nE, nS, "0", nC); else print $0;} else if (at==2) {if (NR-ATB<=nE) print $0; else if (NR-ATB<=nE+nS) print $0; else if (NR-ATB > NNE+NNS && NR-ATB <= nT) print $0; \
+if (NR-ATB > nT) {if (NR-ATB <= nT+nE) print $0; else if (NR-ATB <= nT+nE+nS) print $0; else if (NR-ATB > nT+NNE+NNS && \
 NR-ATB <= 2*nT) print $0;\
 if (NR-ATB == 2*nT+1) print $0}}}' > start_CROWD_MS_${MS}_cnf
 i=$[$i+1]
@@ -37,11 +37,11 @@ FF=`echo "1.0/0.1/${Nav}/${L}^3" | bc -l`
 echo "NS=" $NS " FF=" $FF " L=" $L
 MS=`echo "${FF}*${NS}"| bc -l | awk '{printf("%G",$1)}'`
 echo "MS= " $MS
-cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0} {if (at==1 && NR==ATA) {NE=$1; NS=$2; NC=$4}; if ($0=="@@@"){\
+cat $SC | gawk -v nS=$NS -v nC=$NC -v nE=$NE -v nT=$NT  'BEGIN {at=0;} {if (at==1 && NR==ATA+1) {NNE=$1; NNS=$2; NNC=$4;}; if ($0=="@@@"){\
 at=at+1; if (at==1) ATA=NR;\
-else ATB=NR}; if (at==0 && $1=="parnum:") {print ("parnum:",nS+nE);} else if (at==0) print $0; else \
-if (at==1) {if (NR==ATA+1) print(nE, nS, "0", nC); else print $0;} else if (at==2) {if (NR-ATB<=nE) print $0; else if (NR-ATB<=nE+nS) print $0; else if (NR-ATB > NE+NS && NR-ATB <= nT) print $0; \
-if (NR-ATB > nT) { if (NR-ATB <= nT+nE) print $0; else if (NR-ATB <= nT+nE+nS) print $0; else if (NR-ATB > nT+NE+NS && \
+else ATB=NR}; if (at==0 && $1=="parnum:") {print ("parnum:",nS+nE+nC);} else if (at==0) print $0; else \
+if (at==1) {if (NR==ATA+1) print(nE, nS, "0", nC); else print $0;} else if (at==2) {if (NR-ATB<=nE) print $0; else if (NR-ATB<=nE+nS) print $0; else if (NR-ATB > NNE+NNS && NR-ATB <= nT) print $0; \
+if (NR-ATB > nT) {if (NR-ATB <= nT+nE) print $0; else if (NR-ATB <= nT+nE+nS) print $0; else if (NR-ATB > nT+NNE+NNS && \
 NR-ATB <= 2*nT) print $0;\
 if (NR-ATB == 2*nT+1) print $0}}}' > start_CROWD_MS_${MS}_cnf
 i=$[$i+1]
