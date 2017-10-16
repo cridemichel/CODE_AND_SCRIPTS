529b0588fd906c8f36f45c8a1f1bbb03a3ead529
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2017-10-16 08:59:49 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@3364 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- CHROMONICS/calc_ons_int.c
+++ CHROMONICS/calc_ons_int.c
@@ -5,12 +5,116 @@
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_precision.h>
 #include <gsl/gsl_sf_ellint.h>
+// il quasi MC è molto più lento del MC nella generazione dei numeri casuali quindi meglio non usarlo qui
+//#define SOBOL_LL
+#ifdef SOBOL_LL
+static int imaxarg1,imaxarg2;
+#define IMAX(a,b) (imaxarg1=(a),imaxarg2=(b),(imaxarg1) > (imaxarg2) ?\
+        (imaxarg1) : (imaxarg2))
+
+static int iminarg1,iminarg2;
+#define IMIN(a,b) (iminarg1=(a),iminarg2=(b),(iminarg1) < (iminarg2) ?\
+        (iminarg1) : (iminarg2))
+
+#define SIGN(a,b) ((b) >= 0.0 ? fabs(a) : -fabs(a))
+
+static float maxarg1,maxarg2;
+#define FMAX(a,b) (maxarg1=(a),maxarg2=(b),(maxarg1) > (maxarg2) ?\
+        (maxarg1) : (maxarg2))
+
+static float minarg1,minarg2;
+#define FMIN(a,b) (minarg1=(a),minarg2=(b),(minarg1) < (minarg2) ?\
+        (minarg1) : (minarg2))
+
+static long lmaxarg1,lmaxarg2;
+#define LMAX(a,b) (lmaxarg1=(a),lmaxarg2=(b),(lmaxarg1) > (lmaxarg2) ?\
+        (lmaxarg1) : (lmaxarg2))
+
+static long lminarg1,lminarg2;
+#define LMIN(a,b) (lminarg1=(a),lminarg2=(b),(lminarg1) < (lminarg2) ?\
+        (lminarg1) : (lminarg2))
+
+static int imaxarg1,imaxarg2;
+#define IMAX(a,b) (imaxarg1=(a),imaxarg2=(b),(imaxarg1) > (imaxarg2) ?\
+        (imaxarg1) : (imaxarg2))
+
+
+/*#define IMIN(a,b) (iminarg1=(a),iminarg2=(b),(iminarg1) < (iminarg2) ?\
+        (iminarg1) : (iminarg2))*/
+#define MAXBIT 30 
+#define MAXDIM 6
+void sobseq(int *n, double x[])
+/*When n is negative, internally initializes a set of MAXBIT direction numbers for each of MAXDIM different Sobol’ sequences. When n is positive (but ≤MAXDIM), returns as the vector x[1..n] the next values from n of these sequences. (n must not be changed between initializations.)*/
+{
+  int j,k,l;
+  unsigned long i,im,ipp;
+  static double fac;
+  static unsigned long in,ix[MAXDIM+1],*iu[MAXBIT+1];
+  static unsigned long mdeg[MAXDIM+1]={0,1,2,3,3,4,4};
+  static unsigned long ip[MAXDIM+1]={0,0,1,1,2,1,4}; 
+  static unsigned long iv[MAXDIM*MAXBIT+1]={0,1,1,1,1,1,1,3,1,3,3,1,1,5,7,7,3,3,5,15,11,5,15,13,9};
+  if (*n < 0) 
+    { 
+      /*Initialize, don’t return a vector. */
+      for (k=1;k<=MAXDIM;k++) ix[k]=0;
+      in=0;
+      if (iv[1] != 1) return;
+      fac=1.0/(1L << MAXBIT);
+      for (j=1,k=0;j<=MAXBIT;j++,k+=MAXDIM) 
+	iu[j] = &iv[k];/* To allow both 1D and 2D addressing.*/
+      for (k=1;k<=MAXDIM;k++) 
+	{
+	  for (j=1;j<=mdeg[k];j++)
+	    iu[j][k] <<= (MAXBIT-j); /*Stored values only require normalization.*/
+	  for (j=mdeg[k]+1;j<=MAXBIT;j++) 
+	    {
+	      ipp=ip[k]; i=iu[j-mdeg[k]][k];
+	      i ^= (i >> mdeg[k]);
+	      for (l=mdeg[k]-1;l>=1;l--) 
+		{
+		  if (ipp & 1) i ^= iu[j-l][k];
+		  ipp >>= 1; 
+		}
+	      iu[j][k]=i;
+	    }
+	}
+    } 
+  else 
+    {
+      im=in++;
+      for (j=1;j<=MAXBIT;j++) {
+	if (!(im & 1)) break;
+	im >>= 1; }
+      if (j > MAXBIT) {
+	printf("MAXBIT too small in sobseq");
+	exit(-1);
+      } 
+      im=(j-1)*MAXDIM;
+      for (k=1;k<=IMIN(*n,MAXDIM);k++)
+	{
+	  ix[k] ^= iv[im+k]; 
+	  x[k]=ix[k]*fac;
+	}
+      /*XOR the appropriate direction num- ber into each component of the vector and convert to a floating number.
+       */
+    }
+}
+#endif
+#ifdef SOBOL_LL
+double sv[10];
+int nsv;
+#endif
 double ranf_vb(void)
 {
   /*  Returns a uniform random variate in the range 0 to 1.         
       Good random number generators are machine specific.
       please use the one recommended for your machine. */
+#ifdef SOBOL_LL
+  sobseq(&nsv, sv);
+  return sv[4];  
+#else
   return drand48();
+#endif
 }
 double fons(double theta, double alpha)
 {
@@ -83,6 +187,7 @@ double scalProd(double *A, double *B)
   return R;
 }
 double u1[3], u2[3], alpha;
+char fn[1024];
 int main(int argc, char** argv)
 {
   long long int tt = 0, maxtrials, outstps;
@@ -99,16 +204,28 @@ int main(int argc, char** argv)
   da   = atof(argv[3]);
   maxtrials = atoll(argv[4]);
   if (argc > 5)
-    outstps = atoll(argv[4]);
+    outstps = atoll(argv[5]);
   else
     outstps = 500000;
   printf("aini=%f aend=%f da=%f totsteps=%lld outits=%lld\n", aini, aend, da, maxtrials, outstps);
-  f = fopen("onsint.dat", "w+");
-
+#ifdef SOBOL_LL
+  nsv = -1;  
+  sobseq(&nsv, sv);
+  nsv = 4;
+#endif
   //printf("EllipticE(%f)=%.15G\n", 0.9455, gsl_sf_ellint_Ecomp(sqrt(0.9455), GSL_PREC_DOUBLE));
   //exit(-1);
-  for (alpha=aini; alpha < aend; alpha+=da)
+  f = fopen("An.dat", "w+");
+  fprintf(f, "{ ");
+  fclose(f);
+  f = fopen("kn.dat", "w+");
+  fprintf(f, "{ ");
+  fclose(f);
+  for (alpha=aini; alpha <= aend; alpha+=da)
     {
+      sprintf(fn, "onsint_alpha_%f.dat", alpha);
+      f = fopen(fn, "w+");
+      tt = 0;
       while (tt < maxtrials)
 	{
 	  orient_onsager(&(u1[0]), &(u1[1]), &(u1[2]), alpha);
@@ -131,7 +248,26 @@ int main(int argc, char** argv)
 	    }	  
 	  tt++;
 	}
+      fclose(f);
+
+      f = fopen ("An.dat", "a+");
+      if (alpha==aini)
+	fprintf(f,"{%.15G,%.15G}", alpha, fact*integAn/((double)tt));
+      else
+	fprintf(f,",{%.15G,%.15G}", alpha, fact*integAn/((double)tt));
+      fclose(f);
+      f = fopen ("kn.dat", "a+");
+      if (alpha==aini)
+	fprintf(f,"{%.15G,%.15G}", alpha, fact*integkn/((double)tt));
+      else
+	fprintf(f,",{%.15G,%.15G}", alpha, fact*integkn/((double)tt));
+      fclose(f);
     }
+  f = fopen("An.dat", "a+");
+  fprintf(f, " };\n");
   fclose(f);
-  printf("final An=%.15G kn=%.15G\n", fact*integAn/((double)tt), fact*integkn/((double)tt));
+  f = fopen("kn.dat", "a+");
+  fprintf(f, " };\n");
+  fclose(f);
+
 }
