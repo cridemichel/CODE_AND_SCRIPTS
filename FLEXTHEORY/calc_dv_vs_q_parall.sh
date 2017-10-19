2b050c7fc2aca08d023b825060dfbcc9079cf0a8
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2017-10-19 09:48:10 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@3369 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- FLEXTHEORY/calc_dv_vs_q_parall.sh
+++ FLEXTHEORY/calc_dv_vs_q_parall.sh
@@ -13,14 +13,17 @@ cd ..
 for f in `echo $QS` 
 do
 TTE="0.0"
+TEX="0.0"
 TTR="0"
 for a in `ls -d alpha_*`
 do
 cd $a
 cd $f
 TE=`tail -n 1 $FN | LANG=C awk '{print $3}'` 
+TEX=`tail -n 1 $FN | LANG=C awk '{print $4}'` 
 TR=`tail -n 1 $FN | LANG=C awk '{print $1}'`
 TTE=`echo ${TTE}+${TE} | bc -l`
+TTEX=`echo ${TTEX}+${TEX}|bc -l`
 TTR=`echo ${TTR}+${TR} | bc -l`
 VOL3=`tail -n1 'iniconf_10-10.dat' |LANG=C awk '{print $1*$2*$3}'`
 cd ..
@@ -28,7 +31,7 @@ cd ..
 done
 Q=`echo $f| LANG=C awk -F _ '{print $2}'`
 QSQ=`echo $Q $Q| LANG=C awk '{print $1*$1}'`
-EV=`echo ${TTE}*${VOL3}/$TTR| bc -l`
+EV=`echo (${TTE}-${TTEX})*${VOL3}/${TTR}| bc -l`
 echo $QSQ $EV >> v_vs_qsq.dat
 echo $Q $EV >> v_vs_q.dat
 done
