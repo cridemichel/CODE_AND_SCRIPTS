b029c15a24ef7bf94d3ae515eb364bdfbd936d48
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2018-06-26 14:46:25 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@3947 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- BIFUNCSPH_KF/create_KFpoly_conf.c
+++ BIFUNCSPH_KF/create_KFpoly_conf.c
@@ -1,16 +1,20 @@
 #include<stdlib.h>
 #include<stdio.h>
-double nx, ny, nz, L, *rx, *ry, *rz;
+double nx, ny, nz, L[3], *rx, *ry, *rz;
 double *rxc, *ryc, *rzc, rxl, ryl, rzl, drx, dry, drz;
 int full, ibeg, numpoly;
 int main(int argc, char **argv)
 {
   FILE *f;
-  double Diam, del0, maxL;
-  int parnum=1000, i, j, polylen=1;
+  double Diam, del0, distKF=0.546;
+  int parnum=5848; //phi=0.299 i.e. coexistence according to Thuy's results using SUS
+  int i, j, polylen=1;
   f = fopen(argv[1], "w+");
-  L = 21.8781;
-  Diam=2.0;
+  Diam=1.0;
+  /* elongated box */
+  L[0] = 40.0*Diam;
+  L[1] = 16.0*Diam;
+  L[2] = 16.0*Diam;
   printf("argc=%d\n", argc);
   if (argc==3)
     parnum=atoi(argv[2]);
@@ -28,28 +32,25 @@ int main(int argc, char **argv)
       rzc[i] = 0.0;
     }
   fprintf(f, "parnum: %d\n", parnum);
-  fprintf(f,"ninters: 4\n");
+  fprintf(f,"ninters: 3\n");
   fprintf(f,"nintersIJ: 0\n");
   fprintf(f,"ntypes: 1\n");
   fprintf(f,"saveBonds: 0\n");
   fprintf(f, "@@@\n");
   fprintf(f, "%d\n", parnum);
-  fprintf(f,"1 1 1\n");
+  fprintf(f,"%f %f %f\n", Diam*0.5, Diam*0.5, Diam*0.5);
   fprintf(f,"2 2 2\n");
   fprintf(f, "1 1 1 1 2 0\n");
-  fprintf(f,"3 0\n");
-  fprintf(f,"1 0 0 0.5\n");
-  fprintf(f,"-1 0 0 0.5\n");
-  fprintf(f,"0.0 1 0 0.25\n");
-  fprintf(f,"0 0 0 0 0.0001 1000000000 10000000000 100000\n");
-  fprintf(f,"0 0 0 1 0.0001 1000000000 10000000000 100000\n");
-  fprintf(f,"0 1 0 1 0.0001 1000000000 10000000000 100000\n");
-  fprintf(f,"0 2 0 2 1 0 0 100000\n"); /* kern-frenkel patches */
+  fprintf(f,"2 0\n");
+  fprintf(f,"%f 0 0 %f\n",Diam*0.5, Diam*distKF);
+  fprintf(f,"%f 0 0 %f\n",-Diam*0.5, Diam*distKF);
+  fprintf(f,"0 0 0 0 1 0 0 100000\n");
+  fprintf(f,"0 0 0 1 1 0 0 100000\n");
+  fprintf(f,"0 1 0 1 1 0 0 100000\n");
   fprintf(f,"@@@\n");  
   nx=ny=nz=0;
   full=0;
   ibeg = 0;
-  maxL = L;
   numpoly=parnum/polylen;
   printf("numpoly=%d numpoly*polylen=%d\n", numpoly, numpoly*polylen);
   drx = Diam*polylen; //0.500000000001;
@@ -63,21 +64,21 @@ int main(int argc, char **argv)
   for (i=0; i < numpoly; i++)
     {
       rxl = rxc[polylen-1]+nx*drx+del0;
-      if (rxl+Diam*0.5 > L)
+      if (rxl+Diam*0.5 > L[0])
 	{
 	  ny++;
 	  nx=0;
 	  rxl = rxc[polylen-1]+nx*drx+del0;
 	}
       ryl = ryc[polylen-1]+ny*dry+del0;
-      if (ryl+Diam*0.5 > L)
+      if (ryl+Diam*0.5 > L[1])
 	{
 	  nz++;
 	  ny=0;
 	  ryl = ryc[polylen-1]+ny*dry+del0;
       	}
       rzl = rzc[polylen-1]+nz*drz+del0;
-      if (rzl+Diam*0.5 > L)
+      if (rzl+Diam*0.5 > L[2])
 	{
 	  full=1;
 	}
@@ -101,11 +102,11 @@ int main(int argc, char **argv)
   //   exit(-1);
   for (i=0; i < parnum; i++)
     {
-      rx[i] -= L*0.5;
-      ry[i] -= L*0.5;
-      rz[i] -= L*0.5;
+      rx[i] -= L[0]*0.5;
+      ry[i] -= L[1]*0.5;
+      rz[i] -= L[2]*0.5;
       fprintf(f, "%f %f %f 1 0 0 0 1 0 0 0 1 0\n", rx[i], ry[i], rz[i]);
     }
-  fprintf(f, "%.15G %.15G %.15G\n", L, L, L);
+  fprintf(f, "%.15G %.15G %.15G\n", L[0], L[1], L[2]);
   return 1;
 } 
