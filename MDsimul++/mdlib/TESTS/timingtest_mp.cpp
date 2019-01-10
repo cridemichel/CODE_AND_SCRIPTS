3f64678225cb65633ab6b0abbc99bd375112b33a
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-01-10 13:11:18 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@4536 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MDsimul++/mdlib/TESTS/timingtest_mp.cpp
+++ MDsimul++/mdlib/TESTS/timingtest_mp.cpp
@@ -41,7 +41,7 @@ double gauss(void)
 
 }
 
-#define MPC_MP
+//#define MPC_MP
 #ifdef CPP_MP
 #include <boost/multiprecision/cpp_bin_float.hpp> 
 #include <boost/multiprecision/cpp_complex.hpp>
@@ -63,8 +63,8 @@ using cmplx=complex<numty>;
 using namespace boost;
 using namespace boost::multiprecision;
 using namespace boost::multiprecision::backends;
-using numty=number<mpfr_float_backend<32>>;
-using cmplx=number<mpc_complex_backend<32>>;
+using numty=number<mpfr_float_backend<200>>;
+using cmplx=number<mpc_complex_backend<200>>;
 #elif defined(FL128)
 #include<complex>
 #include <boost/multiprecision/float128.hpp>
@@ -213,7 +213,10 @@ c << -0.2269860014469,0.106758402093,-0.02494545844908,0.08966693274224,-0.26134
 #if defined(MPC_MP) || defined(CPP_MP) || defined(FL128) || defined(GMP_MP)
   // set precision equal to precision of input coefficients (i.e. double epsilon)
   rp.set_output_prec(numeric_limits<double>::epsilon());
+  //rp.set_output_prec(numeric_limits<numty>::epsilon());
   //cout << "qui\n" << " eps=" << numeric_limits<double>::epsilon() << "\n" ;
+#else
+  rp.set_output_prec(0.001);
 #endif
   for (int i=0; i < maxiter; i++)
     {
@@ -230,10 +233,14 @@ c << -0.2269860014469,0.106758402093,-0.02494545844908,0.08966693274224,-0.26134
           //rp.zroots(roots, false);
           //rp.show("f(x)=");
           rp.find_roots(roots);
+          roots.set_show_digits(50);
+          //roots.show("roots=");
+          //rp.show("poly=");
         }
       else
         {
           rphqr.set_coeff(c);
+
           rphqr.find_roots(roots);
         }
     }
