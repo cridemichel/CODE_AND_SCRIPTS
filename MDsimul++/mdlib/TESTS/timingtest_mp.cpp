77beb1738b68588d1742daf63e2d6be60232e49e
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-01-15 13:22:49 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@4566 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MDsimul++/mdlib/TESTS/timingtest_mp.cpp
+++ MDsimul++/mdlib/TESTS/timingtest_mp.cpp
@@ -3,7 +3,7 @@
 //#include "pmatrix.H"
 #define CPOLY
 #define AURENTZ
-#define BACKSTAB
+//#define BACKSTAB
 #ifdef CPOLY
 #include "./cpoly.H"
 #else
@@ -106,7 +106,7 @@ double gauss(void)
 
 }
 
-#define MPC_MP
+//#define MPC_MP
 #ifdef CPP_MP
 #include <boost/multiprecision/cpp_bin_float.hpp> 
 #include <boost/multiprecision/cpp_complex.hpp>
