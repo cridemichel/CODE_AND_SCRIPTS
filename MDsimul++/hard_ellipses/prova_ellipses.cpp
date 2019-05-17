a39393cf2a47e31417a34fcf7fead3e360206f24
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-05-17 08:18:33 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@4861 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MDsimul++/hard_ellipses/prova_ellipses.cpp
+++ MDsimul++/hard_ellipses/prova_ellipses.cpp
@@ -10,16 +10,41 @@ int main (int argc, char **argv)
 {
   hardell<double> A, B;
   long long int tt;
-  double ov=0.0, ovc, ovcpp, shift[2]={0.0,0.0};
+  double Lbox, ov=0.0, ovc, ovcpp, shift[2]={0.0,0.0};
   if (argc==1)
     {
       printf("ok argc=0\n");
     }
+  A.a = 1.0;
+  A.b = 2.0;
+  B.a = 1.0;
+  B.b = 2.0;
+  Lbox = 10.0;
   for (tt=0; tt < atof(argv[1]); tt++)
     {
+      B.random_orient();
+      B.random_box(Lbox);
       if ((ovcpp=overlap(A,B)) < 0.0)
         ov+=1.0;
 #ifdef DEBUG_HE
+      rx[0] = A.r[0];
+      ry[0] = A.r[1];
+      R[0][0][0] = A.na[0];
+      R[0][0][1] = A.na[1];
+      R[0][1][0] = A.nb[0];
+      R[0][1][1] = A.nb[1];
+      sax[0][0] = A.a;
+      sax[0][1] = A.b;
+
+      rx[1] = B.r[0];
+      ry[1] = B.r[1];
+      R[1][0][0] = B.na[0];
+      R[1][0][1] = B.na[1];
+      R[1][1][0] = B.nb[0];
+      R[1][1][1] = B.nb[1];
+      sax[1][0] = B.a;
+      sax[1][1] = B.b;
+
       ovc=check_overlap_pw2d(0,1,shift);
       if (ovc*ovcpp < 0)
         {
