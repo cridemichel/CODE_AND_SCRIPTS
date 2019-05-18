c01eea44833c9cc1d0b94f12a9c574ea339d0c04
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-05-18 11:04:08 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@4870 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MDsimul++/hard_ellipses/prova_ellipses.cpp
+++ MDsimul++/hard_ellipses/prova_ellipses.cpp
@@ -1,6 +1,8 @@
 #include "./hardell.H"
+#include "../mdlib/boxes.H"
 extern double R[2][2][2], rx[2], ry[2], rz[2], sax[2][2];
 //#define DEBUG_HE
+#define USE_BBOX
 #ifdef DEBUG_HE
 extern "C" {
   double check_overlap_pw2d(int i, int j, double *shift);
@@ -9,6 +11,9 @@ extern "C" {
 #endif
 int main (int argc, char **argv)
 {
+#ifdef USE_BBOX
+  rectangle<double> rectA, rectB; 
+#endif
   hardell<double> A, B;
   long long int tt;
 #if DEBUG_HE
@@ -25,12 +30,33 @@ int main (int argc, char **argv)
   B.a = 1.0;
   B.b = 2.0;
   Lbox = 10.0;
+
+#ifdef USE_BBOX
+  rectA.r = A.r;
+  rectA.sax[0] = A.a;
+  rectA.sax[1] = A.b;
+  rectA.R.set_row(0,A.na);
+  rectA.R.set_row(1,A.nb);
+  rectB.sax[0] = B.a;
+  rectB.sax[1] = B.b;
+#endif
   for (tt=0; tt < atof(argv[1]); tt++)
     {
       B.random_orient();
       B.random_box(Lbox);
+#ifdef USE_BBOX
+      rectB.r = B.r;
+      rectB.R.set_row(0,B.na);
+      rectB.R.set_row(1,B.nb);
+      if (overlap(rectA,rectB) < 0.0)
+        {
+          if ((ovcpp=overlap(A,B)) < 0.0)
+            ov+=1.0;
+        }
+#else
       if ((ovcpp=overlap(A,B)) < 0.0)
         ov+=1.0;
+#endif
 #ifdef DEBUG_HE
       rx[0] = A.r[0];
       ry[0] = A.r[1];
@@ -64,4 +90,5 @@ int main (int argc, char **argv)
 #endif
 #endif
     }
+  cout << setprecision(16) << "excluded surface=" << Lbox*Lbox*ov/((double)tt) << "\n";
 }
