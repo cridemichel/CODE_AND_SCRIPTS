7ccef8c557d66b8c8cd7e22d94ce3316201f5561
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-05-21 19:08:47 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@4886 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MDsimul++/hard_ellipses/mcsim_hardell.cpp
+++ MDsimul++/hard_ellipses/mcsim_hardell.cpp
@@ -10,25 +10,7 @@ int main (int argc, char **argv)
     heMC.createconf(0);
 
   heMC.run();
-
 #if 0
-  for (tt=0; tt < he; tt++)
-    {
-      B.random_orient();
-      //B.na << 1,0;
-      //B.nb << 0,1;
-#if 1
-      theta=2.0*M_PI*drand48();
-      //dr = 2.0*(A.b-A.a); 
-
-      B.r << r*cos(theta), r*sin(theta);
-#else
-      B.random_box(Lbox);
-#endif
-      rectB.r = B.r;
-      rectB.R.set_row(0,B.na);
-      rectB.R.set_row(1,B.nb);
-
 #ifdef USE_BBOX
       if (overlap(rectA,rectB) < 0.0)
         {
