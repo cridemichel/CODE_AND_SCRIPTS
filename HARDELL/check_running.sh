8de9365fea08f02708ec8bd0027e5c7c146cb215
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-05-31 13:28:43 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@4989 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HARDELL/check_running.sh
+++ HARDELL/check_running.sh
@@ -10,16 +10,31 @@ IR="0"
 ST="100000000"
 TOTT="100000000000"
 EXE="calc_veff_hardell"
+STATUS=`cat ${PERC}/CHECK_STATUS`
+if [ \( "$STATUS" == "OFF" \) -o \( "$STATUS" == "off" \) -o \( "$STATUS" == "0" \) ]
+then
+echo "Check disabled..."
+exit
+fi
 while [ $IR -lt 300 ]
 do
 #calc_veff_hardell <trials> <X0> <NUMR> <ir> <savett>
+if [ ! -e ${PERC}/IR_$IR ]
+then
+continue
+fi
 cd ${PERC}/IR_$IR
 EN="veff_IR_$IR"
-ISRUN=`ps ax | grep $EN`
+ISRUN=`ps ax | grep $EN| grep mosrun`
 if [ "$ISRUN" == "" ]
 then
 echo "IR=" $IR " is not running, I will restart it"
-nohup mosun ./$EN 0 >> screen_$IR &
+if [ -e "calcveff.chk" ]
+then
+nohup mosrun ./$EN $TOTT $X0 $NUMR $IR $ST > screen_$IR &
+else 
+nohup mosrun ./$EN 0 >> screen_$IR &
+fi
 fi
 sleep 0.2
 IR=$[$[IR]+1]
