fd9222dc53a0af2ff05b9f022fed2f42b96621e4
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-05-31 14:30:23 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@4990 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HARDELL/check_running.sh
+++ HARDELL/check_running.sh
@@ -1,6 +1,6 @@
 if [ "$1" == "" ]
 then
-X0="1.5"
+X0="1.3"
 else
 X0="$1"
 fi
@@ -11,6 +11,11 @@ ST="100000000"
 TOTT="100000000000"
 EXE="calc_veff_hardell"
 STATUS=`cat ${PERC}/CHECK_STATUS`
+if [ ! -e ${PERC}/CHECK_STATUS ] 
+then
+echo "Check disabled..."
+exit
+fi
 if [ \( "$STATUS" == "OFF" \) -o \( "$STATUS" == "off" \) -o \( "$STATUS" == "0" \) ]
 then
 echo "Check disabled..."
@@ -31,11 +36,18 @@ then
 echo "IR=" $IR " is not running, I will restart it"
 if [ -e "calcveff.chk" ]
 then
-nohup mosrun ./$EN $TOTT $X0 $NUMR $IR $ST > screen_$IR &
-else 
-nohup mosrun ./$EN 0 >> screen_$IR &
-fi
-fi
+nohup mosrun ./$EN $TOTT $X0 $NUMR $IR $ST > screen_$I & 
+IR=$[$[IR]+1]
 sleep 0.2
+continue
+fi
+if [ ! -e "calcveff.chk" ] 
+then
+nohup mosrun ./$EN 0 >> screen_$IR & 
+IR=$[$[IR]+1]
+sleep 0.2
+continue
+fi
+fi
 IR=$[$[IR]+1]
 done
