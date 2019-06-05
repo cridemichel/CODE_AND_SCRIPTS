a0334e1d7b60ef6b4c43bb39d7c133eb030f9f4a
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-06-05 10:19:28 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5035 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HARDELL/delaunay.py
+++ HARDELL/delaunay.py
@@ -5,6 +5,8 @@ Created on Tue Jun  4 09:28:48 2019
 
 @author: demichel
 """
+import sys
+#import numpy as np
 from numpy import loadtxt
 import matplotlib.pyplot as plt
 #from scipy.spatial import Voronoi, voronoi_plot_2d
@@ -17,14 +19,44 @@ points = loadtxt("/Users/demichel/conf-python", comments="#", delimiter=" ", unp
 #points = np.array([[0, 0], [0, 1.1], [1, 0], [1, 1]])
 #vor=Voronoi(points)
 tri = Delaunay(points)
-print (points)
-print ("number of points=",points.size/2)
-for pindex in range(0,2515):
-    nn=find_neighbors(pindex,tri)
-    print("i=",pindex, nn)
-plt.triplot(points[:,0], points[:,1], tri.simplices.copy())
-plt.plot(points[:,0], points[:,1], 'o')
-plt.show()
+#print (points)
+#print ("number of points=",points.size/2)
+#for pindex in range(0,2515):
+#    nn=find_neighbors(pindex,tri)
+#    print("i=",pindex, nn)
+#plt.triplot(points[:,0], points[:,1], tri.simplices.copy())
+#plt.plot(points[:,0], points[:,1], 'o')
+#plt.show()
+#sys.exit(0)
 #voronoi_plot_2d(vor)
-plt.plot(points[:,0], points[:,1], 'o')
-plt.show()
+#plt.plot(points[:,0], points[:,1], 'o')
+#plt.show()
+#nnmy = loadtxt("/Users/demichel/nnmy.dat", dtype="int",comments="#", delimiter=" ", unpack=False)
+filename="/Users/demichel/nnmy.dat"
+#nnmy=np.genfromtxt(filename, delimiter=" ")
+#print(filecontents)
+pindex=0
+with open(filename,"r") as f:
+    for line in f:
+        nnmy=line.strip('\n').split(' ')    
+        print ("checkin i=", pindex)
+        nn=find_neighbors(pindex,tri)
+        if len(nn) != len(nnmy):
+            print ("numero diverso elementi per pindex=", pindex)
+            print ("nn=", nn)
+            print ("nnmy=", nnmy)
+            sys.exit(0)
+            print("len=", len(nn))
+            for p2 in range(0,len(nn)):
+                trovato=0         
+                for p3 in range(0,len(nnmy)):
+                    print ("nn=", nn[p2], " nnmy=", nnmy[p3])
+                    if int(nn[p2]) == int(nnmy[p3]):
+                        print ("qui\n")
+                        trovato=1
+                        break
+                    if trovato == 0:
+                        print("non trovato\n")
+                        sys.exit(0)
+        pindex += 1
+print ("tutto ok!\n")                
