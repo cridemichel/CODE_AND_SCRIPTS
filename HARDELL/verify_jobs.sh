c4d7a485bb404a9042e72418b5850115d795e176
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-06-06 15:22:19 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5066 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- HARDELL/verify_jobs.sh
+++ HARDELL/verify_jobs.sh
@@ -2,7 +2,9 @@ DIR=`pwd`
 AA=`basename $DIR`
 X0=`echo $AA | awk -F _ '{print $2}'`
 echo "X0=" $X0
-NRUN=`ps ax | grep veff | grep mosrun | grep -F -e "$X0"|wc -l`
+ps ax > _aaa_
+NRUN=`cat _aaa_ | grep veff | grep mosrun | grep -F -e "$X0"|wc -l`
+rm _aaa_
 NDONE=`tail IR_*/veff*dat|grep 99900000000| wc -l`
 echo "NRUN=" $NRUN
 echo "NDONE=" $NDONE
