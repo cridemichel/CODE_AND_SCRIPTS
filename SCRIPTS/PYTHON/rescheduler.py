06739b38300456ad06642b925dbf171d83939b34
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-06-07 16:27:39 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5102 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/PYTHON/rescheduler.py
+++ SCRIPTS/PYTHON/rescheduler.py
@@ -13,17 +13,30 @@ if len(args) > 1:
     lof=args[1]
 else:
     print('You have to supply a file with all jobs to check (with absolute paths)')
+    print('reschedluer <lista dirs> <totsteps> <extrasteps> <proc str ',end='')
+    print('filter(*=disabled)> <extra args> <sched type> ')
+    print('<sched type>=simpp or veff')
     quit()
 if len(args) > 2:
-    filter_proc=args[2] #common pattern in exec filenames
+    totsteps=int(args[2])
+else:
+    totsteps=-1# if negative do not extend simulation
+if len(args) > 3:
+    extsteps=int(args[3])# increment final step of sim by extsteps
+else:
+    extsteps=-1
+if len(args) > 4:
+    filter_proc=args[4] #common pattern in exec filenames
 else:
     filter_proc=''
-if len(args) > 3:
-    extra_args=args[3] #extra args for launching the executable
+if filter_proc== '*':
+    filter_proc=''
+if len(args) > 5:
+    extra_args=args[5] #extra args for launching the executable
 else:
     extra_args=''
-if len(args) > 4:
-    sched_type=args[4] # simpp or veff for now
+if len(args) > 6:
+    sched_type=args[6] # simpp or veff for now
 else:
     sched_type='simpp' # simpp or veff for now
 #
@@ -44,9 +57,9 @@ def get_proc_info(fil):
             continue
         #print ('p=',p, 'cmd=',pr.cmdline())
         #filtra le command line con la stringa 
-        cli=pr.cmdline()
+        cli=' '.join(pr.cmdline())
         if len(cli) > 1:
-            if fil == '' or cli[1].find(fil) != -1:
+            if fil == '' or cli.find(fil) != -1:
                 #print('cli1=', cli, ' boh=', cli[1].find(fil))
                 #print('pr.cwd=', pr.cwd())
                 allcls.append(cli)#command line con cui è stato eseguito			
@@ -110,9 +123,17 @@ if sched_type == 'simpp':
         return arg_restart[w]
     # test if simulation is finished (customize if needed)
     def sim_done(dir):
-        return os.path.exists(dir+'/'+donefile)        
+        if totsteps > 0:
+             w=choose_restart(dir)
+             s=get_steps(dir,w)
+             if s >= totsteps:
+                 return True
+             else:
+                 return False
+        else:     
+            return os.path.exists(dir+'/'+donefile)        
     #step to extend simulation
-    extra_steps=1000000
+    #extra_steps=1000000
     #
     def extend_sim(bn,w):   
         os.system('rm '+donefile)
@@ -122,7 +143,7 @@ if sched_type == 'simpp':
         #print ('l=',l)
         #print ('lines[0][2]=', ls[0][2])
         #overwrite filed total steps with new value
-        newsteps=int(l[2])+extra_steps
+        newsteps=int(l[2])+extsteps
         l[2]=str(newsteps)
         ls[0]=' '.join(l)
         #print ('dopo ls[0]=',ls[0])
@@ -139,7 +160,7 @@ elif sched_type == 'veff':
 # queste devono rimpiazzare le corrispondenti build_arg_start,
 # build_arg_restart e sim_done
 # inoltre anche la variabile restart_veff deve essere rinominata in restart
-    keep_going=False #if true restart finished simulations too
+# keep_going=False #if true restart finished simulations too
     def sim_done(dir):
         with open(dir+'/veff_vs_tt.dat') as f:
             ls=f.readlines()
@@ -162,7 +183,7 @@ else:
     
 def in_allcls(allcls,en):
     for l in allcls:
-        if l[1].find(en) != -1:
+        if l.find(en) != -1:
             return True
     return False
 #######################################
@@ -211,9 +232,9 @@ c=0
 #return command lines, pids and absolute path
 allcls,pids,allcwds=get_proc_info(filter_proc)
 ok=True
-ndone=0
-ndead=0
-nrun=0
+ndone=0# numero terminati
+ndead=0#numero morti
+nrun=0#numero running
 #we compare absolute paths to determine if process is running
 #(we assume that each jobs has been launched from a different dir)
 nline=0
@@ -231,44 +252,53 @@ for l in lines:
         if sim_done(dir): 
             #found a cnf-final if keepgoing=True 
             # and not max num of jobs reache then restart (see below)
-            if keep_going == True:
-                which=choose_restart(bn)
+            #if keep_going == True:
+             #   which=choose_restart(bn)
                 # se non ci sono i restart ma c'il file cnf-final
                 # allora la simulazione è terminata
                 # se ci sono i restart invece possiamo estenderla
                 # (verrà fatto dopo utilizzando la lista lstdone)
-                if which != -1:
-                    lstdone.append([get_steps(dir,which),l])
-                else:
-                    ndone+=1
-            else:
-                ndone+=1 
+              #  if which != -1:
+               #     lstdone.append([get_steps(dir,which),l])
+               # else:
+               #     ndone+=1
+            #else:
+             #   ndone+=1 
+             ndone += 1 
         else:
             #not a cnf-final found (i.e. simulation is dead)
             #look for restart files
-            ndead+=1
-            if ndead + nrun > max_jobs:
+            if nrun >= max_jobs:
                 #print ('maximum number of jobs (' + str(max_jobs) + ') reached')
-                continue
-            print('job '+ en + ' is not running and it has not finished yet!', end='')
-            print(' I am restarting it...')
+                break
             which=choose_restart(bn)
+            if totsteps > 0 and os.path.exists(dir+'/'+donefile) and which != -1: 
+                lstdone.append([get_steps(dir,which),l])
+                continue
+            else:
+                print(donefile+' exists but restart file does not...')
+                print('I try to restart sim from begin')
+            #print('job '+ en + ' is not running and it has not finished yet!', end='')
+            #print(' I am restarting it...')
             if which == -1:
+                nrun += 1
                 #no restart file, first start
                 exec=' ./'+en+build_arg_start(nline,extra_args)
                 os.chdir(dir)
-                #print('dir=',os.getcwd())
+                print('[first start, dir=',os.getcwd(),'] ',end='')
                 s2e=prepend + exec + postpend 
-                print('Initial launch: ', s2e)
+                print('exec: ', s2e)
                 os.system(s2e)
             else:
+                ndead += 1
                 #at least one restart file found
                 exec=' ./'+en+build_arg_restart(nline,extra_args,which)
                 #print ('exec is: '+exec)
                 os.chdir(dir)
-                #print('dir=',os.getcwd())
+                print('[restart dead, dir=',os.getcwd(),'] ',end='')
                 s2e=prepend + exec + postpend 
-                print('Restart, executing ', s2e)
+                print('exec: ', s2e)
+                nrun += 1
                 os.system(s2e)
                 ok=False
     else:#bn is running if here
@@ -276,14 +306,13 @@ for l in lines:
     nline+=1
 # all jobs with a cnf-final are extended now 
 # if number of jobs does not exceed max_jobs
-if keep_going == True:
+if totsteps > 0:
     #sort list of done simulations from lower steps to higher
     #so that simulations left behind start first
     lstdone.sort(key=itemgetter(0))
     nj=0
-    njmax = max_jobs-(nrun+ndead)
     for l in lstdone:
-        if nj >= njmax:
+        if nrun >= max_jobs:
             break
         bn, en=os.path.split(l[1].strip('\n'))
         os.chdir(bn)
@@ -291,9 +320,10 @@ if keep_going == True:
         extend_sim(bn,which)
         exec=' ./'+en+build_arg_restart(nline,extra_args,which)
         s2e=prepend + exec + postpend 
-        print('[keepgoing] executing ', s2e)
+        print('[extend simulation, dir=',bn,'] exec: ', s2e)
         os.system(s2e) 
         nj += 1
+        nrun +=1
 #       
 if not ok:
 	print('Some jobs (#'+str(ndead)+') were dead and I had to restart them!')
@@ -301,7 +331,7 @@ else:
     if keep_going == True:
         if ndone < len(lines):
             #print ('ndone=',ndone, ' nrun=', nrun, ' ndead=', ndead)
-            print('[keepgoing] Jobs now running='+str(nj+nrun+ndead),end='')
+            print('[keepgoing] Jobs now running='+str(nrun),end='')
             print(', completed='+str(ndone)+'/'+str(len(lines))+' (max:'+str(max_jobs)+')')
         else:
             print('[keepgoing] All done here!')
@@ -309,5 +339,5 @@ else:
         if ndone == len(lines):
             print('All done here!')
         else:
-            print('Jobs now running '+str(nrun+ndead), end='')
+            print('Jobs now running '+str(nrun), end='')
             print(', completed '+ str(ndone) + '/'+str(len(lines)))
