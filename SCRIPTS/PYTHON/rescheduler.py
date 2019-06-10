c0508f8db6cf86d4fdfb4f5fb3a25c14f20fec19
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-06-10 06:52:28 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5125 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- SCRIPTS/PYTHON/rescheduler.py
+++ SCRIPTS/PYTHON/rescheduler.py
@@ -18,7 +18,7 @@ def print_error():
     print('You have to supply a config file where:')
     print('first lines=<tosteps=-1|>0> <extra steps=-1|>0> <max jobs>')
     print('list of jobs to check (with absolute paths)')
-    print('reschedluer [-f<filter string>|-s/-show|-v/-verbos|-t/-type <type>] <conf file>')
+    print('reschedluer [-f<filter string>|-s/-show|-v/-verbos|-t/-type <type>|-extargs|-ea] <conf file>')
     quit()
 show_only = False
 verbose = False   
