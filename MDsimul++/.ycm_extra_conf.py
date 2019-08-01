dc009276a8a8500e1152d5dc8f5106447093bb84
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-08-01 13:00:59 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5351 fea3b138-1bf5-0310-9da5-cbb00e418fd0

--- MDsimul++/.ycm_extra_conf.py
+++
@@ -1,9 +0,0 @@
-def Settings( **kwargs ):
-    #language = kwargs[ 'language' ]
-    #if language=='cfamily':
-    #if language == 'python':
-    #MACROS=''   
-    #MACLST=MACROS.split(' ')
-    flags = [ '-x', 'c++', '-std=c++17', '-Wall', '-Wextra', '-I ../mdlib']
-    #flags=flags+MACLST
-    return {'flags': flags}
