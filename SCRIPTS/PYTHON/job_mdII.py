71ad3d30bb63b60ece35bcc699eb38f39baeb62c
demichel <demichel@fea3b138-1bf5-0310-9da5-cbb00e418fd0>
2019-08-09 07:52:57 +0000

    upd
    git-svn-id: file:///Users/demichel/SVNREPOS/CODE@5359 fea3b138-1bf5-0310-9da5-cbb00e418fd0

---
+++ SCRIPTS/PYTHON/job_mdII.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 09:43:18 2019
+
+@author: demichel
+"""
+#job script
+# arguments passed by rescheduler.py
+# $1 = 1 or 2; 1=restart 2=start
+# $2 = restart file which is good for restarting 
+# $3 = <totsteps> (-1 means to not extend)
+# $4 = <extend steps> steps by which extend current job
+# $5 = jobfinished file (typically named '_DONE_')
+# $6 ... possible extra args
+import sys,os
+args=sys.argv
+jtype=int(args[1])
+restart=args[2]
+totsteps=int(args[3])
+extsteps=int(args[4])
+jobfinished=args[5]
+### SIMULATION DEPENDENT VARIABLES
+prepend=' ' # prepend to exec name
+exec_name=prepend+'./hard_cylinders ' # sim executable
+arg_start=' -fa ellipsoid_flex_mc.par ' # command line arg for starting
+arg_restart=' -ca ' + restart #command line arg for restarting
+##########################################################
+# SIMULATION DEPENDENT CODE
+# following functions depend on simulation file format
+#
+# get current simulation steps from restart file
+def get_steps(restart):
+    with open(restart) as f:
+        ls=f.readlines()
+    for l in ls:
+        ll= l.strip('\n').split(':')
+        if ll[0].find('curStep')!=-1:
+            curstps=int(ll[1])        
+    return curstps
+#
+#extend simulation by modifying restart file
+def extend_sim(restart):   
+    #os.system('rm '+ donefile)
+    with open(restart) as f:
+        ls=f.readlines()
+    cc=0
+    for l in ls:
+        ll= l.strip('\n').split(':')
+        if ll[0].find('totStep')!=-1:
+            ls[cc] = ll[0]+': '+ str(int(ll[1])+extsteps) + '\n'
+        cc+=1     
+    #overwrite file total steps with new value
+    with open(restart,"w") as f:
+        for l in ls:
+            f.write(l)
+#
+#establish whether simulation is finished (simulations steps > totsteps)
+def sim_done(restart):
+    if totsteps > 0:
+        s=get_steps(restart)
+        if s >= totsteps:
+            return True
+        else:
+            return False
+####################################################
+if sim_done(restart):
+    os.system('echo \"job regularly finished\" > '+jobfinished)
+    quit()
+if jtype==2:#start for the first time
+    os.system(exec_name+arg_start + ' > screen') 
+else:#restart extending if requested, i.e. if totsteps > 0 
+    if totsteps > 0:
+        extend_sim(restart)
+    os.system(exec_name + arg_restart + ' >> screen')
+quit()
