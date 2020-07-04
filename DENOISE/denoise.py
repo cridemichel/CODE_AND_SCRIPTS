1f20788892c4b0e73a2ce67bcafa16870651cba6
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2020-07-04 15:46:16 +0200

    minor fix

--- DENOISE/denoise.py
+++ DENOISE/denoise.py
@@ -17,7 +17,10 @@ import sys,json,datetime,os
 #           zerolatency – good for fast encoding and low-latency streaming
 #           psnr – ignore this as it is only used for codec development
 #           ssim – ignore this as it is only used for codec development 
-
+# usage example:
+# ./denoise.py -s ultrafast -f hqdn -t 4.0  video.MOV video_denoised.mkv
+# best denoiser (faster and also with better looking results) is hqdn, default threshold is 4.0
+# with a threshold equal to 16.0 video taken at 12800 with D750 can effectively denoised!
 prog='ffmpeg'
 
 #helper functions
@@ -25,7 +28,9 @@ def print_error():
     print('syntax: extract_data.py [--compression|-c <0-51 0=lossless>')
     print(' -s|--speed <ultrafast|superfast|veryfast|faster|fast|medium|slow|slower|veryslow|placebo >')
     print(' -tune  <film|animation|grain|stillimage|fastdecode|zerolatency|psnr|ssim>')
-    print(' -co|--codec <x264|x265> --filter|-f <hqdn|ata|nlm|vag|bm|dct|owd> --deflicker|-d] <input_file> <output_file default=out.mkv>')
+    print(' -co|--codec <x264|x265> --filter|-f <hqdn|ata|nlm|vag|bm|dct|owd> --deflicker|-d]')
+    print(' --filtpars|-p <params according to ffmpeg documentation --thrden|-t <level of denoise (positive numb)>')
+    print(' <input_file> <output_file default=out.mkv>')
 
 args = sys.argv
 #print(args)
@@ -40,7 +45,11 @@ nfilter = 'hqdn3d'
 speed = 'medium'
 codec= 'x264'
 defl=False
+#filtpars='=6.0:4.5:9.0:6.75' # this values are the default ones
+#filtpars='=4.0:3.0:6.0:4.5' # this values are the default ones
+filtpars=''
 compress='26' #default is 23 ma this way file has a size equal to original nikon MOV
+denthr=''
 del(args[0])
 for a in itargs:
     if a == '--help' or a  == '-h':
@@ -54,16 +63,20 @@ for a in itargs:
         codec=next(itargs)
     elif a == '-f' or a == '--filter':
         nfilter = next(itargs)
+    elif a == '-t' or a == '--thrden':
+        denthr = next(itargs)
+    elif a == '-p' or a == '--filtpars':
+        filtpars = next(itargs)
     elif a == '-d' or a == '--deflicker':
         defl = True
     else:
         print('a=', a)
         infile = a
         outfile= next(itargs)
-#hqdn3dpars='=6.0:4.5:9.0:6.75' # this values are the default ones
-#hqdn3dpars='=4.0:3.0:6.0:4.5' # this values are the default ones
-hqdn3dpars='' # default values
 
+if filtpars != '' and  denthr != '':
+    print('[ERROR] you can set either the thereshold for denoising or the denoise filter parameters')
+    quit()
 #use hqdn (this should be the best one!) or ata which are faster
 #vaguedenoiser is good bu slower
 #nlmeans and bm3d are way too slow...
@@ -76,7 +89,7 @@ hqdn3dpars='' # default values
 #  owdenoise (0.3 fps) - color neutral wavelet denoiser with stunningly good results on high-res sources
 #  vaguedenoiser (7.6 fps) - another color neutral wavelet denoiser whose output looks identical to owdenoise, but its processing speed is 25x faster; tried every combination of threshold and nsteps, and found the default settings of 2/6 to consistently produce the closest-to-real-life results
 if nfilter == 'hqdn':
-    denstr='hqdn3d'+hqdn3dpars
+    denstr='hqdn3d'
 elif nfilter == 'ata':
     denstr='atadenoise'
 elif nfilter == 'nlm':
@@ -88,10 +101,24 @@ elif nfilter == 'bm':
 elif nfilter == 'owd':
     denstr='owdenoise'
 elif nfilter == 'dct':
-    denstr = dctdnoiz'
+    denstr = 'dctdnoiz'
 else:
-    denstr='hqdn3d'+hqdn3dpars
-
+    denstr='hqdn3d'
+if filtpars != '':
+    denstr=denstr+'='+filtpars
+if denthr != '':
+     #hqdn3d: threshold can from 4.0 (default) to 16.0 (high denoising)
+     #vaguedenoiser: threshold can be from 2 to 10 (high denoising)
+    if denstr=='hqdn3d':
+        ls = float(denthr)
+        cs = 3.0*ls/4.0
+        lt = 6.0
+        ct = 4.5
+        denstr=denstr+'='+str(ls)+':'+str(cs)+':'+str(lt)+':'+str(ct)
+    else:
+        denstr=denstr+'='+denthr
+if defl==True:
+    denstr = denstr + ',deflicker'
 if codec=='x264':
     codstr='libx264'
 elif compress=='x265':
@@ -99,8 +126,6 @@ elif compress=='x265':
 else:
     codstr='libx264'
 exestr=prog + ' -i ' + infile + ' -c:v ' + codstr + ' -c:a copy' + ' -crf ' + compress + ' -preset ' + speed + ' -filter:v ' + denstr
-if defl==True:
-    exestr = exestr + ' -filter:v deflicker'
 exestr = exestr + ' ' + outfile
 print(exestr)
 os.system(exestr)
