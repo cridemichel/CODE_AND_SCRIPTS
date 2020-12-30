f4c3ffd1fabe1af6b81f944c8ce3e08bd87c504c
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2020-12-30 09:13:15 +0100

    scripts for checking energy of MD simulations

--- SCRIPTS/PYTHON/calc_sigE_vs_dt.sh
+++ SCRIPTS/PYTHON/calc_sigE_vs_dt.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
 DT="0.0001"
 cc="0"
 K="1.41421"
+#TRUN="0.1"
 TRUN="0.5"
+OFL="logsigE_vs_logdt.dat"
 OF="sigE_vs_dt.dat"
 PF="bmljpars.xasc"
 TPF="bmljpars_templ.xasc"
 echo -n "" > $OF
+echo -n "" > $OFL
 EXE="../mdsim_bmlj 2"
 while [ $cc -lt 14 ]
 do
@@ -16,6 +20,12 @@ do
   cc=$[$cc+1]
   $EXE
   DE=`./calc_avg_sig.py energy.dat | awk '{print $2}'`
+  AVGE=`./calc_avg_sig.py energy.dat | awk '{print $1}'`
+  LDT=`echo "$DT" | awk '{print log($1)/log(10.0)}'`
+  DEN=`echo "$DE $AVGE"| awk '{if ($2 < 0) ae=-$2; else ae=$2; printf("%.15G", $1/ae)}'`
+  LDEN=`echo "$DEN" | awk '{printf ("%.15G", log($1)/log(10.0))}'`
+  LDE=`echo $DE | awk '{printf("%.15G", log($1)/log(10.0)}'`
+  echo $LDT $LDE >> $OFL
   echo $DT $DE >> $OF
   DT=`echo "$DT*$K" | bc -l`
 done
