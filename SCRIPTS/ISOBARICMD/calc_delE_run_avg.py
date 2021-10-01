316521225e6eb0adefe054b8ee348c72e69df088
Cristiano De Michele <cristiano.demichele@uniroma1.it>
2021-10-01 09:37:29 +0200

    upd

--- SCRIPTS/ISOBARICMD/calc_delE_run_avg.py
+++ SCRIPTS/ISOBARICMD/calc_delE_run_avg.py
@@ -23,20 +23,25 @@ def calcstddev(vec):
     s = np.sqrt(s / c1)
     return s
 
-def calcedr(vec,lst,lst2):
+def calcedr(vecx, vecy, lst, xp, lst2,num):
     # calc energy drift according to
     #Yu et al. Chemical Physics 370 (2010) 294–305 (see Fig. 1 therein)
     c1=1.0
-    v0 = vec[0]
+    v0 = vecy[0]
     ssum=0.0
-    for v in vec:
+    subset=np.arange(0, len(vecy),num)
+    print(subset)
+    for i in subset:
+        vy = vecy[i]
+        vx = vecx[i]
         if absenedr==True:
-            val=np.abs((v-v0)/v0)
+            val=np.abs((vy-v0)/v0)
         else:
-            val=(v-v0)/v0
+            val=(vy-v0)/v0
         ssum += val
         lst.append(ssum/c1)
         lst2.append(val)
+        xp.append(vx)
         c1+=1.0
 
 def calcra(vec, lst):
@@ -59,24 +64,30 @@ if len(sys.argv) == 1:
     print("[ERROR] you have to supply a file with a list of directories")
     quit()
 fn=sys.argv[1]
+if len(sys.argv) > 2:
+    elems=int(sys.argv[2])
+else:
+    elems=1
 with open(fn) as f:
     listadirs=f.readlines()
 yp=[1.0]
 yp2=[1.0]
+xp=[1.0]
 for ef in listadirs:
     olddir=os.getcwd()
     os.chdir(ef.strip('\n'))
     x, y = np.loadtxt('energy.dat', comments=['#'], usecols=(0,1), unpack=True)
     yp.clear()
     ra.clear()
-    calcedr(y, ra, yp)
-    columns=np.column_stack((x,yp))
+    xp.clear()
+    calcedr(x, y, ra, xp, yp, elems)
+    columns=np.column_stack((xp,yp))
     #print(yp)
     np.savetxt('delE.dat', columns)
     #sd=calcstddev(y)
     #yp2.clear()
     #calcra(y,yp2)
     yp2=ra
-    columns=np.column_stack((x,yp2))
+    columns=np.column_stack((xp,yp2))
     np.savetxt('runavg_delE.dat', columns)
     os.chdir(olddir)
